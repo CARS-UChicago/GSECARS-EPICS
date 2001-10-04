21b08ff47f7e4d79ea3827ddbdf973da3e615b70
epics <>
2001-10-04 13:37:08 +0000

    Initial version of ioc13bmd

---
+++ iocBoot/ioc13bmd/st.cmd
@@ -0,0 +1,179 @@
+# vxWorks startup file for 13BMD IOC
+< cdCommands
+
+< ../nfsCommandsGSE
+loginUserAdd "epics","SzeSebbzRR"
+
+cd topbin
+ld < iocCore
+ld < seq
+ld < CARSLib
+
+# This IOC talks to a remote GPIB server
+ld < GpibHideosRemote.o
+
+# Currently, the only thing we do in initHooks is call reboot_restore(), which
+# restores positions and settings saved ~continuously while EPICS is alive.
+# See calls to "create_monitor_set()" at the end of this file.  To disable
+# autorestore, comment out the following line.
+ld < initHooks.o
+
+cd startup
+
+# Initialize remote MPF connection
+routerInit
+tcpMessageRouterClientStart(1,9900,"164.54.160.126",10000,40)
+
+# Initialize local MPF connection
+localMessageRouterStart(0)
+
+# Set debugging flags
+#reboot_restoreDebug = 5
+#save_restore_debug =10
+devMcaMpfDebug=0
+mcaAIMServerDebug=0
+aimDebug=0
+
+# override address, interrupt vector, etc. information in module_types.h
+module_types()
+
+# need more entries in Wait-record channel-access queue (?)
+recDynLinkQsize = 1024
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build.
+dbLoadDatabase("../../dbd/CARSApp.dbd")
+
+# Load database
+dbLoadRecords  "stdApp/Db/Jscaler.db","P=13BMD:,S=scaler1,C=0", std
+dbLoadRecords  "ipApp/Db/SR570.db", "P=13BMD:,A=A1,C=1,IPSLOT=a,CHAN=0", ip
+dbLoadRecords  "ipApp/Db/SR570.db", "P=13BMD:,A=A2,C=1,IPSLOT=a,CHAN=2", ip
+dbLoadRecords  "ipApp/Db/SR570.db", "P=13BMD:,A=A3,C=1,IPSLOT=a,CHAN=3", ip
+dbLoadRecords  "CARSApp/Db/generic_serial.db","P=13BMD:,R=ser2,C=1,IPSLOT=a,CHAN=5,BAUD=9600,PRTY=None,DBIT=8,SBIT=1", top
+#dbLoadRecords  "CARSApp/Db/generic_gpib.db", "P=13BMD:,R=gpib1,SIZE=2048", top
+dbLoadRecords  "CARSApp/Db/Keithley2kDMM_mf.db", "P=13BMD:,Dmm=DMM2,C=1,IPSLOT=a,CHAN=6", top
+dbLoadRecords  "CARSApp/Db/lvp_dmm.db", "P=13BMD:,Dmm=DMM2,DLY=0.1", top
+dbLoadRecords  "CARSApp/Db/Keithley2kDMM_mf.db", "P=13BMD:,Dmm=DMM1,C=1,IPSLOT=a,CHAN=7", top
+dbLoadRecords  "CARSApp/Db/lvp_dmm.db", "P=13BMD:,Dmm=DMM1,DLY=0.1", top
+dbLoadTemplate "DAC.template"
+dbLoadTemplate "LVP_furnace_control.template"
+dbLoadTemplate "motors.template"
+
+# Acromag Ip330 ADC
+dbLoadTemplate "Ip330_ADC.template"
+
+# IP-Unidig binary I/O
+dbLoadTemplate "IpUnidig.template"
+
+# SMART detector database
+str=malloc(256)
+strcpy(str,"P=13BMD:,R=smart1,C=1,IPSLOT=a,CHAN=5,BAUD=9600,")
+strcat(str,"FSHUT=UnidigBo0,TRIG=UnidigBo1,SSHUT=UnidigBo2")
+dbLoadRecords("CARSApp/Db/smartControl.db",str,top)
+
+
+# CCD synchronization record
+dbLoadRecords  "CARSApp/Db/CCD.db", "P=13BMD:,C=CCD1", top
+
+# Multichannel analyzer stuff
+# AIMConfig(mpfServer, card, ethernet_address, port, maxChans, 
+#           maxSignals, maxSequences, ethernetDevice, queueSize)
+AIMConfig("NI9CE/1", 0x9CE, 1, 2048, 1, 1,"dc0", 40)
+AIMConfig("NI9CE/2", 0x9CE, 2, 2048, 4, 1,"dc0", 40)
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=aim_adc1,DTYPE=MPF MCA,INP=#C0 S0 @NI9CE/1,NCHAN=2048", mca)
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=aim_mcs1,DTYPE=MPF MCA,INP=#C0 S0 @NI9CE/2,NCHAN=2048", mca)
+picbServer = icbConfig("icb/1", 10, "NI9CE:5", 100)
+dbLoadRecords "mcaApp/Db/icb_adc.db", "P=13BMD:,ADC=adc1,CARD=0,SERVER=icb/1,ADDR=0", mca
+icbAddModule(picbServer, 1, "NI9CE:3")
+dbLoadRecords "mcaApp/Db/icb_amp.db", "P=13BMD:,AMP=amp1,CARD=0,SERVER=icb/1,ADDR=1", mca
+
+# Set up Struck multichannel scaler
+#STR7201Setup(1,0xA0000000,220,6)
+#STR7201Config(0, 4, 2048)
+#dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=mca_str1,DTYPE=Struck STR7201 MCS,NCHAN=1024,INP=#C0 S0", mca)
+
+# IP-330 ADC with MCA record as transient recorder
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=mip330_1,DTYPE=MPF MCA,NCHAN=2048,INP=#C1 S0 @b-Ip330Sweep", mca)
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=mip330_2,DTYPE=MPF MCA,NCHAN=2048,INP=#C1 S1 @b-Ip330Sweep", mca)
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=mip330_3,DTYPE=MPF MCA,NCHAN=2048,INP=#C1 S2 @b-Ip330Sweep", mca)
+dbLoadRecords("mcaApp/Db/mca.db", "P=13BMD:,M=mip330_4,DTYPE=MPF MCA,NCHAN=2048,INP=#C1 S3 @b-Ip330Sweep", mca)
+
+### Allstop, alldone
+# This database must agree with the motors you've actually loaded.
+# Several versions (e.g., all_com_40.db) are in std/stdApp/Db
+dbLoadRecords("stdApp/Db/all_com_56.db","P=13BMD:", std)
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("stdApp/Db/scan.db","P=13BMD:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10", std)
+
+# A set of scan parameters for each positioner.  This is a convenience
+# for the user.  It can contain an entry for each scannable thing in the
+# crate.
+dbLoadTemplate "scanParms.template"
+
+# Miscellaneous PV's, such as burtResult
+dbLoadRecords("stdApp/Db/misc.db","P=13BMD:", std)
+dbLoadRecords("stdApp/Db/userTransform.db", "P=13BMD:, N=1", std)
+dbLoadRecords("stdApp/Db/userTransform.db", "P=13BMD:, N=2", std)
+
+# Experiment description
+dbLoadRecords("CARSApp/Db/experiment_info.db","P=13BMD:", top)
+
+# vxWorks statistics
+dbLoadRecords("stdApp/Db/VXstats.db","P=13BMD:", std)
+
+################################################################################
+# Setup device/driver support addresses, interrupt vectors, etc.
+
+# OMS VME58 driver setup parameters: 
+#     (1)cards, (2)axis per card, (3)base address(short, 4k boundary), 
+#     (4)interrupt vector (0=disable or  64 - 255), (5)interrupt level (1 - 6),
+#     (6)motor task polling rate (min=1Hz,max=60Hz)
+oms58Setup(7, 8, 0x4000, 190, 5, 10)
+
+# Joerger VSC setup parameters: 
+#     (1)cards, (2)base address(ext, 256-byte boundary), 
+#     (3)interrupt vector (0=disable or  64 - 255)
+VSCSetup(1, 0xB0000000, 200)
+ 
+# dbrestore setup
+sr_restore_incomplete_sets_ok = 1
+#reboot_restoreDebug=5
+
+# Link to GPIB server.  Do this just before iocInit, since it waits for MPF
+# server to connect
+#HiDEOSGpibLinkConfig(10,1,"GPIB0")
+
+# Wait 40 seconds so the MPF server can come up.  Problems with serial stuff
+# if we don't do this.
+taskDelay(2400)
+
+iocInit
+
+### Start up the autosave task and tell it what to do.
+# The task is actually named "save_restore".
+# (See also, 'initHooks' above, which is the means by which the values that
+# will be saved by the task we're starting here are going to be restored.
+#
+# save positions every five seconds
+create_monitor_set("auto_positions.req",5.0)
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req",30.0)
+
+seq &Keithley2kDMM, "P=13BMD:, Dmm=DMM1, stack=10000"
+#ybW 1/19/99
+seq &Keithley2kDMM, "P=13BMD:, Dmm=DMM2, stack=15000"
+seq &BMD_LVP_Detector, "P=13BMD:,PMT=pm4,PMR=pm3,X=m9,Y=m16,Z=m10,TV=m12,TH=m13"
+
+# Force the DAC to be 0 volts.  The hardware does this automatically on VME 
+# reset but the the software display is not correct
+dbpf "13BMD:DAC1_1", "0."
+
+# Need to wait 15 seconds before starting this task or CPU use is 100% - TRACK DOWN WHY !!!
+taskDelay(900)
+seq &smartControl, "P=13BMD:,R=smart1,TTH=m38,OMEGA=m38,PHI=m38,KAPPA=m38,SCALER=scaler1,I0=2,stack=10000"
---
+++ iocBoot/ioc13bmd/st_mpfserver.cmd
@@ -0,0 +1,166 @@
+<cdCommands
+
+< ../nfsServerCommandsGSE
+
+cd topbin
+# topbin is for the primary CPU arch, need to move to AUX CPU arch
+ld < ../mv162/mpfLib 
+ld < ../mv162/mpfServLib 
+
+carrier = "ipac"
+ipacAddCarrier(&ipmv162, "A:l=3,3 m=0xe0000000,64;B:l=3,3 m=0xe0010000,64;C:l=3,3 m=0xe0020000,64;D:l=3,3 m=0xe0030000,64")
+initIpacCarrier(carrier, 0)
+
+routerInit
+tcpMessageRouterServerStart(1,9900,"164.54.160.126",10000,100)
+
+# Initialize GPIB stuff
+# REMOVED 9/17/00 to make room for Unidig Binary I/O
+#initGpibGsTi9914("GS-IP488-0",carrier,"IP_c",104)
+#initGpibServer("GPIB0","GS-IP488-0",1024,1000)
+
+# Initialize Octal UART stuff
+initOctalUART("octalUart0",carrier,"IP_a",8,100)
+#initOctalUART("octalUart1",carrier,"IP_b",8,101)
+# initOctalUARTPort(char* portName,char* moduleName,int port,int baud,
+#                   char* parity,int stop_bits,int bits_char,char* flow_control)
+# 'baud' is the baud rate. 1200, 2400, 4800, 9600, 19200, 38400
+# 'parity' is "E" for even, "O" for odd, "N" for none.
+# 'stop_bits' = {1,2}
+# 'bits_per_character' = {5,6,7,8}
+# 'flow_control' is "N" for none, "H" for hardware
+initOctalUARTPort("UART[0]","octalUart0",0, 9600,"N",2,8,"N") /* SRS 570 */
+initOctalUARTPort("UART[1]","octalUart0",1,19200,"N",1,8,"N") /* Keithley 2000 */
+initOctalUARTPort("UART[2]","octalUart0",2, 9600,"N",2,8,"N") /* SRS 570 */
+initOctalUARTPort("UART[3]","octalUart0",3, 9600,"N",2,8,"N") /* SRS 570 */
+initOctalUARTPort("UART[4]","octalUart0",4,19200,"N",1,8,"N") /* MM4000 */
+initOctalUARTPort("UART[5]","octalUart0",5, 9600,"N",1,8,"N") /* SMART PC */
+initOctalUARTPort("UART[6]","octalUart0",6, 9600,"N",1,8,"N") /* Keithley 2000 */
+initOctalUARTPort("UART[7]","octalUart0",7, 9600,"N",1,8,"N") /* Keithley 2000 */
+initSerialServer("a-Serial[0]","UART[0]",1000,20,"\r",1)
+initSerialServer("a-Serial[1]","UART[1]",1000,20,"\r",1)
+initSerialServer("a-Serial[2]","UART[2]",1000,20,"\r",1)
+initSerialServer("a-Serial[3]","UART[3]",1000,20,"\r",1)
+initSerialServer("a-Serial[4]","UART[4]",1000,20,"\r",1)
+initSerialServer("a-Serial[5]","UART[5]",1000,20,"\r",1)
+initSerialServer("a-Serial[6]","UART[6]",1000,20,"\r",1)
+initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
+
+# Initialize Systran DAC
+initDAC128V("d-DAC",carrier,"IP_d",20)
+
+# Initialize Acromag IP-330 ADC
+# Ip330 *initIp330(
+#   const char *moduleName, const char *carrierName, const char *siteName,
+#   const char *typeString, const char *rangeString,
+#   int firstChan, int lastChan,
+#   int maxClients, int intVec)
+# function return value  = pointer to the Ip330 object, needed by configIp330
+#                          and to initialize the application-specific classes
+# serverName  = name to give this server
+# carrierName = name of IPAC carrier from initIpacCarrier above
+# siteName    = name of IP site, e.g. "IP_a"
+# typeString  = "D" or "S" for differential or single-ended
+# rangeString = "-5to5","-10to10","0to5", or "0to10"
+#               This value must match hardware setting selected
+# firstChan   = first channel to be digitized.  This must be in the range:
+#               0 to 31 (single-ended)
+#               0 to 15 (differential)
+# lastChan    = last channel to be digitized
+# maxClients =  Maximum number of Ip330 tasks which will attach to this
+#               Ip330 module.  For example Ip330Scan, Ip330Sweep, etc.  This
+#               does not refer to the number of EPICS clients.  A value of
+#               10 should certainly be safe.
+# intVec        Interrupt vector
+pIp330 = initIp330("b-Ip330", carrier,"IP_b","D","-5to5",0,15,10,120)
+
+# int configIp330(
+#   Ip330 *pIp330,
+#   scanModeType scanMode, const char *triggerString,
+#   int microSecondsPerScan, int secondsBetweenCalibrate)
+# pIp330      = pointer to the Ip330 object, returned by initIp330 above
+# scanMode    = scan mode:
+#               0 = disable
+#               1 = uniformContinuous
+#               2 = uniformSingle
+#               3 = burstContinuous (normally recommended)
+#               4 = burstSingle
+#               5 = convertOnExternalTriggerOnly
+# triggerString = "Input" or "Output". Selects the direction of the external
+#               trigger signal.
+# microSecondsPerScan = repeat interval to digitize all channels
+#               The minimum theoretical time is 15 microseconds times the
+#               number of channels, but a practical limit is probably 100
+#               microseconds.
+# secondsBetweenCalibrate = number of seconds between calibration cycles.
+#               If zero then there will be no periodic calibration, but
+#               one calibration will still be done at initialization.
+configIp330(pIp330, 3,"Input",500,0)
+
+# int initIp330Scan(
+#      Ip330 *pIp330, const char *serverName, int firstChan, int lastChan,
+#      int milliSecondsToAverage, int queueSize)
+# pIp330     = pointer returned by initIp330 above
+# serverName = name to give this server.  Must match the INP parm field in
+#              EPICS records
+# firstChan  = first channel to be used by Ip330Scan.  This must be in the
+#              range firstChan to lastChan specified in initIp330
+# lastChan   = last channel to be used by Ip330Scan.  This must be in the range
+#              firstChan to lastChan specified in initIp330
+# milliSecondsToAverage = number of milliseconds to average readings
+# queueSize  = size of output queue for MPF. Make this the maximum number 
+#              of ai records attached to this server.
+initIp330Scan(pIp330,"b-Ip330Scan",0,15,100,20)
+
+# int initIp330Sweep(Ip330 *pIp330, char *serverName, int firstChan, 
+#     int lastChan, int maxPoints, int queueSize)
+# pIp330     = pointer returned by initIp330 above
+# serverName = name to give this server
+# firstChan  = first channel to be used by Ip330Sweep.  This must be in the
+#              range firstChan to lastChan specified in initIp330
+# lastChan   = last channel to be used by Ip330Sweep.  This must be in the
+#              range firstChan to lastChan specified in initIp330
+# maxPoints  = maximum number of points in a sweep.  The amount of memory
+#              allocated will be maxPoints*(lastChan-firstChan+1)*4 bytes
+# queueSize  = size of output queue for EPICS
+initIp330Sweep(pIp330,"b-Ip330Sweep",0,3,2048,100)
+
+# Ip330PID *initIp330PID(const char *serverName,
+#        Ip330 *pIp330, int ADCChannel, DAC128V *pDAC128V, int DACChannel,
+#        int queueSize)
+# serverName = name to give this server
+# pIp330     = pointer returned by initIp330 above
+# ADCChannel = ADC channel to be used by Ip330PID as its readback source.  
+#              This must be in the range firstChan to lastChan specified in 
+#              initIp330
+# pDAC128V   = pointer returned by initDAC128V
+# DACChannel = DAC channel to be used by Ip330PID as its control output.  This
+#              must be in the range 0-7.
+# queueSize  = size of output queue for EPICS
+#pIp330PID = initIp330PID("Ip330PID_1", pIp330, 0, pDAC128V, 0, 20)
+
+# int configIp330PID(Ip330PID *pIp330PID,
+#        double KP, double KI, double KD,
+#        int interval, int feedbackOn, int lowLimit, int highLimit)
+# pIp330PID  = pointer returned by initIp330PID above
+# KP         = proportional gain
+# KI         = integral gain
+# KD         = derivative gain
+# interval   = microseconds per feedback loop
+# feedbackOn = 0 for feedback off, 1 for feedback on
+# lowLimit   = low limit on DAC output
+# highLimit  = high limit on DAC output
+#configIp330PID(pIp330PID, .1, 10., 0., 1000, 0, 500, 1500)
+
+# Initialize Greenspring IP-Unidig
+# initIpUnidig(char *serverName, char *carrierName, char *siteName,
+#              int queueSize)
+# serverName  = name to give this server
+# carrierName = name of IPAC carrier from initIpacCarrier above
+# siteName    = name of IP site, e.g. "IP_a"
+# queueSize   = size of output queue for EPICS
+initIpUnidig("c-Unidig", carrier, "IP_c", 20)
+
+
+# DEBUGGING
+#serialPortSniff("UART[3]",1000)
