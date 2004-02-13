f9b7016f3470be6c37d88f426642892c42113f94
epics <>
2004-02-13 04:32:15 +0000

    New file for Newport XPS motor controller

---
+++ CARSApp/src/Socket.h
@@ -0,0 +1,8 @@
+
+int    ConnectToServer (char *Ip_Address, int Ip_Port, double Timeout);
+void   SetTCPTimeout(int SocketIndex, double TimeOut);
+void   SendAndReceive (int SocketIndex, char *buffer, char *valueRtrn);
+void   CloseSocket (int SocketIndex);
+void   CloseAllSockets (void);
+void   ResetAllSockets (void);
+char * GetError (int SocketIndex);
