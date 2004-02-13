8fb568a6b53dbb3f67cb8af20dce4588d3133124
epics <>
2004-02-13 06:00:31 +0000

    Changes for 3.14

--- iocBoot/ioc13idc/Struck8.cmd
+++ iocBoot/ioc13idc/Struck8.cmd
@@ -1,4 +1,4 @@
 STR7201Setup(1,0x90000000,220,6)
 STR7201Config(0, 8, 2048)
-dbLoadRecords("mcaApp/Db/Struck8.db","P=13IDC:str:", mca)
+dbLoadRecords("$(MCA)/mcaApp/Db/Struck8.db","P=13IDC:str:")
 dbLoadTemplate("Struck8.template")
