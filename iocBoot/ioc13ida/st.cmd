70e1f0bc3d9b3dfa7294361c367aeaebdcfae15b
epics <>
2004-10-03 21:52:48 +0000

    Changes for asyn; moved serial stuff to serial.cmd

--- iocBoot/ioc13ida/st.cmd
+++ iocBoot/ioc13ida/st.cmd
@@ -16,14 +16,13 @@ CARSVX_registerRecordDeviceDriver(pdbbase)
 
 # This IOC loads the MPF server code locally
 cd startup
-< st_mpfserver.cmd
+< industryPack.cmd
+< serial.cmd
 
 # Debugging flags
 devPM304Debug=0
 drvPM304Debug=0
 devA32VmeDebug=0
-devMPCDebug=0
-quadEMDebug=0
 
 # Set up the Allen-Bradley 6008 scanner
 abConfigNlinks 1
@@ -36,27 +35,7 @@ dbLoadTemplate("eps_inputs.template")
 dbLoadTemplate("eps_outputs.template")
 dbLoadTemplate("eps_valves.template")
 
-# Load asyn records on all serial ports
-dbLoadTemplate("asynRecord.template")
-dbLoadRecords("$(IP)/ipApp/Db/MKS.db","P=13IDA:,C=0,SERVER=serial1,CC1=cc1,CC2=cc3,PR1=pr1,PR2=pr3")
-dbLoadRecords("$(IP)/ipApp/Db/MKS.db","P=13IDA:,C=0,SERVER=serial2,CC1=cc2,CC2=ccA,PR1=pr2,PR2=prA")
-dbLoadRecords("$(IP)/ipApp/Db/Digitel.db","P=13IDA:,PUMP=ip1,C=0,PORT=serial3")
-dbLoadRecords("$(IP)/ipApp/Db/Digitel.db","P=13IDA:,PUMP=ip3,C=0,PORT=serial4")
-dbLoadRecords("$(IP)/ipApp/Db/MKS.db","P=13IDA:,C=0,SERVER=serial5,CC1=cc5,CC2=cc6,PR1=pr5,PR2=pr6")
-dbLoadRecords("$(IP)/ipApp/Db/Digitel.db","P=13IDA:,PUMP=ip5,C=0,PORT=serial6")
-dbLoadRecords("$(IP)/ipApp/Db/MPC.db","P=13IDA:,PUMP=ip2,C=0,SERVER=serial7,PA=0,PN=1", ip)
-# serial8 is McClennan PM-304 motor controller
-dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13IDA:,Dmm=DMM1,C=0,PORT=serial9")
-dbLoadRecords("$(CARS)/CARSApp/Db/ILM200.db","P=13IDA:,R=ILM200,C=0,PORT=serial10")
-dbLoadRecords("$(IP)/ipApp/Db/MKS.db","P=13IDA:,C=0,SERVER=serial11,CC1=cc7,CC2=ccB,PR1=pr7,PR2=prB")
-dbLoadRecords("$(IP)/ipApp/Db/MPC.db","P=13IDA:,PUMP=ip6,C=0,SERVER=serial12,PA=0,PN=1")
-dbLoadRecords("$(IP)/ipApp/Db/MPC.db","P=13IDA:,PUMP=ip7,C=0,SERVER=serial12,PA=0,PN=2")
-dbLoadRecords("$(IP)/ipApp/Db/TSP.db","P=13IDA:,TSP=tsp1,C=0,SERVER=serial12,PA=0")
-
-dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13IDA:,Dmm=DMM2,C=0,PORT=serial13")
-
 dbLoadTemplate("motors.template")
-
 # Digital to analog converter, used for Queensgate piezo drivers
 dbLoadTemplate("DAC.template")
 
@@ -64,9 +43,9 @@ dbLoadTemplate("DAC.template")
 dbLoadTemplate("IpUnidig.template")
 
 #Quad electrometer
- dbLoadRecords("$(QUADEM)/quadEMApp/Db/quadEM.db", "P=13IDA:, EM=EM1, CARD=0, SERVER=quadEM1")
+dbLoadRecords("$(QUADEM)/quadEMApp/Db/quadEM.db", "P=13IDA:, EM=EM1, CARD=0, PORT=quadEM1")
 
-dbLoadRecords("$(QUADEM)/quadEMApp/Db/quadEM_med.db", "P=13IDA:quadEM:,NCHAN=2048,C=0,SERVER=quadEMSweep")
+dbLoadRecords("$(QUADEM)/quadEMApp/Db/quadEM_med.db", "P=13IDA:quadEM:,NCHAN=2048,PORT=quadEMSweep")
 dbLoadRecords("$(QUADEM)/quadEMApp/Db/quadEM_med_FFT.db", "P=13IDA:quadEM_FFT:,NCHAN=1024")
 
 # Monochromator PID
@@ -99,6 +78,10 @@ dbLoadRecords("$(STD)/stdApp/Db/misc.db","P=13IDA:")
 # vxWorks statistics
 dbLoadTemplate("vxStats.substitutions")
 
+< ../save_restore.cmd
+save_restoreSet_status_prefix("13IDA:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13IDA:")
+
 ################################################################################
 # Setup device/driver support addresses, interrupt vectors, etc.
 
@@ -123,20 +106,6 @@ devA32VmeConfig(0, 0xb0000200, 30, 0xa0, 5)
 #     (6)motor task polling rate (min=1Hz,max=60Hz)
 oms58Setup(4, 8, 0x4000, 190, 5, 10)
 
-# PM304 driver setup parameters: 
-#     (1) maximum # of controllers, 
-#     (2) maximum # axis per controller
-#     (3) motor task polling rate (min=1Hz, max=60Hz)
-PM304Setup(1, 1, 10)
-
-# PM304 driver configuration parameters:
-#     (1) controller
-#     (2) asyn port
-#     (3) # axes
-# Example:
-#   PM304Config(0, "serial4", 1)
-PM304Config(0, "serial8", 1)
-
 # initQuadEM(baseAddress, fiberChannel, microSecondsPerScan, maxClients,
 #            unidigName, unidigChan)
 #  quadEMName  = name of quadEM object created
@@ -144,52 +113,22 @@ PM304Config(0, "serial8", 1)
 #  channel     = 0-3, fiber channel number
 #  microSecondsPerScan = microseconds to integrate.  When used with ipUnidig
 #                interrupts the unit is also read at this rate.
-#  maxClients  = maximum number of clients that will connect to the
-#                quadEM interrupt.  10 should be fine.
 #  unidigName  = name of ipInidig server if it is used for interrupts.
 #                Set to 0 if there is no IP-Unidig being used, in which
 #                case the quadEM will be read at 60Hz.
 #  unidigChan  = IP-Unidig channel connected to quadEM pulse output
 
 # Use this line for 60Hz polling
-#initQuadEM("quadEM1", 0xf000, 0, 1000, 10, 0, 0)
+#initQuadEM("quadEM1", 0xf000, 0, 1000, 0, 0)
 # Use this line for interrupts on channel 0 of IpUnidig
-initQuadEM("quadEM1", 0xf000, 0, 1000, 10, "Unidig1", 0)
+initQuadEM("quadEM1", 0xf000, 0, 1000, "Unidig1", 0)
 
-# initQuadEMScan(quadEMName, serverName, queueSize)
-#  quadEMName = name of quadEM object created with initQuadEM
-#  serverName = name of MPF server (string)
-#  queueSize  = size of MPF queue
-initQuadEMScan("quadEM1", "quadEM1", 100)
-
-# initQuadEMSweep(quadEMName, serverName, maxPoints, int queueSize)
-#  quadEMName = name of quadEM object created with initQuadEM
-#  serverName = name of MPF server (string)
+# initFastSweep(portName, inputName, maxSignals, maxPoints)
+#  portName = asyn port name for this new port (string)
+#  inputName = name of asynPort providing data
+#  maxSignals  = maximum number of signals (spectra)
 #  maxPoints  = maximum number of channels per spectrum
-#  queueSize  = size of MPF queue
-initQuadEMSweep("quadEM1", "quadEMSweep", 2048, 400)
-
-# initQuadEMPID(serverName, quadEMName, quadEMChannel,
-#               DACName, DACChannel, queueSize)
-#  serverName  = name of MPF server (string)
-#  quadEMName = name of quadEM object created with initQuadEM
-#  quadEMChannel = quadEM "channel" to be used for feedback (0-9)
-#                  These are defined as:
-#                        0 = current 1
-#                        1 = current 2
-#                        2 = current 3
-#                        3 = current 4
-#                        4 = sum 1 = current1 + current3
-#                        5 = sum 2 = current2 + current4
-#                        6 = difference 1 = current3 - current1
-#                        7 = difference 2 = current4 - current2
-#                        8 = position 1 = difference1/sum1 * 32767
-#                        9 = position 2 = difference2/sum2 * 32767
-#  DACName     = name of DAC128V server created with initDAC128V
-#  DACVChannel = DAC channel number used for this PID (0-7)
-#  queueSize   = size of MPF queue
-initQuadEMPID("quadEMPID1", "quadEM1", 8, "DAC1", 2, 20)
-initQuadEMPID("quadEMPID2", "quadEM1", 9, "DAC1", 1, 20)
+initFastSweep("quadEMSweep", "quadEM1", 10, 2048)
 
 # dbrestore setup
 sr_restore_incomplete_sets_ok = 1
@@ -202,7 +141,6 @@ iocInit
 # (See also, 'initHooks' above, which is the means by which the values that
 # will be saved by the task we're starting here are going to be restored.
 #
-< ../requestFileCommands
 # save positions every five seconds
 create_monitor_set("auto_positions.req",5)
 # save other things every thirty seconds
