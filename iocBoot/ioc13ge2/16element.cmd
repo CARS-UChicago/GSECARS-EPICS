97834ebc07b5c26683a6c85ded9e2a613640ef0b
epics <>
2004-12-06 01:42:20 +0000

    New Fippi files and convert to asyn

--- iocBoot/ioc13ge2/16element.cmd
+++ iocBoot/ioc13ge2/16element.cmd
@@ -1,35 +1,43 @@
 # Setup the DXP CAMAC MCA.
 # Change the following environment variable to point to the desired
 # system configuration file
-putenv("XIA_CONFIG=xiasystem.cfg")
+
+epicsEnvSet("XIA_CONFIG", "xiasystem.cfg")
 # Set environment variables for the FiPPI files. 
 # FIPPI0,FIPPI1,FIPPI2,FIPPI3 should point to the files for 
 # decimation=0,2,4,6 respectively. FIPPI_DEFAULT should point to the
 # file that will be loaded initially at boot time.
 #
-# MN: Jun-28-2004 changed from:
-putenv("FIPPI0=f01x2p0g.fip")
-putenv("FIPPI1=f01x2p2g.fip")
-putenv("FIPPI2=f01x2p4g.fip")
-putenv("FIPPI3=f01x2p6g.fip")
-putenv("FIPPI_DEFAULT=f01x2p4g.fip")
+# These are the new Rev. 9 files.  They don't work right.
+#epicsEnvSet("FIPPI0", "fxpd00j_psam.fip")
+#epicsEnvSet("FIPPI1", "fxpd200j_st.fip")
+#epicsEnvSet("FIPPI2", "fxpd420j_st.fip")
+#epicsEnvSet("FIPPI3", "fxpd640j_st.fip")
+#epicsEnvSet("FIPPI_DEFAULT", "fxpd200j_st.fip")
 
-# from PNC CAT:
-# putenv("FIPPI0=f01x2pc0j.fip")
-# putenv("FIPPI1=f01x2pc2j.fip")
-# putenv("FIPPI2=f01x2pc4j.fip")
-# putenv("FIPPI3=f01x2pc6j.fip")
-# putenv("FIPPI_DEFAULT=f01x2pc0j.fip")
+# These are the older Rev. 6 reset files
+epicsEnvSet("FIPPI0", "f01x2p0g.fip")
+epicsEnvSet("FIPPI1", "f01x2p2g.fip")
+epicsEnvSet("FIPPI2", "f01x2p4g.fip")
+epicsEnvSet("FIPPI3", "f01x2p6g.fip")
+epicsEnvSet("FIPPI_DEFAULT", "f01x2p4g.fip")
+
+# These are the Rev. 8 RC files
+#epicsEnvSet("FIPPI0", "fxrc00.fip")
+#epicsEnvSet("FIPPI1", "fxrc20.fip")
+#epicsEnvSet("FIPPI2", "fxrc40.fip")
+#epicsEnvSet("FIPPI3", "fxrc60.fip")
+#epicsEnvSet("FIPPI_DEFAULT", "fxrc20.fip")
 
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
 dxp_md_set_log_level(2)
 dxp_initialize
 
-# DXPConfig(serverName, detChan1, detChan2, detChan3, detChan4, queueSize)
-DXPConfig("DXP1",  0,  1,  2,  3, 300)
-DXPConfig("DXP2",  4,  5,  6,  7, 300)
-DXPConfig("DXP3",  8,  9, 10, 11, 300)
-DXPConfig("DXP4", 12, 13, 14, 15, 300)
+# DXPConfig(serverName, detChan1, detChan2, detChan3, detChan4)
+DXPConfig("DXP1",  0,  1,  2,  3)
+DXPConfig("DXP2",  4,  5,  6,  7)
+DXPConfig("DXP3",  8,  9, 10, 11)
+DXPConfig("DXP4", 12, 13, 14, 15)
 
 dbLoadRecords("$(DXP)/dxpApp/Db/16element_dxp.db","P=13GE2:med:")
 
