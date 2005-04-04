baa84a32f814f11f572e1a7f40ee324afd5891f5
epics <>
2005-04-04 23:03:55 +0000

    Change Unidig to Undiig1; change syntax of tyGSAsynInit

--- iocBoot/ioc13idd/serial.cmd
+++ iocBoot/ioc13idd/serial.cmd
@@ -1,19 +1,19 @@
-tyGSAsynInit("serial1",  0, 0,  9600,'N',2,8,'N',"") /* SRS 570 */
-tyGSAsynInit("serial2",  0, 1,  9600,'N',2,8,'N',"") /* SRS 570 */
-tyGSAsynInit("serial3",  0, 2, 19200,'N',1,8,'N',"") /* Keithley 2000 */
-tyGSAsynInit("serial4",  0, 3,  4800,'N',1,8,'N',"") /* Generic serial / LPC */
-tyGSAsynInit("serial5",  0, 4, 19200,'N',1,8,'N',"") /* Keithley 2000 */
-tyGSAsynInit("serial6",  0, 5, 19200,'N',1,8,'N',"") /* Keithley 2000 */
-tyGSAsynInit("serial7",  0, 6,  9600,'N',2,8,'N',"") /* SRS 570 */
-tyGSAsynInit("serial8",  0, 7,  9600,'N',1,8,'N',"") /* SMART PC */
-tyGSAsynInit("serial9",  1, 0,  9600,'N',2,7,'N',"") /* Omega meter */
-tyGSAsynInit("serial10", 1, 1, 19200,'N',1,8,'N',"") /* RSF encoder readout */
-tyGSAsynInit("serial11", 1, 2, 19200,'N',1,8,'N',"") /* PicoMotor controller */
-tyGSAsynInit("serial12", 1, 3, 19200,'N',1,8,'N',"") /* Keithley 2700 */
-tyGSAsynInit("serial13", 1, 4,  9600,'N',1,8,'N',"") /* Unused */
-tyGSAsynInit("serial14", 1, 5,  9600,'N',1,8,'N',"") /* Unused */
-tyGSAsynInit("serial15", 1, 6,  9600,'N',1,8,'N',"") /* Unused */
-tyGSAsynInit("serial16", 1, 7,  9600,'N',1,8,'N',"") /* Unused */
+tyGSAsynInit("serial1",  "UART0", 0,  9600,'N',2,8,'N',"\r","\r") /* SRS 570 */
+tyGSAsynInit("serial2",  "UART0", 1,  9600,'N',2,8,'N',"\r","\r") /* SRS 570 */
+tyGSAsynInit("serial3",  "UART0", 2, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
+tyGSAsynInit("serial4",  "UART0", 3,  4800,'N',1,8,'N',"\r","\r") /* Generic serial / LPC */
+tyGSAsynInit("serial5",  "UART0", 4, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
+tyGSAsynInit("serial6",  "UART0", 5, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
+tyGSAsynInit("serial7",  "UART0", 6,  9600,'N',2,8,'N',"\r","\r") /* SRS 570 */
+tyGSAsynInit("serial8",  "UART0", 7,  9600,'N',1,8,'N',"\r","\r") /* SMART PC */
+tyGSAsynInit("serial9",  "UART1", 0,  9600,'N',2,7,'N',"\r","\r") /* Omega meter */
+tyGSAsynInit("serial10", "UART1", 1, 19200,'N',1,8,'N',"\r","\r") /* RSF encoder readout */
+tyGSAsynInit("serial11", "UART1", 2, 19200,'N',1,8,'N',"\r","\r") /* PicoMotor controller */
+tyGSAsynInit("serial12", "UART1", 3, 19200,'N',1,8,'N',"\r","\r") /* Keithley 2700 */
+tyGSAsynInit("serial13", "UART1", 4,  9600,'N',1,8,'N',"\r","\r") /* Unused */
+tyGSAsynInit("serial14", "UART1", 5,  9600,'N',1,8,'N',"\r","\r") /* Unused */
+tyGSAsynInit("serial15", "UART1", 6,  9600,'N',1,8,'N',"\r","\r") /* Unused */
+tyGSAsynInit("serial16", "UART1", 7,  9600,'N',1,8,'N',"\r","\r") /* Unused */
 
 # asyn record on each serial port
 dbLoadTemplate("asynRecord.template")
@@ -31,7 +31,7 @@ dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDD:,A=A3,PORT=serial7")
 str=malloc(256)
 strcpy(str,"P=13IDD:,R=smart1,PORT=serial8,")
 # Use Bo0 for Bruker shutter, Bo11 for XIA
-strcat(str,"FSHUT=UnidigBo11,TRIG=UnidigBo1,SSHUT=UnidigBo2")
+strcat(str,"FSHUT=Unidig1Bo11,TRIG=Unidig1Bo1,SSHUT=Unidig1Bo2")
 dbLoadRecords("$(CCD)/ccdApp/Db/smartControl.db", str)
 
 
@@ -39,5 +39,4 @@ dbLoadRecords("$(CARS)/CARSApp/Db/RSF715.db","P=13IDD:,ENCODER=RSF715,PORT=seria
 # Serial 11 is picoMotors
 dbLoadTemplate("picoMotors.template")
 dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db", "P=13IDD:,Dmm=DMM5,PORT=serial12")
-
 
