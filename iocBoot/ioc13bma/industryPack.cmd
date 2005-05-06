ccba4a5cdc548db43278e9cff461aff9a8a2227c
epics <>
2005-05-06 03:05:34 +0000

    New syntax for several commands

--- iocBoot/ioc13bma/industryPack.cmd
+++ iocBoot/ioc13bma/industryPack.cmd
@@ -2,10 +2,23 @@ ipacAddVIPC616_01("0x3000,0xa0000000")
 
 # Initialize Octal UART stuff
 tyGSOctalDrv 2
-tyGSOctalModuleInit("GSIP_OCTAL232", 0x80, 0, 0)
-tyGSOctalModuleInit("GSIP_OCTAL232", 0x81, 0, 1)
+# Initialize IP Octal modules.
+# ----------------------------
+# tyGSOctalModuleInit(char *moduleID, char *ModuleType, int irq_num,
+#                     char *carrier#, int slot#)
+#   moduleID   - assign the IP module a name for future reference.
+#   ModuleType - "232", "422", or "485".
+#   irq_num    - interrupt request number.
+#   carrier#   - carrier# assigned from the ipacAddCarrierType() call.
+#   slot#      - slot number on carrier; slot[A,B,C,D] -> slot#[0,1,2,3].
+tyGSOctalModuleInit("UART0", "232", 0x80, 0, 0)
+tyGSOctalModuleInit("UART1", "232", 0x81, 0, 1)
 
 # Initialize Systran DAC
+# initDAC128V(char *portName, int carrier, int slot)
+# portName  = name to give this asyn port
+# carrier     = IPAC carrier number (0, 1, etc.)
+# slot        = IPAC slot (0,1,2,3, etc.)
 initDAC128V("DAC1",0,3)
 dbLoadTemplate "DAC.template"
 
--- iocBoot/ioc13ida/industryPack.cmd
+++ iocBoot/ioc13ida/industryPack.cmd
@@ -2,8 +2,25 @@ ipacAddVIPC616_01("0x3000,0xa0000000")
 
 # Initialize Octal UART stuff
 tyGSOctalDrv 2
-tyGSOctalModuleInit("GSIP_OCTAL232", 0x80, 0, 0)
-tyGSOctalModuleInit("GSIP_OCTAL232", 0x81, 0, 1)
+# Initialize IP Octal modules.
+# ----------------------------
+# tyGSOctalModuleInit(char *moduleID, char *ModuleType, int irq_num,
+#                     char *carrier#, int slot#)
+#   moduleID   - assign the IP module a name for future reference.
+#   ModuleType - "232", "422", or "485".
+#   irq_num    - interrupt request number.
+#   carrier#   - carrier# assigned from the ipacAddCarrierType() call.
+#   slot#      - slot number on carrier; slot[A,B,C,D] -> slot#[0,1,2,3].
+tyGSOctalModuleInit("UART0", "232", 0x80, 0, 0)
+tyGSOctalModuleInit("UART1", "232", 0x81, 0, 1)
+
+# Initialize Systran DAC
+# initDAC128V(char *portName, int carrier, int slot)
+# portName  = name to give this asyn port
+# carrier     = IPAC carrier number (0, 1, etc.)
+# slot        = IPAC slot (0,1,2,3, etc.)
+initDAC128V("DAC1",0,3)
+dbLoadTemplate "DAC.template"
 
 # Initialize Greenspring IP-Unidig
 # initIpUnidig(char *portName,
@@ -13,14 +30,16 @@ tyGSOctalModuleInit("GSIP_OCTAL232", 0x81, 0, 1)
 #              int intVec,
 #              int risingMask,
 #              int fallingMask)
-# portName    = name to give this asyn port
+# portName  = name to give this asyn port
 # carrier     = IPAC carrier number (0, 1, etc.)
 # slot        = IPAC slot (0,1,2,3, etc.)
-# msecPoll    = polling time for input bits in msec.  Default=100.
+# msecPoll    = polling time for input bits that don't use interrupts in msec.
 # intVec      = interrupt vector
 # risingMask  = mask of bits to generate interrupts on low to high (24 bits)
 # fallingMask = mask of bits to generate interrupts on high to low (24 bits)
-initIpUnidig("Unidig1", 0, 2, 2000, 116, 0x0, 0x0)
 
-# Initialize Systran DAC
-initDAC128V("DAC1",0,3)
+# Note: We have the quadEM connected to channel 0.  Its driver will
+# enable interrupts on one edge of the pulses, so we don't need to do it here.  All other
+# inputs generate interrupts on both rising and falling edge
+initIpUnidig("Unidig1", 0, 2, 2000, 116, 0xfffffe, 0xfffffe)
+dbLoadTemplate "ipUnidig.substitutions"
