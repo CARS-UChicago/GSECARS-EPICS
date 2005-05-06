7a89e6c618f3fd2d1dbfe4a4f940e9359904a498
epics <>
2005-05-06 03:58:02 +0000

    Added MAXV; enable Keithley seq programs

--- iocBoot/ioc13lab/st.cmd
+++ iocBoot/ioc13lab/st.cmd
@@ -179,11 +179,37 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13LAB:")
 ################################################################################
 # Setup device/driver support addresses, interrupt vectors, etc.
 
+
+# OMS MAXv driver setup parameters:
+#     (1)number of cards in array.
+#     (2)VME Address Type (16,24,32).
+#     (3)Base Address on 4K (0x1000) boundary.
+#     (4)interrupt vector (0=disable or  64 - 255).
+#     (5)interrupt level (1 - 6).
+#     (6)motor task polling rate (min=1Hz,max=60Hz).
+MAXvSetup(1, 16, 0x4000, 190, 5, 10)
+
+#!drvMAXvdebug=4
+
+# OMS MAXv configuration string:
+#     (1) number of card being configured (0-14).
+#     (2) configuration string; axis type (PSO/PSE/PSM) MUST be set here.
+#         For example, set which TTL signal level defines
+#         an active limit switch.  Set X,Y,Z,T to active low and set U,V,R,S
+#         to active high.  Set all axes to open-loop stepper (PSO). See MAXv
+#         User's Manual for LL/LH and PSO/PSE/PSM commands.
+#config0="AX LL PSO; AY LL PSO; AZ LL PSO; AT LL PSO; AU LH PSO; AV LH PSO; AR LH PSO; AS LH PSO;"
+#!config0="AX LH PSM; AY LL PSO; AZ LL PSO; AT LL PSO; AU LH PSO; AV LH PSO; AR LH PSO; AS LH PSO;"
+
+# Set all axes to open-loop stepper and active low limits
+config0="AX LH PSO; AY LH PSO; AZ LH PSO; AT LH PSO; AU LH PSO; AV LH PSO; AR LH PSO; AS LH PSO;"
+MAXvConfig(0, config0)
+
 # OMS VME58 driver setup parameters: 
 #     (1)cards, (2)base address(short, 4k boundary), 
 #     (3)interrupt vector (0=disable or  64 - 255), (4)interrupt level (1 - 6),
 #     (5)motor task polling rate (min=1Hz,max=60Hz)
-oms58Setup(2, 0x4000, 190, 5, 10)
+oms58Setup(1, 0x5000, 190, 5, 10)
 
 # initQuadEM(quadEMName, baseAddress, fiberChannel, microSecondsPerScan, 
 #            maxClients, unidigName, unidigChan)
@@ -212,7 +238,7 @@ initFastSweep("quadEMSweep", "quadEM1", 10, 2048)
 VSCSetup(1, 0xB0000000, 200)
 
 #asynSetTraceMask "AIM1/1",0,0xff
-#asynSetTraceMask "icbTca1",0,0xff
+#asynSetTraceMask "icbTca1",0,0x13
 #asynSetTraceMask "icbHvps1",0,0xff
 #asynSetTraceMask "Unidig1",0,0x19
 #asynSetTraceMask "DAC1",0,0xff
@@ -236,8 +262,8 @@ create_monitor_set("auto_settings.req", 30)
 dbpf "13LAB:EnableUserTrans.PROC","1"
 dbpf "13LAB:EnableUserSCalcs.PROC","1"
 
-#seq &Keithley2kDMM, "P=13LAB:, Dmm=DMM1, stack=10000"
-#seq &Keithley2kDMM, "P=13LAB:, Dmm=DMM2, channels=22, model=2700, stack=10000"
+seq &Keithley2kDMM, "P=13LAB:, Dmm=DMM1, stack=10000"
+seq &Keithley2kDMM, "P=13LAB:, Dmm=DMM2, channels=22, model=2700, stack=10000"
 #seq &Keithley2kDMM, "P=13LAB:, Dmm=DMM3, channels=22, model=2700, stack=10000"
 #seq &seq_test, "pv1=13LAB:m1, pv2=13LAB:m2"
 
