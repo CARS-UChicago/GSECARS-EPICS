1cf3fb6c6f5440d386030d183ed0f023d5cb22a2
epics <>
2005-05-06 03:57:21 +0000

    New syntax for tyGSAsynInit; added other devices; changed syntax for new asyn version

--- iocBoot/ioc13lab/serial.cmd
+++ iocBoot/ioc13lab/serial.cmd
@@ -1,34 +1,38 @@
 # int tyGSAsynInit(char *port, int uart, int channel, int baud, char parity, int sbits,
-#                 int dbits, char handshake, char *eomstr)
-tyGSAsynInit("serial1",   0, 0, 9600,'N',1,8,'N',"\r")  /* SMART PC */
-tyGSAsynInit("serial2",   0, 1, 9600,'N',1,8,'N',"\r")  /* LAE 500 */
-tyGSAsynInit("serial3",   0, 2,19200,'E',1,8,'N',"\r")  /* MKS */
-#tyGSAsynInit("serial3",   0, 2,19200,'N',1,8,'N',"\r")  /* RSF715 */
-tyGSAsynInit("serial4",   0, 3,19200,'N',1,8,'N',"\r")  /* ACS MCB4B */
-tyGSAsynInit("serial5",   0, 4,19200,'N',1,8,'N',"\n")  /* Keithley 2000 */
-tyGSAsynInit("serial6",   0, 5,19200,'N',1,8,'N',"\n")  /* Keithley 2000 */
-tyGSAsynInit("serial7",   0, 6,38400,'N',1,8,'N',"\r")  /* MM4000 */
-tyGSAsynInit("serial8",   0, 7, 9600,'N',1,8,'N',"\r")  /* SRS 570 */
-tyGSAsynInit("serial9" ,  1, 0, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial10",  1, 1, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial11",  1, 2, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial12",  1, 3, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial13",  1, 4, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial14",  1, 5, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial15",  1, 6, 9600,'N',1,8,'N',"\r")  /* Unused */
-tyGSAsynInit("serial16",  1, 7, 9600,'N',1,8,'N',"\r")  /* Unused */
+#                 int dbits, char handshake, char *inputEos, char *outputEos)
+tyGSAsynInit("serial1",  "UART0", 0, 9600,'N',1,8,'N',"\r","\r")  /* SMART PC */
+tyGSAsynInit("serial2",  "UART0", 1, 9600,'N',1,8,'N',"\r","\r")  /* LAE 500 */
+tyGSAsynInit("serial3",  "UART0", 2,19200,'E',1,8,'N',"\r","\r")  /* MKS */
+#tyGSAsynInit("serial3", "UART0", 2,19200,'N',1,8,'N',"\r","\r")  /* RSF715 */
+tyGSAsynInit("serial4",  "UART0", 3,19200,'N',1,8,'N',"\r","\r")  /* ACS MCB4B */
+tyGSAsynInit("serial5",  "UART0", 4,19200,'N',1,8,'N',"\n","\r")  /* Keithley 2000 */
+tyGSAsynInit("serial6",  "UART0", 5,19200,'N',1,8,'N',"\n","\r")  /* Keithley 2000 */
+tyGSAsynInit("serial7",  "UART0", 6,38400,'N',1,8,'N',"\r","\r")  /* MM4000 */
+tyGSAsynInit("serial8",  "UART0", 7,19200,'N',1,8,'N',"\r\n","\r\n")  /* Verdi Laser */
+tyGSAsynInit("serial9",  "UART1", 0, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial10", "UART1", 1, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial11", "UART1", 2, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial12", "UART1", 3, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial13", "UART1", 4, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial14", "UART1", 5, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial15", "UART1", 6, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
+tyGSAsynInit("serial16", "UART1", 7, 9600,'N',1,8,'N',"\r","\r")  /* Unused */
 
 # Set up first 2 ports on Moxa box
 drvAsynIPPortConfigure("serial20", "164.54.160.50:4001", 0, 0)
 drvAsynIPPortConfigure("serial21", "164.54.160.50:4002", 0, 0)
 # Make these ports available from the iocsh command line
-#asynOctetConnect("serial20", "serial20", 0, "\r", "\r")
-#asynOctetConnect("serial21", "serial21", 0, "\r", "\r")
+#asynOctetConnect("serial20", "serial20", 0, 1, 80)
+#asynOctetConnect("serial21", "serial21", 0, 1, 80)
 
 # GPIB addresses: 1=Tektronix scope, 2=Keithley 2000, 3=Fluke meter
-asynOctetConnect("gpib1:1", "gpib1", 1, "", "\n", 1, 80)
-asynOctetConnect("gpib1:2", "gpib1", 2, "", "\n", 1, 80)
-asynOctetConnect("gpib1:3", "gpib1", 3, "\r", "\r", 1, 80)
+asynInterposeEosConfig("gpib1", 1, 1, 0)
+asynOctetSetInputEos("gpib1", 1, "\n")
+asynOctetConnect("gpib1:1", "gpib1", 1, 1, 80)
+asynOctetConnect("gpib1:2", "gpib1", 2, 1, 80)
+asynInterposeEosConfig("gpib1", 3, 1, 0)
+asynOctetSetInputEos("gpib1", 3, "\r")
+asynOctetConnect("gpib1:3", "gpib1", 3, 1, 80)
 
 # Debugging
 #asynSetTraceMask("gpib1",3,0xff)
@@ -96,9 +100,12 @@ dbLoadRecords("$(CARS)/CARSApp/Db/trajectoryScan.db", str, top)
 # Serial 8 Stanford Research Systems SR570 Current Preamplifier
 #dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13LAB:,A=A1,PORT=serial8")
 
+# Serial 8 is Verdi Laser for testing
+#dbLoadRecords("$(CARS)/CARSApp/Db/VerdiLaser.db", "P=13LAB:,R=Verdi1:,PORT=serial8")
+
 # Serial 8 XIA filter rack
 #dbLoadRecords("$(OPTICS)/opticsApp/Db/XIA_shutter.db", "P=13LAB:,S=filter1,ADDRESS=1,PORT=serial8")
 
 # GPIB 3 is Fluke multimeter
-#dbLoadRecords("$(CARS)/CARSApp/Db/Fluke_8842A.db", "P=13LAB:,M=Fluke1,PORT=gpib1,A=3")
+dbLoadRecords("$(CARS)/CARSApp/Db/Fluke_8842A.db", "P=13LAB:,M=Fluke1,PORT=gpib1,A=3")
 
