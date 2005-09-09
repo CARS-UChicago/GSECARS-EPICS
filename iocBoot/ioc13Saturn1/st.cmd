0ba507cb072b2145f36ee14fcf57294f6f9a8668
epics <>
2005-09-09 13:52:28 +0000

    Use macros for other modules

--- iocBoot/ioc13Saturn1/st.cmd
+++ iocBoot/ioc13Saturn1/st.cmd
@@ -1,7 +1,9 @@
+< envPaths
+
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from dxpApp
-dbLoadDatabase("../../dbd/dxp.dbd")
-dxp_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("../../dbd/CARS.dbd")
+CARS_registerRecordDeviceDriver(pdbbase)
 
 # Initialize the XIA software
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
@@ -20,23 +22,21 @@ DXPConfig("DXP1",  1)
 # DXP record
 # Execute the following line if you have a Vortex detector or 
 # another detector with a reset pre-amplifier
-dbLoadRecords("../../dxpApp/Db/dxp2x_reset.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxp2x_reset.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
 # Execute the following line if you have a Ketek detector or 
 # another detector with an RC pre-amplifier
-#dbLoadRecords("../../dxpApp/Db/dxp2x_rc.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
+#dbLoadRecords("$(DXP)/dxpApp/Db/dxp2x_rc.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
 
 # MCA record
-dbLoadRecords("../../../mca/mcaApp/Db/mca.db", "P=13Saturn1:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13Saturn1:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
 
 # Template to copy MCA ROIs to DXP SCAs
 dbLoadTemplate("roi_to_sca.substitutions")
 
 # Setup for save_restore
-< ../save_restore.cmd
+< ../save_restore_cygwin.cmd
 save_restoreSet_status_prefix("13Saturn1:")
-dbLoadRecords("../../../autosave/asApp/Db/save_restoreStatus.db", "P=13Saturn1:")
-set_pass0_restoreFile("auto_settings.sav")
-set_pass1_restoreFile("auto_settings.sav")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13Saturn1:")
 
 # Debugging flags
 #asynSetTraceMask DXP1 0 255
