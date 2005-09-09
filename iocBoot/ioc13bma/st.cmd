4f7c3155cc24defd6fa82010c522cae53e93153c
epics <>
2005-09-09 13:02:16 +0000

    New oms58Setup syntax

--- iocBoot/ioc13bma/st.cmd
+++ iocBoot/ioc13bma/st.cmd
@@ -80,11 +80,11 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13BMA:")
 ################################################################################
 # Setup device/driver support addresses, interrupt vectors, etc.
 
-# OMS VME58 driver setup parameters: 
-#     (1)cards, (2)axis per card, (3)base address(short, 4k boundary), 
-#     (4)interrupt vector (0=disable or  64 - 255), (5)interrupt level (1 - 6),
-#     (6)motor task polling rate (min=1Hz,max=60Hz)
-oms58Setup(4, 8, 0x4000, 190, 5, 10)
+# OMS VME58 driver setup parameters:
+#     (1)cards, (2)base address(short, 4k boundary),
+#     (3)interrupt vector (0=disable or  64 - 255), (4)interrupt level (1 - 6),
+#     (5)motor task polling rate (min=1Hz,max=60Hz)
+oms58Setup(4, 0x4000, 190, 5, 10)
 
 # dbrestore setup
 sr_restore_incomplete_sets_ok = 1
