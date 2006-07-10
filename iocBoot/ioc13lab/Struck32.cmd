eece05ea204ba531c780fda04fcd9ae49b82b5fd
epics <>
2006-07-10 19:55:56 +0000

    Fix path

--- iocBoot/ioc13lab/Struck32.cmd
+++ iocBoot/ioc13lab/Struck32.cmd
@@ -1,5 +1,5 @@
 STR7201Setup(1,0x90000000,220,6)
 # STR7201Config(card, maxSignals, maxChans, ch1RefEnable)
 STR7201Config(0, 32, 2048, 1)
-dbLoadRecords("../../../mca/mcaApp/Db/Struck32.db","P=13LAB:str:")
+dbLoadRecords("$(MCA)/mcaApp/Db/Struck32.db","P=13LAB:str:")
 dbLoadTemplate("Struck32.template")
