615a1afed2a2ea64b6e0e4e5e6def47526d0d86e
epics <>
2006-09-14 04:15:14 +0000

    New file for serial

---
+++ iocBoot/ioc13test/serial.cmd
@@ -0,0 +1,18 @@
+# BEGIN serial.cmd ------------------------------------------------------------
+
+# Set up port on Digi box
+
+#drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
+#                        noProcessEos)
+drvAsynIPPortConfigure("serial1", "gsets5:2101", 0, 0, 0)
+asynOctetConnect("serial1", "serial1")
+asynOctetSetInputEos("serial1",0,"\r")
+asynOctetSetOutputEos("serial1",0,"\r")
+
+# Load asynRecord records on all ports
+dbLoadTemplate("asynRecord.substitutions")
+
+# IPG laser
+dbLoadRecords("$(CARS)/CARSApp/Db/IPG_YLR_laser.db","P=13TEST:,R=Laser1,PORT=serial1")
+
+# END serial.cmd --------------------------------------------------------------
