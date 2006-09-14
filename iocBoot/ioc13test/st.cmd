7a603e0ebaf7f0b759bc428a8d1e2519439f370b
epics <>
2006-09-14 04:14:55 +0000

    Lots of changes

--- iocBoot/ioc13test/st.cmd
+++ iocBoot/ioc13test/st.cmd
@@ -1,13 +1,51 @@
-# Allocate 96MB of memory so we load everything else into low memory
-mem = malloc(1024*1024*96)
-< cdCommands
-< ../nfsCommandsGSE
-cd topbin
-ld < CARSApp.munch
-cd startup
-sscanRecordDebug=20
-dbLoadDatabase("$(CARS)/dbd/CARSVX.dbd")
-CARSVX_registerRecordDeviceDriver(pdbbase)
-dbLoadRecords("$(STD)/stdApp/Db/scan.db.CA", "P=13LAB:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+errlogInit(5000)
+< envPaths
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build from CARS
+dbLoadDatabase("../../dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+< serial.cmd
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=13TEST:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+
+# Free-standing user string/number calculations (sCalcout records)
+dbLoadRecords("$(CALC)/calcApp/Db/userStringCalcs10.db", "P=13TEST:")
+
+# Free-standing user transforms (transform records)
+dbLoadRecords("$(CALC)/calcApp/Db/userTransforms10.db", "P=13TEST:")
+
+# Miscellaneous PV's, such as burtResult
+dbLoadRecords("$(STD)/stdApp/Db/misc.db", "P=13TEST:")
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("13TEST:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13TEST:")
+
 iocInit
-free(mem)
+
+# save positions every five seconds
+create_monitor_set("auto_positions.req", 5)
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req", 30)
+
+### Start the saveData task.
+# saveData_MessagePolicy
+# 0: wait forever for space in message queue, then send message
+# 1: send message only if queue is not full
+# 2: send message only if queue is not full and specified time has passed (SetCptWait()
+#    sets this time.)
+# 3: if specified time has passed, wait for space in queue, then send message
+# else: don't send message
+#debug_saveData = 2
+#saveData_MessagePolicy = 2
+#saveData_SetCptWait_ms(100)
+#saveData_Init("saveDataExtraPVs.req", "P=13TEST:")
+#saveData_PrintScanInfo("13TEST:scan1")
+
