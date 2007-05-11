38f150de92ad880e06deda30cb44fec217b10a7a
epics <>
2007-05-11 00:07:25 +0000

    Added comments

--- iocBoot/ioc13bmc/Struck8.cmd
+++ iocBoot/ioc13bmc/Struck8.cmd
@@ -1,4 +1,6 @@
+# STR7201Setup(int maxCards, int baseAddress, int intVector, int intLevel);
 STR7201Setup(1,0x90000000,220,6)
-STR7201Config(0, 8, 2048, 1)
+# STR7201Config(int card, int maxSignals, int maxChans, int ch1RefEnable, int softAdvance);
+STR7201Config(0, 8, 2048, 1, 0)
 dbLoadRecords("$(MCA)/mcaApp/Db/Struck8.db","P=13BMC:str:")
 dbLoadTemplate("Struck8.template")
