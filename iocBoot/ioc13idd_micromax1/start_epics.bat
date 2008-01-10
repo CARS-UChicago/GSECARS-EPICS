5f3116ea68bb62687f17f1c0b0283ce5a7b630b1
epics <>
2008-01-10 19:52:11 +0000

    New path

--- iocBoot/ioc13idd_micromax1/start_epics.bat
+++ iocBoot/ioc13idd_micromax1/start_epics.bat
@@ -5,11 +5,11 @@ rem Edit it if WinSpec rather than WinView should be started
 start WinView
 
 rem Start the Roper server
-start J:\epics\devel\ccd\1-7beta\bin\win32-x86\roperServer.exe
+start J:\epics\support\ccd\1-7beta\bin\win32-x86\roperServer.exe
 
 rem Start MEDM
 start medm -x -macro "P=13IDDMicroMax1:, C=det1:" ccd.adl
 
 rem Start IOC
-J:\epics\devel\ccd\1-7beta\bin\win32-x86\roperCCDApp.exe st.cmd
+J:\epics\support\ccd\1-7beta\bin\win32-x86\roperCCDApp.exe st.cmd
 
