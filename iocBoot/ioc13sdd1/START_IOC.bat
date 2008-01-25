4b39c688f3656921c7afeb1e04d8c910fbefd732
epics <>
2008-01-25 18:58:47 +0000

    Change to 4element, change application path

--- iocBoot/ioc13sdd1/START_IOC.bat
+++ iocBoot/ioc13sdd1/START_IOC.bat
@@ -2,7 +2,7 @@ REM Tell medm where to look for .adl files.  Edit the following line, or comment
 REM out if the environment variable is already set
 REM set EPICS_DISPLAY_PATH=C:\epics_adls
 REM Note that we must use the Windows "start" command or medm won't find X11 dlls
-start medm -x -macro "P=13SDD1:, D=dxp, M=mca" 16element_dxp.adl
+start medm -x -macro "P=13SDD1:, D=dxp, M=mca" 4element_dxp.adl
 REM Put Cygwin in the path so the EPICS application can find cygwin1.dll
 PATH=c:\cygwin\bin
-..\..\bin\cygwin-x86\dxpApp.exe st.cmd
+J:\epics\support\dxp\2-8beta\bin\cygwin-x86\dxpApp.exe 4element.cmd
