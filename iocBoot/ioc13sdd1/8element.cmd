b9d98ac5a852cfc1c8fbb7cebb2bc6007544ebb3
epics <>
2008-01-25 18:58:14 +0000

    Use IOCSH version of save_restore.cmd

--- iocBoot/ioc13sdd1/8element.cmd
+++ iocBoot/ioc13sdd1/8element.cmd
@@ -6,7 +6,7 @@ dbLoadDatabase("$(DXP)/dbd/dxp.dbd")
 dxp_registerRecordDeviceDriver(pdbbase)
 
 # Setup for save_restore
-< ../save_restore.cmd
+< ../save_restore_IOCSH.cmd
 save_restoreSet_status_prefix("13SDD1:")
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13SDD1:")
 set_pass0_restoreFile("auto_settings8.sav")
