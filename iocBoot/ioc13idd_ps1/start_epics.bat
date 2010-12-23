362dda554d0decb5b082e3daab0b79d7e1b36a99
epics <>
2010-12-23 23:32:26 +0000

    Converted to areaDetector 1-5

--- iocBoot/ioc13idd_ps1/start_epics.bat
+++ iocBoot/ioc13idd_ps1/start_epics.bat
@@ -1,8 +1,10 @@
 rem This batch file starts the software for controlling Prosilica cameras from EPICS
 
 rem Start MEDM
-rem start medm -x -macro "P=13IDDPS2:, R=cam2:" ADBase.adl
+start medm -x -macro "P=13IDDPS1:,R=cam1:,I=image1:,NETCDF=netCDF1:,JPEG=JPEG1:,TIFF=TIFF1:,ROI=ROI1:" prosilica.adl
 
 rem Start IOC
-J:\epics\devel\areaDetector\1-4\bin\win32-x86\prosilicaApp st.cmd
+J:\epics\support\areaDetector\1-5\bin\win32-x86\prosilicaApp st.cmd
+
+pause
 
