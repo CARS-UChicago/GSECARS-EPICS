f79a2eee9ec62d0ed0ff70c59612eb7d08bca28d
epics <>
2010-12-23 22:57:40 +0000

    Updated to dxp 2-10

--- iocBoot/ioc13sdd1/START_IOC.bat
+++ iocBoot/ioc13sdd1/START_IOC.bat
@@ -5,4 +5,6 @@ REM Note that we must use the Windows "start" command or medm won't find X11 dll
 start medm -x -macro "P=13SDD1:, D=dxp, M=mca" 4element_dxp.adl
 REM Put Cygwin in the path so the EPICS application can find cygwin1.dll
 PATH=c:\cygwin\bin
-J:\epics\support\dxp\2-8beta\bin\cygwin-x86\dxpApp.exe 4element.cmd
+J:\epics\support\dxp\2-10\bin\cygwin-x86\dxpApp.exe 4element.cmd
+pause
+
