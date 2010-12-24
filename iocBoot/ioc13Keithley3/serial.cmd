2671bf622edc13b978bcd062302e71ee7ec4c592
epics <>
2010-12-24 00:08:57 +0000

    New file

---
+++ iocBoot/ioc13Keithley3/serial.cmd
@@ -0,0 +1,17 @@
+# BEGIN serial.cmd ------------------------------------------------------------
+
+# Set up port on Digi box
+
+#drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
+#                        noProcessEos)
+#drvAsynIPPortConfigure("serial1", "164.54.160.156:1394", 0, 0, 0)
+drvAsynIPPortConfigure("serial1", "164.54.160.154:2001", 0, 0, 0)
+asynOctetConnect("serial1", "serial1")
+
+# Load asynRecord records on all ports
+dbLoadTemplate("asynRecord.template")
+
+# Keithley 2700 DMM
+dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13Keithley3:,Dmm=DMM1,PORT=serial1")
+
+# END serial.cmd --------------------------------------------------------------
