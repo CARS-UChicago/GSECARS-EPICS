528986bdf56b14ef6a5df7c5f002db43e68ba46d
epics <>
2010-12-24 00:01:18 +0000

    Changed comment

--- iocBoot/ioc13bmc/Struck8.cmd
+++ iocBoot/ioc13bmc/Struck8.cmd
@@ -2,7 +2,7 @@
 STR7201Setup(1,0x90000000,220,6)
 # STR7201Config(int card, int maxSignals, int maxChans, int ch1RefEnable, int softAdvance);
 #STR7201Config(0, 8, 2048, 1, 0)
-# Change to use the 1MHz clock in channel 1
+# Change to use the 1MHz clock in channel 1 and disable softAdvance to it starts counting immediately
 STR7201Config(0, 8, 2048, 0, 0)
 dbLoadRecords("$(MCA)/mcaApp/Db/Struck8.db","P=13BMC:str:")
 dbLoadTemplate("Struck8.template")
