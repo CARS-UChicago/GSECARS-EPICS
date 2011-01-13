f2674b78b8d0a8e5037727d09903d8f4a6147fb0
epics <>
2011-01-13 20:20:12 +0000

    New file

---
+++ iocBoot/ioc13ide/softGlue.cmd
@@ -0,0 +1,52 @@
+# BEGIN softGlue.cmd ----------------------------------------------------------
+# This must run after industryPack.cmd
+
+#devAsynSoftGlueDebug=1
+#drvIP_EP201Debug=1
+
+card = 1
+slot = 0
+
+# Write content to the FPGA.    This command will fail if the FPGA already has
+# content loaded, as it will after a soft reboot.  To load new FPGA content,
+# you must power cycle the ioc.
+# initIP_EP200_FPGA(ushort_t carrier, ushort_t slot, char *filename)
+
+# Standard softGlue 2.1
+initIP_EP200_FPGA(card, slot, "$(SOFTGLUE)/softGlueApp/Db/SoftGlue_2_1.hex")
+# SoftGlue 2.0 with octupole additions (shift registers)
+#initIP_EP200_FPGA(card, slot, "$(SOFTGLUE)/softGlueApp/Db/SoftGlue_2_0_1_Octupole_0_0.hex")
+
+# Each instance of a fieldIO_registerSet component is initialized as follows:
+#
+#int initIP_EP201(const char *portName, ushort_t carrier, ushort_t slot,
+#	int msecPoll, int dataDir, int sopcOffset, int interruptVector,
+#	int risingMask, int fallingMask)
+# Note that while the addresses and interrupt vector look adjustable, they
+# really are not.  Also note that the user can overwrite risingMask and
+# fallingMask, and probably has these registers autosaved.
+#
+# dataDir: bits 0 and 8 are the only ones that matter.  The meaning depends on
+# which IP module is in use.
+#     IP-EP201:
+#         If bit 0 is set, I/O bits 0...7 are outputs, else they're inputs.
+#         If bit 8 is set, I/O bits 8...15 are outputs, else they're inputs.
+
+# bits 1-16 are inputs
+initIP_EP201("SGIO_1",card,slot,1000000,0x0000,0x800000,0x90,0x00,0x00)
+# bits 33-48 are outputs
+initIP_EP201("SGIO_2",card,slot,1000000,0x0101,0x800010,0x91,0x00,0x00)
+initIP_EP201("SGIO_3",card,slot,1000000,0x0101,0x800020,0x92,0x00,0x00)
+
+# All instances of a single-register component are initialized with a single
+# call, as follows:
+#
+#initIP_EP201SingleRegisterPort(const char *portName, ushort_t carrier,
+#	ushort_t slot)
+#
+# For example:
+# initIP_EP201SingleRegisterPort("SOFTGLUE", 0, 2)
+initIP_EP201SingleRegisterPort("SOFTGLUE", card, slot)
+
+dbLoadTemplate("softGlue.substitutions")
+# END softGlue.cmd ------------------------------------------------------------
---
+++ iocBoot/ioc13ide/st.cmd
@@ -0,0 +1,114 @@
+# vxWorks startup file
+< cdCommands
+< ../nfsCommandsGSE
+
+cd topbin
+ld < CARSApp.munch
+
+# Increase size of errlog buffer
+errlogInit(20000)
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build.
+dbLoadDatabase("$(CARS)/dbd/CARSVX.dbd")
+CARSVX_registerRecordDeviceDriver(pdbbase)
+
+cd startup
+< industryPack.cmd
+< serial.cmd
+
+# Set debugging flags
+mcaRecordDebug = 0
+save_restoreDebug = 0
+# Asyn XPS driver debug variable 0-5
+asynXPSC8Debug = 0
+
+
+#dbLoadTemplate("motors.template")
+
+# SIS3820 MCS as 32-channel multi-element detector and scaler
+<SIS3820_32.cmd
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=13IDE:,MAXPTS1=2000,MAXPTS2=500,MAXPTS3=20,MAXPTS4=5,MAXPTSH=10")
+
+# A set of scan parameters for each positioner.  This is a convenience
+# for the user.  It can contain an entry for each scannable thing in the
+# crate.
+dbLoadTemplate("scanParms.template")
+
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_1,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 0)")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_2,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 1)")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_3,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 2)")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_4,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 3)")
+# added 2-05 for split ion chmaber
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_5,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 4)")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDE:,M=mip330_6,DTYP=asynMCA,NCHAN=2048,INP=@asyn(Ip330Sweep1 5)")
+
+# Miscellaneous PV's, such as burtResult
+dbLoadRecords("$(STD)/stdApp/Db/misc.db","P=13IDE:")
+
+# Experiment description
+dbLoadRecords("$(CARS)/CARSApp/Db/experiment_info.db","P=13IDE:")
+
+# vxWorks statistics
+dbLoadTemplate("vxStats.substitutions")
+
+< ../save_restore.cmd
+save_restoreSet_status_prefix("13IDE:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13IDE:")
+
+# Setup device/driver support addresses, interrupt vectors, etc.
+
+################################################################################
+# OMS VME58 driver setup parameters:
+#     (1)cards, (2)base address(short, 4k boundary),
+#     (3)interrupt vector (0=disable or  64 - 255), (4)interrupt level (1 - 6),
+#     (5)motor task polling rate (min=1Hz,max=60Hz)
+oms58Setup(9, 0x4000, 190, 5, 10)
+################################################################################
+
+
+################################################################################
+
+# dbrestore setup
+sr_restore_incomplete_sets_ok = 1
+#reboot_restoreDebug=5
+
+iocInit
+
+# Sleep for 10 seconds to let iocInit do its thing and see any messages
+# before going on
+taskDelay(600)
+
+### Start up the autosave task and tell it what to do.
+# The task is actually named "save_restore".
+# (See also, 'initHooks' above, which is the means by which the values that
+# will be saved by the task we're starting here are going to be restored.
+#
+# save positions every five seconds
+create_monitor_set("auto_positions.req",5,"P=13IDE:")
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=13IDE:")
+
+### Start the saveData task.
+# saveData_MessagePolicy
+# 0: wait forever for space in message queue, then send message
+# 1: send message only if queue is not full
+# 2: send message only if queue is not full and specified time has passed (SetCptWait()
+#    sets this time.)
+# 3: if specified time has passed, wait for space in queue, then send message
+# else: don't send message
+#debug_saveData = 2
+saveData_MessagePolicy = 2
+saveData_SetCptWait_ms(10)
+saveData_Init("saveDataExtraPVs.req", "P=13IDE:")
+#saveData_PrintScanInfo("13IDE:scan1")
+
+seq &Keithley2kDMM, "P=13IDE:, Dmm=DMM1"
