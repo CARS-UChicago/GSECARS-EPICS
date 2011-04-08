157efe079bd3d4f6ea5eef78264c92ba42e3b35c
epics <>
2011-04-08 15:47:52 +0000

    Many changes as serial ports were configured

--- iocBoot/ioc13ide/serial.cmd
+++ iocBoot/ioc13ide/serial.cmd
@@ -1,14 +1,14 @@
 # tyGSAsynInit(char *port, char *moduleName, int channel, int baud,
 #              char parity, int sbits, int dbits, char handshake,
 #              char *inputEos, char *outputEos)
-tyGSAsynInit("serial1", "UART0", 0,  9600,'N',2,8,'N',"\r","\r") /* SRS570 */
-tyGSAsynInit("serial2", "UART0", 1,  9600,'N',2,8,'N',"\r","\r") /* SRS570 */
-tyGSAsynInit("serial3", "UART0", 2,  9600,'N',2,8,'N',"\r","\r") /* SRS570 */
-tyGSAsynInit("serial4", "UART0", 3,  9600,'N',2,8,'N',"\r","\r") /* SRS570 */
-tyGSAsynInit("serial5", "UART0", 4, 19200,'N',1,8,'N',"\r","\r") /* SRS570 */
+tyGSAsynInit("serial1", "UART0", 0,  9600,'N',2,8,'N',"\r","\r")   /* SRS570 */
+tyGSAsynInit("serial2", "UART0", 1,  9600,'N',2,8,'N',"\r","\r")   /* SRS570 */
+tyGSAsynInit("serial3", "UART0", 2,  9600,'N',2,8,'N',"\r","\r")   /* SRS570 */
+tyGSAsynInit("serial4", "UART0", 3,  9600,'N',2,8,'N',"\r","\r")   /* SRS570 */
+tyGSAsynInit("serial5", "UART0", 4,  9600,'N',2,8,'N',"\r","\r")   /* SRS570 */
 tyGSAsynInit("serial6", "UART0", 5, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
-tyGSAsynInit("serial7", "UART0", 6,  9600,'N',2,8,'N',"\r","\r") /* Pelco CM6700 */
-tyGSAsynInit("serial8", "UART0", 7, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
+tyGSAsynInit("serial7", "UART0", 6, 19200,'N',1,8,'N',"\r\n","\n") /* Keithley 2000 */
+tyGSAsynInit("serial8", "UART0", 7,  9600,'N',2,8,'N',"\r","\r")   /* Pelco CM6700 */
 
 # Load asyn records on all serial ports
 dbLoadTemplate("asynRecord.template")
@@ -18,11 +18,11 @@ dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A1,PORT=serial1")
 dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A2,PORT=serial2")
 dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A3,PORT=serial3")
 dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A4,PORT=serial4")
-# dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A5,PORT=serial5")
-# dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A6,PORT=serial6")
+dbLoadRecords("$(IP)/ipApp/Db/SR570.db", "P=13IDE:,A=A5,PORT=serial5")
+
 # Serial 7 is Pelco CM6700 video switch
 dbLoadTemplate("Pelco_CM6700.substitutions")
 
 dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13IDE:,Dmm=DMM1,PORT=serial6")
-dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13IDE:,Dmm=DMM2,PORT=serial8")
+dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=13IDE:,Dmm=DMM2,PORT=serial7")
 
