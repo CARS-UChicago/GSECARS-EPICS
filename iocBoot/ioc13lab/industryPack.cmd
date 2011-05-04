167f5ff0be868cb1585367ce1bc46fe02481922e
epics <>
2011-05-04 21:37:57 +0000

    Changed second IP carrier to TVME200

--- iocBoot/ioc13lab/industryPack.cmd
+++ iocBoot/ioc13lab/industryPack.cmd
@@ -1,4 +1,11 @@
-ipacAddVIPC616_01("0x3000,0xa0000000")
+# The second carrier in our system is a TEWS TVME200
+# The argument to ipacAddTVME200 is the values of the 6 switches on the board
+# In thise case 30 = base address 3000
+#                2 = interrupt mapping 4, 5, 2, 1, 4, 5, 2, 1
+#                F = A32 address space, 8MB per slot
+#               A0 = A0000000 base address in A32 space
+ipacAddTVME200("302FA0") 
+#ipacAddVIPC616_01("0x3000,0xa0000000")
 ipacAddVIPC616_01("0x3400,0xa2000000")
 
 ipacReport(2)
@@ -65,9 +72,10 @@ dbLoadTemplate "DAC.template"
 #               0 to 15 (differential)
 # lastChan    = last channel to be digitized
 # intVec        Interrupt vector
-initIp330("Ip330_1",0,2,"D","-10to10",0,15,120)
+initIp330("Ip330_1",0,2,"D","-5to5",0,15,120)
 
-#asynSetTraceMask "Ip330_1",0,0x11
+#asynSetTraceIOMask "Ip330_1",0,0x2
+#asynSetTraceMask "Ip330_1",0,0x9
 
 # int configIp330(
 #   const char *portName,
