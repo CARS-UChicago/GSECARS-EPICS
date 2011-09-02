d87062b5f08c4149a672c1d5051be95eb6da544b
epics <>
2011-09-02 22:05:12 +0000

    Added ch1 ref enable

--- iocBoot/ioc13ide/SIS3820_2.cmd
+++ iocBoot/ioc13ide/SIS3820_2.cmd
@@ -6,10 +6,11 @@ mcaSIS3820Setup(0xA8000000, 224, 6)
 #                  signals,
 #                  input mode,
 #                  output mode,
-#                  lne prescale)
-mcaSIS3820Config("mcaSIS3820/1", 0, 2000000, 2, 2, 0, 1000000)
+#                  lne prescale
+#                  ch1 ref enable)
+mcaSIS3820Config("mcaSIS3820/1", 0, 2000000, 2, 2, 0, 1000000, 1)
 dbLoadTemplate("SIS3820_2.substitutions")
 
 #asynSetTraceIOMask("mcaSIS3820/1",0,2)
-#asynSetTraceMask  ("mcaSIS3820/1",0,255)
+#asynSetTraceMask  ("mcaSIS3820/1",0,0x11)
 
