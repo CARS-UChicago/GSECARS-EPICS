ddec4bf9cadb661506adad19a966e367f3c76bce
epics <>
2011-09-02 22:26:53 +0000

    Changes for DPX R3-0

--- iocBoot/ioc13sdd1/4element.cmd
+++ iocBoot/ioc13sdd1/4element.cmd
@@ -5,6 +5,13 @@
 dbLoadDatabase("$(DXP)/dbd/dxp.dbd")
 dxp_registerRecordDeviceDriver(pdbbase)
 
+
+# The default callback queue in EPICS base is only 2000 bytes. 
+# The dxp detector system needs this to be larger to avoid the error message: 
+# "callbackRequest: cbLow ring buffer full" 
+# right after the epicsThreadSleep at the end of this script
+callbackSetQueueSize(4000)
+
 # Setup for save_restore
 < ../save_restore_IOCSH.cmd
 save_restoreSet_status_prefix("13SDD1:")
@@ -12,20 +19,24 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13SDD1:")
 set_pass0_restoreFile("auto_settings4.sav")
 set_pass1_restoreFile("auto_settings4.sav")
 
-# Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
+# Set logging level (1=ERROR, 2=WARNING, 3=INFO, 4=DEBUG)
 xiaSetLogLevel(2)
 xiaInit("xmap4.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, ndetectors, ngroups, pollFrequency)
-DXPConfig("DXP1",  4, 1, 100)
+# DXPConfig(serverName, ndetectors, maxBuffers, maxMemory)
+NDDxpConfig("DXP1",  4, -1, -1)
 
-dbLoadTemplate("4element.template")
+dbLoadTemplate("4element.substitutions")
 
-#xiaSetLogLevel(4)
+# Create a netCDF file saving plugin
+NDFileNetCDFConfigure("DXP1NetCDF", 100, 0, "DXP1", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileNetCDF.template","P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
+
 #asynSetTraceMask DXP1 0 255
-#asynSetTraceIOMask DXP1 0 2
-#var dxpRecordDebug 10
+asynSetTraceIOMask DXP1 0 2
 
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
@@ -34,7 +45,7 @@ dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=13SDD1:,MAXPTS1=2000,MAXPTS2=100
 
 iocInit
 
-seq dxpMED, "P=13SDD1:, DXP=dxp, MCA=mca, N_DETECTORS=4"
+seq dxpMED, "P=13SDD1:, DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=16"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
@@ -43,3 +54,7 @@ create_monitor_set("auto_settings4.req", 30, "P=13SDD1:")
 ### Start the saveData task.
 saveData_Init("saveData.req", "P=13SDD1:")
 
+# Sleep for 10 seconds to let initialization complete and then turn on AutoApply and do Apply manually once
+epicsThreadSleep(10.)
+dbpf("13SDD1:AutoApply", "Yes")
+dbpf("13SDD1:Apply", "1")
--- iocBoot/ioc13sdd1/8element.cmd
+++ iocBoot/ioc13sdd1/8element.cmd
@@ -2,30 +2,35 @@
 
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from dxpApp
-dbLoadDatabase("$(DXP)/dbd/dxp.dbd")
-dxp_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("../../dbd/CARS.dbd")
+CARS_registerRecordDeviceDriver(pdbbase)
 
 # Setup for save_restore
-< ../save_restore_IOCSH.cmd
+< ../save_restore.cmd
 save_restoreSet_status_prefix("13SDD1:")
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13SDD1:")
 set_pass0_restoreFile("auto_settings8.sav")
 set_pass1_restoreFile("auto_settings8.sav")
 
-# Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
+# Set logging level (1=ERROR, 2=WARNING, 3=INFO, 4=DEBUG)
 xiaSetLogLevel(2)
 xiaInit("xmap8.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, ndetectors, ngroups, pollFrequency)
-DXPConfig("DXP1",  8, 1, 100)
+# DXPConfig(serverName, ndetectors, maxBuffers, maxMemory)
+NDDxpConfig("DXP1",  8, -1, -1)
 
-dbLoadTemplate("8element.template")
+dbLoadTemplate("8element.substitutions")
+
+# Create a netCDF file saving plugin
+NDFileNetCDFConfigure("DXP1NetCDF", 100, 0, "DXP1", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileNetCDF.template","P=13SDD1:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
 
 #xiaSetLogLevel(4)
 #asynSetTraceMask DXP1 0 255
-#asynSetTraceIOMask DXP1 0 2
-#var dxpRecordDebug 10
+asynSetTraceIOMask DXP1 0 2
 
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
@@ -34,7 +39,7 @@ dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=13SDD1:,MAXPTS1=2000,MAXPTS2=100
 
 iocInit
 
-seq dxpMED, "P=13SDD1:, DXP=dxp, MCA=mca, N_DETECTORS=8"
+seq dxpMED, "P=13SDD1:, DXP=dxp, MCA=mca, N_DETECTORS=8, N_SCAS=16"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
@@ -42,4 +47,11 @@ create_monitor_set("auto_settings8.req", 30, "P=13SDD1:")
 
 ### Start the saveData task.
 saveData_Init("saveData.req", "P=13SDD1:")
+
+# Sleep for 10 seconds to let initialization complete and then turn on AutoApply and do Apply manually once
+epicsThreadSleep(10.)
+dbpf("13SDD1:AutoApply", "1")
+dbpf("13SDD1:Apply", "1")
+# Seems to be necessary to write mapping mode parameters, otherwise initial values are wrong?
+dbpf("13SDD1:PixelsPerRun.PROC", "1");
 
