b6f623ede1902cb5aa9ff99d0b8ee3a6b6329f17
epics <>
2011-09-02 21:15:26 +0000

    Disabled software channel advance

--- iocBoot/ioc13idc/Struck8.cmd
+++ iocBoot/ioc13idc/Struck8.cmd
@@ -2,9 +2,13 @@
 STR7201Setup(1,0x90000000,220,6)
 # STR7201Config(int card, int maxSignals, int maxChans,
 #               int 1=enable internal 25MHZ clock,
-#               int 1=enable initial software channel advance in MCS external advance mode)
+#               int 0=disable initial software channel advance in MCS external advance mode)
 # NOTE: We are not using the 25MHz internal clock because it was overflowing on really long
 # count times.  Instead we use the 1MHz clock from the Nova V/F converter.
-STR7201Config(0, 8, 2048, 0, 1)
+STR7201Config(0, 8, 2048, 0, 0)
 dbLoadRecords("$(MCA)/mcaApp/Db/Struck8.db","P=13IDC:str:")
 dbLoadTemplate("Struck8.template")
+
+
+### Scalers: Struck/SIS as simple scaler 
+dbLoadRecords("$(MCA)/mcaApp/Db/STR7201scaler.db","P=13IDC:,S=scaler2,C=0")
