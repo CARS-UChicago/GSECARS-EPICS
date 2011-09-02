11c4587757990bbf87b4a0f5a5b0d8382182d551
epics <>
2011-09-02 20:56:42 +0000

    Changes for new version of AD

--- iocBoot/ioc13lab_roper1/start_epics.bat
+++ iocBoot/ioc13lab_roper1/start_epics.bat
@@ -1,4 +1,4 @@
-start medm -x -macro "P=13LABRP1:, R=cam1:, I=image1:, ROI=ROI1:, NETCDF=netCDF1:, TIFF=TIFF1:, JPEG=JPEG1:, NEXUS=Nexus1:" Roper.adl
-J:\epics\support\areaDetector\1-5\bin\win32-x86\roperApp st.cmd
+start medm -x -macro "P=13LABRP1:, R=cam1:" Roper.adl
+J:\epics\support\areaDetector\bin\win32-x86\roperApp st.cmd
 pause
 
