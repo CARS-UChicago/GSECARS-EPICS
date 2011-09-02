addca99ef63d5f5682d092d135843d485e895134
epics <>
2011-09-02 21:49:42 +0000

    Minor change

--- iocBoot/ioc13Keithley2/serial.cmd
+++ iocBoot/ioc13Keithley2/serial.cmd
@@ -6,7 +6,9 @@
 #                        noProcessEos)
 drvAsynIPPortConfigure("serial1", "164.54.160.156:1394", 0, 0, 0)
 #drvAsynIPPortConfigure("serial1", "164.54.160.154:2001", 0, 0, 0)
-asynOctetConnect("serial1", "serial1")
+
+asynSetTraceIOMask("serial1",0,2)
+#asynSetTraceMask("serial1",0,255)
 
 # Load asynRecord records on all ports
 dbLoadTemplate("asynRecord.template")
