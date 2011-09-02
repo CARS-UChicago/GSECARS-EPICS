0c59328fcaece11742b39695dfe832fdeee45d68
epics <>
2011-09-02 22:42:23 +0000

    Start medm

--- iocBoot/ioc13idc_ps1/start_epics.bat
+++ iocBoot/ioc13idc_ps1/start_epics.bat
@@ -1,3 +1,3 @@
-# start medm -x -macro "P=13IDCPS1:, R=cam1:" prosilica.adl
+start medm -x -macro "P=13IDCPS1:, R=cam1:" prosilica.adl
 ..\..\..\areaDetector\bin\win32-x86\prosilicaApp st.cmd.win32
 
