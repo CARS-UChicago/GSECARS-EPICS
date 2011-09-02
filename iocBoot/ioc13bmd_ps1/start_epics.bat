b073bbef5fa4a09d16285369fe4bf114e3cdcc10
epics <>
2011-09-02 21:32:49 +0000

    Changed for tomo_collect and for AD R1-6

--- iocBoot/ioc13bmd_ps1/start_epics.bat
+++ iocBoot/ioc13bmd_ps1/start_epics.bat
@@ -2,9 +2,12 @@ rem This batch file starts the software for controlling Prosilica cameras from E
 
 rem Start MEDM
 rem start medm -x -macro "P=13BMDPS1:, R=cam1:" ADBase.adl
+start medm -x -macro "P=13BMDPS1:,R=cam1:" prosilica.adl
+
+rem Start TC window
+start medm -x -macro "P=13BMDPS1: ,R=TC:" test.adl
 
 rem Start IOC
-J:\epics\support\areaDetector\1-5\bin\win32-x86\prosilicaApp st.cmd
+J:\epics\support\areaDetector\bin\win32-x86\prosilicaApp st.cmd
 
-pause
 
