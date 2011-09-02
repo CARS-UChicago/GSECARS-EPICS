01e531896e464bdaf15d2bfb31425c19d367c42a
epics <>
2011-09-02 21:27:16 +0000

    Changed for tomo_collect and AD R1-6

--- iocBoot/ioc13bmd_roper1/start_epics.bat
+++ iocBoot/ioc13bmd_roper1/start_epics.bat
@@ -1,4 +1,8 @@
-start medm -x -macro "P=13BMDRP1:, R=cam1:, I=image1:, ROI=ROI1:, NETCDF=netCDF1:, TIFF=TIFF1:, JPEG=JPEG1:, NEXUS=Nexus1:" Roper.adl
-J:\epics\support\areaDetector\1-5\bin\win32-x86\roperApp st.cmd
+start medm -x -macro "P=13BMDRP1:, R=cam1:" Roper.adl
+
+rem Start TC window
+start medm -x -macro "P=13BMDRP1: ,R=TC:" test.adl
+
+J:\epics\support\areaDetector\bin\win32-x86\roperApp st.cmd
 pause
 
