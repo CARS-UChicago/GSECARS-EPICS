d01692c8fe10acfe906cc53e07fb07ac1c21d22d
epics <>
2011-09-24 21:25:57 +0000

    Don't start second medm for TomoCollect

--- iocBoot/ioc13bmd_roper1/start_epics.bat
+++ iocBoot/ioc13bmd_roper1/start_epics.bat
@@ -1,8 +1,5 @@
 start medm -x -macro "P=13BMDRP1:, R=cam1:" Roper.adl
 
-rem Start TC window
-start medm -x -macro "P=13BMDRP1: ,R=TC:" test.adl
-
 J:\epics\support\areaDetector\bin\win32-x86\roperApp st.cmd
 pause
 
