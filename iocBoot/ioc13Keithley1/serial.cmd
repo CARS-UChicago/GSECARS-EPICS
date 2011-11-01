7d25440cb68297856157f5f851b6e4c6619642da
epics <>
2011-11-01 19:07:36 +0000

    New IP

--- iocBoot/ioc13Keithley1/serial.cmd
+++ iocBoot/ioc13Keithley1/serial.cmd
@@ -4,8 +4,12 @@
 
 #drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
 #                        noProcessEos)
-drvAsynIPPortConfigure("serial1", "164.54.160.155:1394", 0, 0, 0)
+drvAsynIPPortConfigure("serial1", "164.54.160.156:1394", 0, 0, 0)
 #drvAsynIPPortConfigure("serial1", "164.54.160.154:2001", 0, 0, 0)
+#asynSetTraceIOMask serial1 0 2
+#asynSetTraceMask serial1 0 255
+asynOctetSetInputEos("serial1",0,"\n")
+asynOctetSetOutputEos("serial1",0,"\n")
 asynOctetConnect("serial1", "serial1")
 # Load asynRecord records on all ports
 dbLoadTemplate("asynRecord.template")
