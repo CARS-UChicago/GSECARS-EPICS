b78f64559d45cd0889847aab86bf42c2ef21af22
epics <>
2012-02-14 15:37:04 +0000

    New file

---
+++ iocBoot/ioc13LQExcel/serial.cmd
@@ -0,0 +1,18 @@
+# BEGIN serial.cmd ------------------------------------------------------------
+
+# Set up port on Digi box
+
+#drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
+#                        noProcessEos)
+drvAsynIPPortConfigure("serial1", "gsets4:2101", 0, 0, 0)
+asynOctetConnect("serial1", "serial1")
+asynOctetSetInputEos("serial1",0,"\r\n")
+asynOctetSetOutputEos("serial1",0,"\r\n")
+
+# Load asynRecord records on all ports
+dbLoadTemplate("asynRecord.substitutions")
+
+# Laser Quantum Excel laser
+dbLoadRecords("$(CARS)/CARSApp/Db/LQExcel.db", "P=13LQE1:,R=L1,PORT=serial1")
+
+# END serial.cmd --------------------------------------------------------------
---
+++ iocBoot/ioc13LQExcel/st.cmd
@@ -0,0 +1,54 @@
+errlogInit(5000)
+< envPaths
+
+epicsEnvSet(STREAM_PROTOCOL_PATH, $(IP)/ipApp/Db:$(CARS)/CARSApp/Db)
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build from CARS
+dbLoadDatabase("../../dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+< serial.cmd
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=13LQE1:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+
+# Free-standing user string/number calculations (sCalcout records)
+dbLoadRecords("$(CALC)/calcApp/Db/userStringCalcs10.db", "P=13LQE1:")
+
+# Free-standing user transforms (transform records)
+dbLoadRecords("$(CALC)/calcApp/Db/userTransforms10.db", "P=13LQE1:")
+
+# Miscellaneous PV's, such as burtResult
+dbLoadRecords("$(STD)/stdApp/Db/misc.db", "P=13LQE1:")
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("13LQE1:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13LQE1:")
+
+iocInit
+
+# save positions every five seconds
+create_monitor_set("auto_positions.req", 5, "P=13LQE1:")
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req", 30, "P=13LQE1:")
+
+### Start the saveData task.
+# saveData_MessagePolicy
+# 0: wait forever for space in message queue, then send message
+# 1: send message only if queue is not full
+# 2: send message only if queue is not full and specified time has passed (SetCptWait()
+#    sets this time.)
+# 3: if specified time has passed, wait for space in queue, then send message
+# else: don't send message
+#debug_saveData = 2
+#saveData_MessagePolicy = 2
+saveData_SetCptWait_ms(100)
+saveData_Init("saveDataExtraPVs.req", "P=13LQE1:")
+#saveData_PrintScanInfo("13LQE1:scan1")
+
