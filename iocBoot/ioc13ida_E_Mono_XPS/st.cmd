cd673b115be947f82827f52af91226b70653050a
epics <>
2012-04-12 21:00:14 +0000

    New file

---
+++ iocBoot/ioc13ida_E_Mono_XPS/st.cmd
@@ -0,0 +1,123 @@
+< envPaths
+# Start errlog Task before any possible error messsage to prevent
+# erroneous "Interrupted system call" message on Linux OS.
+errlogInit(0)
+#
+dbLoadDatabase("../../../dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+
+################################################################################
+# XPS Setup
+
+# asyn port, IP address, IP port, number of axes, 
+# active poll period (ms), idle poll period (ms), 
+# enable set position, set position settling time (ms)
+XPSCreateController("XPS1", "newport-xps9", 5001, 4, 10, 500, 0, 500)
+asynSetTraceIOMask("XPS1", 0, 2)
+#asynSetTraceMask("XPS1", 0, 255)
+
+# asynPort, IP address, IP port, poll period (ms)
+XPSAuxConfig("XPS_AUX1", "newport-xps9", 5001, 50)
+#asynSetTraceIOMask("XPS_AUX1", 0, 2)
+#asynSetTraceMask("XPS_AUX1", 0, 255)
+
+# XPS asyn port,  axis, groupName.positionerName, stepSize
+XPSCreateAxis("XPS1",0,"GROUP1.THETA",  "10000")  
+XPSCreateAxis("XPS1",1,"GROUP2.HEIGHT", "5000")  
+XPSCreateAxis("XPS1",2,"GROUP3.PITCH",  "5600")  
+XPSCreateAxis("XPS1",3,"GROUP4.ROLL",   "5600")  
+
+# XPS asyn port,  max points, FTP username, FTP password
+# Note: this must be done after configuring axes
+XPSCreateProfile("XPS1", 10010, "Administrator", "Administrator")
+
+# Disable setting position from motor record
+XPSEnableSetPosition(0) 
+
+################################################################################
+# Motor records
+dbLoadTemplate("motors.template")
+
+# Auxillary I/O records
+dbLoadTemplate("XPSAux.substitutions")
+
+# asyn record for debugging
+drvAsynIPPortConfigure("xps", "newport-xps9:5001", 0, 0, 0)
+asynSetTraceIOMask("xps",0,2)
+asynSetTraceMask("xps",0,9)
+dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=13IDA:, R=trajAsyn2, PORT=xps, ADDR=0, OMAX=300, IMAX=32000")
+
+# Debug-output level
+save_restoreSet_Debug(0)
+
+# Ok to save/restore save sets with missing values (no CA connection to PV)?
+save_restoreSet_IncompleteSetsOk(1)
+# Save dated backup files?
+save_restoreSet_DatedBackupFiles(1)
+
+# Number of sequenced backup files to write
+save_restoreSet_NumSeqFiles(3)
+# Time interval between sequenced backups
+save_restoreSet_SeqPeriodInSeconds(300)
+
+# specify where save files should be
+set_savefile_path(".", "autosave")
+
+###
+# specify what save files should be restored.  Note these files must be
+# in the directory specified in set_savefile_path(), or, if that function
+# has not been called, from the directory current when iocInit is invoked
+set_pass0_restoreFile("auto_positions.sav")
+set_pass0_restoreFile("auto_settings.sav")
+set_pass1_restoreFile("auto_settings.sav")
+
+###
+# specify directories in which to to search for included request files
+set_requestfile_path("./")
+set_requestfile_path("$(CARS)",     "CARSApp/Db")
+set_requestfile_path("$(AREA_DETECTOR)", "ADApp/Db")
+set_requestfile_path("$(AUTOSAVE)", "asApp/Db")
+set_requestfile_path("$(CALC)",     "calcApp/Db")
+set_requestfile_path("$(CAMAC)",    "camacApp/Db")
+set_requestfile_path("$(DAC128V)",  "dac128VApp/Db")
+set_requestfile_path("$(DXP)",      "dxpApp/Db")
+set_requestfile_path("$(IP)",       "ipApp/Db")
+set_requestfile_path("$(MCA)",      "mcaApp/Db")
+set_requestfile_path("$(MOTOR)",    "motorApp/Db")
+set_requestfile_path("$(OPTICS)",   "opticsApp/Db")
+set_requestfile_path("$(QUADEM)",   "quadEMApp/Db")
+set_requestfile_path("$(SSCAN)",    "sscanApp/Db")
+set_requestfile_path("$(STD)",      "stdApp/Db")
+set_requestfile_path("$(VME)",      "vmeApp/Db")
+
+save_restoreSet_status_prefix("13IDA_XPS2:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13IDA_XPS2:")
+
+# A set of scan parameters for each positioner.  This is a convenience
+# for the user.  It can contain an entry for each scannable thing in the
+# crate.
+dbLoadTemplate "scanParms.template"
+
+### motorUtil - for allstop, moving, etc.
+dbLoadRecords("$(MOTOR)/motorApp/Db/motorUtil.db","P=13IDA_XPS2:")
+
+iocInit
+
+### Start up the autosave task and tell it what to do.
+# The task is actually named "save_restore".
+# (See also, 'initHooks' above, which is the means by which the values that
+# will be saved by the task we're starting here are going to be restored.
+#
+# save positions every five seconds
+create_monitor_set("auto_positions.req",5,"P=13IDA:")
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=13IDA:")
+
+# Set the NTM fields of the XPS motors to 0 (NO) so they don't get stopped when the motor changes direction due to PID
+dbpf("13IDA:m61.NTM","0")
+dbpf("13IDA:m62.NTM","0")
+dbpf("13IDA:m63.NTM","0")
+dbpf("13IDA:m64.NTM","0")
+
+motorUtilInit("13IDA_XPS2:")
