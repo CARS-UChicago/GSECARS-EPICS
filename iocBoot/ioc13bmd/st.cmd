e5bb9101019623435889f570ac06099155ce2f3b
epics <>
2013-05-01 21:26:57 +0000

    Add MCB-4B

--- iocBoot/ioc13bmd/st.cmd
+++ iocBoot/ioc13bmd/st.cmd
@@ -98,6 +98,18 @@ configServo="AX LL PSM; AY LL PSM; AZ LL PSM; AT LL PSM; AU LL PSM; AV LL PSM; A
 # First MAXv
 MAXvConfig(0, configStep)
 
+# MCB-4B driver setup parameters:
+#     (1) maximum # of controllers,
+#     (2) maximum # axis per controller
+#     (3) motor task polling rate (min=1Hz, max=60Hz)
+MCB4BSetup(2, 4, 10)
+
+# MCB-4B driver configuration parameters:
+#     (1) controller
+#     (2) asyn port name (e.g. serial1)
+MCB4BConfig(0, "serial17")
+MCB4BConfig(1, "serial18")
+
 dbLoadTemplate "motors.template"
 
 ### Allstop, alldone
