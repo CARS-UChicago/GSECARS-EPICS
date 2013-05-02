a503e202d55caedb4f149068a565620ed11c9dda
epics <>
2013-05-02 18:37:19 +0000

    New file

---
+++ iocBoot/ioc13idc_ps2/start_epics.bat
@@ -0,0 +1,5 @@
+## start medm -x -macro "P=13IDCPS1:, R=cam1:" prosilica.adl
+SET EPICS_CA_MAX_ARRAY_BYTES=4800000
+
+..\..\..\areaDetector\bin\win32-x86\prosilicaApp st.cmd.win32
+
---
+++ iocBoot/ioc13idc_ps2/start_epics.linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /home/epics/support/CARS/iocBoot/ioc13idc_ps2
+/home/epics/support/areaDetector/bin/linux-x86/prosilicaApp st.cmd.linux 
+
