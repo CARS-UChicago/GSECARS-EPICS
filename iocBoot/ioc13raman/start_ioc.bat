bd9d54ff5c59f6a8080a0a82364f142e921a321c
epics <>
2013-09-19 16:58:53 +0000

    No longer need mca/bin in path

--- iocBoot/ioc13raman/start_ioc.bat
+++ iocBoot/ioc13raman/start_ioc.bat
@@ -1,4 +1,4 @@
 start medm -x 13Raman_user_new.adl
-set PATH=c:\cygwin\bin;..\..\..\mca\bin\cygwin-x86
+set PATH=c:\cygwin\bin;
 ..\..\bin\cygwin-x86\CARSApp.exe st.cmd
 pause
--- iocBoot/ioc13raman/start_ioc_win32.bat
+++ iocBoot/ioc13raman/start_ioc_win32.bat
@@ -1,4 +1,3 @@
 start medm -x 13Raman_user.adl
-set PATH=..\..\..\mca\bin\win32-x86
 ..\..\bin\win32-x86\CARSApp.exe st.cmd.win32
 pause
