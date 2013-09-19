57696f79c92b31bd2c720de69d63f14b4ca621df
epics <>
2013-09-19 18:01:29 +0000

    Removed mca/bin path, no longer needed

--- iocBoot/ioc13GasLoading/start_epics.bat
+++ iocBoot/ioc13GasLoading/start_epics.bat
@@ -1,5 +1,3 @@
 start medm -x 13GasLoading.adl 
-rem Must add mca\bin\win32-x86 to the PATH so it finds needed DLLs
-set PATH=%PATH%;..\..\..\mca\bin\win32-x86
 ..\..\bin\win32-x86\CARSApp st.cmd
 pause
