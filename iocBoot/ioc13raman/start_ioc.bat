77f0ec339a1eea82fccd997d20c302b3c834d16f
epics <>
2013-10-02 22:50:55 +0000

    Switch to win32-x86; add call dllPath.bat

--- iocBoot/ioc13raman/start_ioc.bat
+++ iocBoot/ioc13raman/start_ioc.bat
@@ -1,4 +1,4 @@
-start medm -x 13Raman_user_new.adl
-set PATH=c:\cygwin\bin;
-..\..\bin\cygwin-x86\CARSApp.exe st.cmd
+start medm -x 13Raman_user.adl
+call dllPath.bat
+..\..\bin\win32-x86\CARSApp.exe st.cmd
 pause
