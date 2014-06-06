08d070c3147983e06cac79a792fb97e5000ed056
epics <>
2014-06-06 05:23:41 +0000

    Changes for new version of dxp

--- iocBoot/ioc13Saturn1/st.cmd
+++ iocBoot/ioc13Saturn1/st.cmd
@@ -1,36 +1,37 @@
 < envPaths
 
-# Tell EPICS all about the record types, device-support modules, drivers,
-# etc. in this build from dxpApp
-#dbLoadDatabase("../../dbd/CARS.dbd")
-dbLoadDatabase("../../dbd/CARSLinux.dbd")
-#CARS_registerRecordDeviceDriver(pdbbase)
-CARSLinux_registerRecordDeviceDriver(pdbbase)
+### Register all support components
+dbLoadDatabase("$(DXP)/dbd/dxp.dbd")
+dxp_registerRecordDeviceDriver(pdbbase)
+
+# Set EPICS_CA_MAX_ARRAY_BYTES large enough for the trace buffers
+epicsEnvSet EPICS_CA_MAX_ARRAY_BYTES 100000
+
+# On Linux execute the following command so that libusb uses /dev/bus/usb
+# as the file system for the USB device.  
+# On some Linux systems it uses /proc/bus/usb instead, but udev
+# sets the permissions on /dev, not /proc.
+epicsEnvSet USB_DEVFS_PATH /dev/bus/usb
 
 # Initialize the XIA software
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
 xiaSetLogLevel(2)
-# Execute the following line if you have a Vortex detector or 
-# another detector with a reset pre-amplifier
-xiaInit("vortex.ini")
-# Execute the following line if you have a Ketek detector or 
-# another detector with an RC pre-amplifier
-#xiaInit("ketek.ini")
+# Edit saturn.ini to match your Saturn speed (20 or 40 MHz), 
+# pre-amp type (reset or RC), and interface type (EPP, USB 1.0, USB 2.0)
+xiaInit("saturn.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, nchans)
-DXPConfig("DXP1",  1)
+NDDxpConfig("DXP1", 1, 10, 42000000)
+asynSetTraceIOMask("DXP1", 0, 2)
+#asynSetTraceMask("DXP1", 0, 255)
 
-# DXP record
-# Execute the following line if you have a Vortex detector or 
-# another detector with a reset pre-amplifier
-dbLoadRecords("$(DXP)/dxpApp/Db/dxp2x_reset.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
-# Execute the following line if you have a Ketek detector or 
-# another detector with an RC pre-amplifier
-#dbLoadRecords("$(DXP)/dxpApp/Db/dxp2x_rc.db","P=13Saturn1:, R=dxp1, INP=@asyn(DXP1 0)")
-
-# MCA record
-dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13Saturn1:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxpSystem.template",   "P=13Saturn1:, R=dxp1:,IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxpHighLevel.template","P=13Saturn1:, R=dxp1:,IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxpSaturn.template",   "P=13Saturn1:, R=dxp1:,IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxpLowLevel.template", "P=13Saturn1:, R=dxp1:,IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(DXP)/dxpApp/Db/dxpSCA_16.template",   "P=13Saturn1:, R=dxp1:,IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(DXP)/dxpApp/Db/mcaCallback.template", "P=13Saturn1:, R=mca1, IO=@asyn(DXP1 0 1)")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db",               "P=13Saturn1:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
 
 # Template to copy MCA ROIs to DXP SCAs
 dbLoadTemplate("roi_to_sca.substitutions")
@@ -40,15 +41,17 @@ dbLoadTemplate("roi_to_sca.substitutions")
 save_restoreSet_status_prefix("13Saturn1:")
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13Saturn1:")
 
-# Debugging flags
-#asynSetTraceMask DXP1 0 255
-#var mcaRecordDebug 10
-#var dxpRecordDebug 10
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=13Saturn1:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
 
-iocInit
+iocInit()
 
 ### Start up the autosave task and tell it what to do.
-
 # Save settings every thirty seconds
-create_monitor_set("auto_settings.req", 30, "P=13Saturn1:")
+create_monitor_set("auto_settings.req", 30, P=13Saturn1:)
+
+### Start the saveData task.
+saveData_Init("saveData.req", "P=13Saturn1:")
 
