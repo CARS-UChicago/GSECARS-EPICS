de07bba518e216675624d8d428980648a79ef462
epics <>
2014-06-06 05:35:49 +0000

    New file

---
+++ iocBoot/ioc13idd_lightfield1/start_epics.bat
@@ -0,0 +1,4 @@
+start medm -x -macro "P=13LF1:, R=cam1:" LightField.adl
+call dllPath.bat
+J:\epics\support\areaDetector\ADLightField\iocs\lightFieldIOC\bin\windows-x64-dynamic\LightFieldApp st.cmd
+
