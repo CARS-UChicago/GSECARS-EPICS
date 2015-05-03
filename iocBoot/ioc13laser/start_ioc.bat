cb35d25fbe9cccfe19f8c252bb24bc088881f01f
epics <>
2015-05-03 02:23:06 +0000

    Change arch to win32-x86-static

--- iocBoot/ioc13laser/start_ioc.bat
+++ iocBoot/ioc13laser/start_ioc.bat
@@ -1,4 +1,4 @@
 start medm -x 13Laser.adl
 call dllPath.bat
-..\..\bin\win32-x86\CARSApp.exe st.cmd
+..\..\bin\win32-x86-static\CARSApp.exe st.cmd
 
--- iocBoot/ioc13raman/start_ioc.bat
+++ iocBoot/ioc13raman/start_ioc.bat
@@ -1,4 +1,4 @@
 start medm -x 13Raman_user.adl
 call dllPath.bat
-..\..\bin\win32-x86\CARSApp.exe st.cmd
+..\..\bin\win32-x86-static\CARSApp.exe st.cmd
 pause
