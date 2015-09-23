78ba672f43d1e0a2ab05fc32c7d18af109c9a5fe
epics <>
2015-09-23 20:27:15 +0000

    Changed prefix and ARCH

--- iocBoot/ioc13idd_lightfield1/start_epics.bat
+++ iocBoot/ioc13idd_lightfield1/start_epics.bat
@@ -1,4 +1,4 @@
-start medm -x -macro "P=13LF1:, R=cam1:" LightField.adl
+start medm -x -macro "P=13IDDLF1:, R=cam1:" LightField.adl
 call dllPath.bat
-J:\epics\support\areaDetector\ADLightField\iocs\lightFieldIOC\bin\windows-x64-dynamic\LightFieldApp st.cmd
+J:\epics\support\areaDetector\ADLightField\iocs\lightFieldIOC\bin\windows-x64\LightFieldApp st.cmd
 
