229f4deaf25ebbac5e6f7eb629bd8a175b9753bd
epics <>
2015-09-23 20:52:18 +0000

    Obsolete

--- iocBoot/ioc13bmc_ps1/start_epics.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/epics/support/CARS/iocBoot/ioc13bmc_ps1
-../../../areaDetector/bin/linux-x86/prosilicaApp st.cmd.linux
-
--- iocBoot/ioc13idc_ps1/start_epics.linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/epics/support/CARS/iocBoot/ioc13idc_ps1
-/home/epics/support/areaDetector/bin/linux-x86/prosilicaApp st.cmd.linux 
-
