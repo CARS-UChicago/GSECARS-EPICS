3f7ea47d2ac6c0aba688679a28367d0a12b6cd77
epics <>
2015-09-23 21:27:00 +0000

    Changed ARCH

--- iocBoot/ioc13LUAndor/start_epics.bat
+++ iocBoot/ioc13LUAndor/start_epics.bat
@@ -1,5 +1,5 @@
 start medm -x -macro "P=13LUANDOR:, R=cam1:" andor.adl &
 start medm -x -macro "P=13LUANDOR:, R=sham1:" Shamrock.adl &
-J:\epics\devel\areaDetector\ADAndor\iocs\andorIOC\bin\win32-x86\andorCCDApp st.cmd
+J:\epics\support\areaDetector\ADAndor\iocs\andorIOC\bin\win32-x86-static\andorCCDApp st.cmd
 pause
 
--- iocBoot/ioc13PE1/start_epics.bat
+++ iocBoot/ioc13PE1/start_epics.bat
@@ -1,3 +1,3 @@
 #start medm -x -macro "P=13PE1:, R=cam1:" PerkinElmer.adl
-J:\epics\support\areaDetector\ADPerkinElmer\iocs\perkinElmerIOC\bin\windows-x64\PerkinElmerApp st.cmd
+J:\epics\support\areaDetector\ADPerkinElmer\iocs\perkinElmerIOC\bin\windows-x64-static\PerkinElmerApp st.cmd
 pause
