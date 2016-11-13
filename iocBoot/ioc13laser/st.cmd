a91fe8a7c5eca5a4e8d35c69f8e843514dc1104e
epics <>
2016-11-13 01:38:09 +0000

    Added AG_CONEX on serial5 (COM5) for Y piezo

--- iocBoot/ioc13laser/st.cmd
+++ iocBoot/ioc13laser/st.cmd
@@ -15,12 +15,15 @@ drvAsynIPPortConfigure("serial1", "gsets3:4001", 0, 0)
 drvAsynIPPortConfigure("serial2", "gsets3:4002", 0, 0)
 drvAsynIPPortConfigure("serial3", "gsets3:4003", 0, 0)
 drvAsynIPPortConfigure("serial4", "gsets3:4004", 0, 0)
+drvAsynSerialPortConfigure("serial5", "COM5", 0, 0, 0)
+
 
 # Make these ports available from the iocsh command line
 asynOctetConnect("serial1", "serial1", 0, 1, 80)
 asynOctetConnect("serial2", "serial2", 0, 1, 80)
 asynOctetConnect("serial3", "serial3", 0, 1, 80)
 asynOctetConnect("serial4", "serial4", 0, 1, 80)
+asynOctetConnect("serial5", "serial5", 0, 1, 80)
 asynOctetSetInputEos("serial1",0,"\r")
 asynOctetSetOutputEos("serial1",0,"\r")
 asynOctetSetInputEos("serial2",0,"\r")
@@ -29,6 +32,14 @@ asynOctetSetInputEos("serial3",0,"\r")
 asynOctetSetOutputEos("serial3",0,"\r")
 asynOctetSetInputEos("serial4",0,"\r\n")
 asynOctetSetOutputEos("serial4",0,"\r\n")
+asynOctetSetInputEos("serial5",0,"\r\n")
+asynOctetSetOutputEos("serial5",0,"\r\n")
+asynSetOption("serial5",0,"baud","921600")
+asynSetOption("serial5",0,"bits","8")
+asynSetOption("serial5",0,"stop","1")
+asynSetOption("serial5",0,"parity","none")
+asynSetOption("serial5",0,"clocal","Y")
+asynSetOption("serial5",0,"crtscts","N")
 
 #asynSetTraceIOMask("serial1", 0, 2)
 #asynSetTraceIOMask("serial2", 0, 2)
@@ -59,7 +70,10 @@ dbLoadTemplate "scanParms.template"
 ### Allstop, alldone
 # This database must agree with the motors you've actually loaded.
 # Several versions (e.g., all_com_32.db) are in stdApp/Db
-dbLoadRecords("$(STD)/stdApp/Db/all_com_4.db", "P=13Laser:")
+# dbLoadRecords("$(STD)/stdApp/Db/all_com_8.db", "P=13Laser:")
+### motorUtil - for allstop, moving, etc.
+dbLoadRecords("$(MOTOR)/motorApp/Db/motorUtil.db","P=13Laser:")
+
 
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
@@ -87,6 +101,12 @@ dbLoadRecords("$(STD)/stdApp/Db/misc.db", "P=13Laser:")
 MCB4BCreateController("MCB4B_1", "serial1", 4, 100, 1000)
 MCB4BCreateController("MCB4B_2", "serial2", 4, 100, 1000)
 
+# AG_CONEXCreateController(asyn port, serial port, controllerID, 
+#                          active poll period (ms), idle poll period (ms)) 
+AG_CONEXCreateController("Agilis1", "serial5", 1, 50, 500)
+asynSetTraceIOMask("Agilis1", 0, 2)
+#asynSetTraceMask("Agilis1", 0, 255)
+
 < ../save_restore_IOCSH.cmd
 save_restoreSet_status_prefix("13Laser:")
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13Laser:")
@@ -109,3 +129,6 @@ create_monitor_set("auto_positions.req", 5, "P=13Laser:")
 # save other things every thirty seconds
 create_monitor_set("auto_settings.req", 30, "P=13Laser:")
 
+dbpf 13Laser1:m5.RTRY 0
+dbpf 13Laser1:m5.NTM 0
+motorUtilInit("13Laser:")
