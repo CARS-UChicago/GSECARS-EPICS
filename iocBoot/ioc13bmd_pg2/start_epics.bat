0ea01abd15c1548374c99e88b8e92526865f3958
epics <>
2016-11-13 01:35:29 +0000

    Changed prefix

--- iocBoot/ioc13bmd_pg2/start_epics.bat
+++ iocBoot/ioc13bmd_pg2/start_epics.bat
@@ -1,4 +1,4 @@
-rem start medm -x -macro "P=13BMDPG1:, R=cam1:" PointGrey.adl
+rem start medm -x -macro "P=13BMDPG2:, R=cam1:" PointGrey.adl
 call dllPath.bat
 J:\epics\support\areaDetector\ADPointGrey\iocs\pointGreyIOC\bin\windows-x64-static\pointGreyApp st.cmd
 pause
