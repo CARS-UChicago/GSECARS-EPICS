8bf45d39c370ef5876462fd4f5014c381b78555b
epics <>
2017-01-05 21:07:19 +0000

    New file

---
+++ iocBoot/ioc15Pilatus3/st.cmd
@@ -0,0 +1,49 @@
+< envPaths
+errlogInit(20000)
+
+dbLoadDatabase("$(ADPILATUS)/iocs/pilatusIOC/dbd/pilatusDetectorApp.dbd")
+pilatusDetectorApp_registerRecordDeviceDriver(pdbbase) 
+
+# Prefix for all records
+epicsEnvSet("PREFIX", "15PIL3:")
+# The port name for the detector
+epicsEnvSet("PORT",   "PIL")
+# The queue size for all plugins
+epicsEnvSet("QSIZE",  "20")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
+epicsEnvSet("XSIZE",  "981")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
+epicsEnvSet("YSIZE",  "1043")
+# The maximum number of time seried points in the NDPluginStats plugin
+epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+
+###
+# Create the asyn port to talk to the Pilatus on port 41234.
+drvAsynIPPortConfigure("camserver","10.10.10.100:41234")
+# Set the input and output terminators.
+asynOctetSetInputEos("camserver", 0, "\030")
+asynOctetSetOutputEos("camserver", 0, "\n")
+
+pilatusDetectorConfig("$(PORT)", "camserver", $(XSIZE), $(YSIZE), 0, 0)
+dbLoadRecords("$(ADPILATUS)/db/pilatus.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,CAMSERVER_PORT=camserver")
+
+# Create a standard arrays plugin
+NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=1023183")
+
+# Load all other plugins using commonPlugins.cmd
+< $(ADCORE)/iocBoot/commonPlugins.cmd
+set_requestfile_path("$(ADPILATUS)/pilatusApp/Db")
+
+#asynSetTraceMask("$(PORT)",0,255)
+#asynSetTraceMask("$(PORT)",0,3)
+
+
+iocInit()
+
+# save things every thirty seconds
+create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
