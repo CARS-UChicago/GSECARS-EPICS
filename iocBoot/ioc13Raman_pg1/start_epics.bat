f1ce6517c219a263b3bc8fcb8076fe17d1163781
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-21 08:10:04 -0600

    Removed commented out line

--- iocBoot/ioc13Raman_pg1/start_epics.bat
+++ iocBoot/ioc13Raman_pg1/start_epics.bat
@@ -1,7 +1,6 @@
 rem start medm -x -macro "P=13RamanPG1:, R=cam1:" PointGrey.adl
 call dllPath.bat
 set EPICS_CA_MAX_ARRAY_BYTES=32010203
-# set EPICS_CA_MAX_ARRAY_BYTES=20000000
 J:\epics\support\areaDetector\ADPointGrey\iocs\pointGreyIOC\bin\windows-x64-static\pointGreyApp st.cmd
 pause
 
