6aa3634519ef29f9b0743e4e6ee83e954c4ad99b
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 11:59:32 -0500

    Added another OMS-58

--- iocBoot/ioc13idc/st.cmd
+++ iocBoot/ioc13idc/st.cmd
@@ -119,7 +119,7 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13IDC:")
 #     (1)cards, (2)base address(short, 4k boundary),
 #     (3)interrupt vector (0=disable or  64 - 255), (4)interrupt level (1 - 6),
 #     (5)motor task polling rate (min=1Hz,max=60Hz)
-oms58Setup(5, 0x4000, 190, 5, 10)
+oms58Setup(6, 0x4000, 190, 5, 10)
 ################################################################################
 
 ################################################################################
