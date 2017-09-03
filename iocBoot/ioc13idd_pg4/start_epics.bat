6e7ce744b486d5dbbfb69bd688374c79371b2475
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 12:18:28 -0500

    New file

---
+++ iocBoot/ioc13idd_pg4/start_epics.bat
@@ -0,0 +1,5 @@
+rem start medm -x -macro "P=13IDDPG4:, R=cam1:" PointGrey.adl
+call dllPath.bat
+J:\epics\support\areaDetector\ADPointGrey\iocs\pointGreyIOC\bin\windows-x64-static\pointGreyApp st.cmd
+pause
+
