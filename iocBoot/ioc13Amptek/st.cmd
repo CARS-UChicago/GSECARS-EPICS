8ca46c50463679c8a37860b6ba7daf46b932ceda
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 12:33:02 -0500

    New file

---
+++ iocBoot/ioc13Amptek/st.cmd
@@ -0,0 +1,4 @@
+< envPaths
+dbLoadDatabase("$(CARS)/dbd/CARSWin32.dbd",0,0)
+CARSWin32_registerRecordDeviceDriver(pdbbase) 
+< st_base.cmd
---
+++ iocBoot/ioc13Amptek/st_base.cmd
@@ -0,0 +1,20 @@
+epicsEnvSet(STARTUP,$(TOP)/iocBoot/$(IOC))
+
+drvAmptekConfigure(Amptek1, 0, "164.54.160.200")
+
+#asynSetTraceMask(Amptek1, 0, 9)
+asynSetTraceIOMask(Amptek1, 0, 2)
+
+dbLoadRecords("$(MCA)/db/mca.db","P=mcaTest:,M=mca1,NCHAN=8192,DTYP=asynMCA,INP=@asyn(Amptek1)")
+dbLoadRecords("$(MCA)/db/Amptek.db","P=mcaTest:,R=Amptek1:,PORT=Amptek1")
+
+dbLoadRecords("$(ASYN)/db/asynRecord.db","P=mcaTest:,R=asyn1,PORT=Amptek1,ADDR=0,OMAX=256,IMAX=256")
+
+< save_restore.cmd
+
+iocInit()
+
+# save settings every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=mcaTest:,M=mca1,R=Amptek1:")
+
+asynReport 10 Amptek1
