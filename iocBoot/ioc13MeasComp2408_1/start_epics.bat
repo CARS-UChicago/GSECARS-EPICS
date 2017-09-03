289d2b7d1236b666e62efcd5099522922dd48343
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 12:15:56 -0500

    Changed from windows-x64-static to windows-x64-static-vs2015; not sure why this was done

--- iocBoot/ioc13MeasComp2408_1/start_epics.bat
+++ iocBoot/ioc13MeasComp2408_1/start_epics.bat
@@ -1,4 +1,4 @@
-rem start medm -x -macro "P=13USB2408_1:, R=cam1: USB2408_module.adl
-..\..\bin\windows-x64-static\CARSApp st.cmd
+rem start medm -x -macro "P=13USB2408_1:, R=cam1: USB2408_module.adl"
+..\..\bin\windows-x64-static-vs2015\CARSApp st.cmd
 pause
 
