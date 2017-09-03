2f1f662c77b50366d8fc3c1e6e0f5a11968bba08
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 12:24:50 -0500

    New file

---
+++ iocBoot/ioc13Raman_ps1/start_epics.bat
@@ -0,0 +1,4 @@
+## start medm -x -macro "P=13RamanPS1:, R=cam1:" prosilica.adl
+call dllPath.bat
+J:\epics\support\areaDetector-3-1\ADProsilica\iocs\prosilicaIOC\bin\windows-x64-static\prosilicaApp st.cmd
+
