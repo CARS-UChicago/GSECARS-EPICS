a6523608b54decc12181a44790bccd9092e18a56
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-03 12:09:09 -0500

    Change from 32 to 64 bit Windows

--- iocBoot/ioc13bmc_ps1/start_epics.bat
+++ iocBoot/ioc13bmc_ps1/start_epics.bat
@@ -6,6 +6,6 @@ start medm -x -macro "P=13BMCPS1:,R=cam1:" prosilica.adl
 call dllPath.bat
 
 rem Start IOC
-J:\epics\support\areaDetector\ADProsilica\iocs\prosilicaIOC\bin\win32-x86-static\prosilicaApp st.cmd
+J:\epics\support\areaDetector\ADProsilica\iocs\prosilicaIOC\bin\windows-x64-static\prosilicaApp st.cmd
 pause
 
