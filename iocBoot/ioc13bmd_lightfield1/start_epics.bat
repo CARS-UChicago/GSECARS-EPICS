c2aea0541278e0a84049451beb1b30c56f8977d3
Mark Rivers <rivers@cars.uchicago.edu>
2017-12-20 07:18:49 -0600

    New file

---
+++ iocBoot/ioc13bmd_lightfield1/start_epics.bat
@@ -0,0 +1,6 @@
+start medm -x -macro "P=13BMDLF1:, R=cam1:" LightField.adl
+call J:\epics\support\areaDetector\ADLightField\iocs\lightFieldIOC\iocBoot\iocLightField\dllPath.bat
+J:\epics\support\areaDetector\ADLightField\iocs\lightFieldIOC\bin\windows-x64\LightFieldApp st.cmd
+pause
+
+
