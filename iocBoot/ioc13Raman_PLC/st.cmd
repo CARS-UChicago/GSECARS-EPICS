9fa4228a5792dda2026b9fd27b24fc1ab5b21df9
Mark Rivers <rivers@cars.uchicago.edu>
2018-03-23 14:21:20 -0500

    New file

---
+++ iocBoot/ioc13Raman_PLC/st.cmd
@@ -0,0 +1,32 @@
+errlogInit(5000)
+< envPaths
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build from CARS
+dbLoadDatabase("../../dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet(PREFIX, 13RamanPLC:)
+
+# Koyo PLC
+< Koyo.cmd
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix($(PREFIX))
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(PREFIX)")
+
+# devIocStats
+epicsEnvSet("ENGINEER", "Mark Rivers")
+epicsEnvSet("LOCATION","Laser interlock")
+epicsEnvSet("GROUP","GSECARS")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(PREFIX)")
+
+iocInit
+
+### Start up the autosave task and tell it what to do.
+# The task is actually named "save_restore".
+# (See also, 'initHooks' above, which is the means by which the values that
+# will be saved by the task we're starting here are going to be restored.
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+
