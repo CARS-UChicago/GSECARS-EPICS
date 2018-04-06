5793b171507264f8ef213c8b131f7227ea8fdba5
Mark Rivers <rivers@cars.uchicago.edu>
2018-04-06 16:23:21 -0500

    Not needed

--- iocBoot/ioc13lab_roper1/start_epics_new.bat
+++
@@ -1,6 +0,0 @@
-cd J:\epics\devel\areaDetector-1-6\ADApp\op\adl
-start medm -x -macro "P=13LABRP1:, R=cam1:" Roper.adl
-cd J:\epics\support\CARS\1-5\iocBoot\ioc13lab_roper1
-J:\epics\devel\areaDetector-1-6\bin\win32-x86\roperApp st.cmd
-pause
-
