1e9455af6f6f19f8068fe6aa5689056d12f97657
Mark Rivers <rivers@cars.uchicago.edu>
2018-04-06 15:30:06 -0500

    Use local copy of Windows binaries

--- iocBoot/ioc13Raman2/start_ioc.bat
+++ iocBoot/ioc13Raman2/start_ioc.bat
@@ -1,4 +1,4 @@
 start medm -x 13Raman2.adl
-call dllPath.bat
-..\..\bin\windows-x64-static\CARSApp.exe st.cmd
+set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
+CARSApp.exe st.cmd
 pause
