1b559afa968782e6182b96eb9d5e5deffb4e60b3
Mark Rivers <rivers@cars.uchicago.edu>
2018-09-16 17:42:20 -0500

    2 motors for testing soft motors

--- iocBoot/ioc13xps_test/st.cmd
+++ iocBoot/ioc13xps_test/st.cmd
@@ -14,7 +14,8 @@ dbLoadTemplate  "motors.template"
 # asyn port, IP address, IP port, number of axes, 
 # active poll period (ms), idle poll period (ms), 
 # enable set position, set position settling time (ms)
-XPSCreateController("XPS1", "164.54.160.41", 5001, 1, 10, 200, 0, 500)
+# This is newport-xps9
+XPSCreateController("XPS1", "164.54.160.41", 5001, 2, 10, 200, 0, 500)
 asynSetTraceIOMask("XPS1", 0, 2)
 #asynSetTraceMask("XPS1", 0, 255)
 
@@ -24,9 +25,8 @@ asynSetTraceIOMask("XPS1", 0, 2)
 #asynSetTraceMask("XPS_AUX1", 0, 255)
 
 # XPS asyn port,  axis, groupName.positionerName, stepSize
-XPSCreateAxis("XPS1",0,"GROUP1.PHI",  "10000")  
-#XPSCreateAxis("XPS1",1,"Group1.Pos2",  "10000")  
-#XPSCreateAxis("XPS1",2,"Group1.Pos3",  "10000")  
+XPSCreateAxis("XPS1",0,"GROUP1.M1",  "10000")  
+XPSCreateAxis("XPS1",1,"GROUP1.M2",  "10000")  
 
 # XPS asyn port,  max points, FTP username, FTP password
 # Note: this must be done after configuring axes
