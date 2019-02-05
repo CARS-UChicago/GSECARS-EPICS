a3db870ed89ad9696b12abfbe092b83d3adb10ab
Mark Rivers <rivers@cars.uchicago.edu>
2019-02-05 16:13:01 -0600

    Use PREFIX; add calc_GSECARS; add pid_slow

--- iocBoot/ioc13MeasCompE1608_1/st.cmd
+++ iocBoot/ioc13MeasCompE1608_1/st.cmd
@@ -7,6 +7,8 @@ CARSLinux_registerRecordDeviceDriver pdbbase
 epicsEnvSet(INPUT_POINTS, "4096")
 epicsEnvSet(OUTPUT_POINTS, "4096")
 
+epicsEnvSet("PREFIX", "13E1608_1:")
+
 cbAddBoard("E-1608", "164.54.160.216")
 
 ## Configure port driver
@@ -19,14 +21,20 @@ MultiFunctionConfig("E1608_1", 0, $(INPUT_POINTS), $(OUTPUT_POINTS))
 #asynSetTraceMask E1608_1 -1 255
 
 dbLoadTemplate("E1608.substitutions")
+dbLoadTemplate("pid_slow.substitutions")
+
+< ../calc_GSECARS.iocsh
+
+### Scan-support software
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=1$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
 
 < ../save_restore_IOCSH.cmd
 
 iocInit
 
-create_monitor_set("auto_settings.req",30)
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
 
 # Need to force the time arrays to process because the records are scan=I/O Intr
 # but asynPortDriver does not do array callbacks before iocInit.
 
-dbpf 13E1608_1:WaveDigDwell.PROC 1
+dbpf $(PREFIX)WaveDigDwell.PROC 1
