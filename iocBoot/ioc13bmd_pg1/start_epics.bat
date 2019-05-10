a30a5a2420a4671cc6c4df5317a4df26d6db0ce8
Mark Rivers <rivers@cars.uchicago.edu>
2019-05-10 08:22:24 -0500

    Change from windows-x64-static-vs2010 to windows-x64-static

--- iocBoot/ioc13bmd_pg1/start_epics.bat
+++ iocBoot/ioc13bmd_pg1/start_epics.bat
@@ -1,5 +1,5 @@
 rem start medm -x -macro "P=13BMDPG1:, R=cam1:" PointGrey.adl
-set PATH=C:\epics_windows_binaries\windows-x64-static-vs2010;%PATH%
+set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
 pointGreyApp st.cmd
 pause
 
