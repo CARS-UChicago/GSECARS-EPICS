d032fc5b2c6989d2d8d932416644376e170858c1
Mark Rivers <rivers@cars.uchicago.edu>
2019-05-10 07:57:49 -0500

    Added commented out code for AIM; not working yet

--- iocBoot/ioc13idd_LVP/st.cmd
+++ iocBoot/ioc13idd_LVP/st.cmd
@@ -52,6 +52,32 @@ dbLoadTemplate("LVP_furnace_control.template")
 # LVP Theta (temperature ramping) controller
 dbLoadRecords("$(CARS)/CARSApp/Db/RampScan.db","P=$(PREFIX),R=Theta1_,DRV=LVP:PID1.VAL,RBV=LVP_furnace_calcs.E")
 
+var aimDebug 1
+# Multichannel analyzer stuff
+# AIMConfig(portName, ethernet_address, portNumber(1 or 2), maxChans,
+#           maxSignals, maxSequences, ethernetDevice)
+#AIMConfig("NI3ED/1", 0x59E, 1, 4000, 1, 1,"p5p1")
+#AIMConfig("NI3ED/2", 0x59E, 2, 4000, 1, 1,"p5p1")
+#dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDD:,M=aim_adc1,DTYP=asynMCA,INP=@asyn(NI3ED/1 0),NCHAN=4000")
+#dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=13IDD:,M=aim_mcs1,DTYP=asynMCA,INP=@asyn(NI3ED/2 0),NCHAN=4000")
+
+#icbConfig(portName, module, ethernetAddress, icbAddress, moduleType)
+#   portName to give to this asyn port
+#   ethernetAddress - Ethernet address of module, low order 16 bits
+#   icbAddress - rotary switch setting inside ICB module
+#   moduleType
+#      0 = ADC
+#      1 = Amplifier
+#      2 = HVPS
+#      3 = TCA
+#      4 = DSP
+#icbConfig("icbAdc1", 0x59E, 5, 0)
+#dbLoadRecords("$(MCA)/mcaApp/Db/icb_adc.db", "P=13IDD:,ADC=adc1,PORT=icbAdc1")
+#icbConfig("icbAmp1", 0x59E, 3, 1)
+#dbLoadRecords("$(MCA)/mcaApp/Db/icb_amp.db", "P=13IDD:,AMP=amp1,PORT=icbAmp1")
+#icbConfig("icbHvps1", 0x59E, 2, 2)
+#dbLoadRecords("$(MCA)/mcaApp/Db/icb_hvps.db", "P=13IDD:,HVPS=hvps1,PORT=icbHvps1,LIMIT=1000")
+
 
 < ../calc_GSECARS.iocsh
 
