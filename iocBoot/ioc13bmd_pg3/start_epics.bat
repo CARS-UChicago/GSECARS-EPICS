44f8d6de376ed88a06c88f41d9f532e461b9ba97
Mark Rivers <rivers@cars.uchicago.edu>
2019-10-01 12:47:03 -0500

    Use ADSpinnaker

--- iocBoot/ioc13bmd_pg3/start_epics.bat
+++ iocBoot/ioc13bmd_pg3/start_epics.bat
@@ -1,4 +1,4 @@
-J:\epics\devel\areaDetector\ADSpinnaker\iocs\spinnakerIOC\bin\windows-x64-static\spinnakerApp st.cmd
+J:\epics\support\areaDetector\ADSpinnaker\iocs\spinnakerIOC\bin\windows-x64-static\spinnakerApp st.cmd.BlackflyS_50S5C
 pause
 
 
