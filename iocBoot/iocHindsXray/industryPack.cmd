f2678f4a341a19d1283d38e8c9066a2101bc3005
Mark Rivers <rivers@cars.uchicago.edu>
2019-10-01 12:37:36 -0500

    New file

---
+++ iocBoot/iocHindsXray/industryPack.cmd
@@ -0,0 +1,9 @@
+ipacAddHyLinux9010("99,0")
+ipacReport 10
+
+
+< Hy8522.cmd
+< DAC128V.cmd
+#< IpUnidig.cmd
+#< Ip330.cmd
+#< SoftGlue.cmd
---
+++ iocBoot/iocHindsXray/st.cmd
@@ -0,0 +1,79 @@
+errlogInit(5000)
+< envPaths
+
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in this build from CARS
+dbLoadDatabase("../../dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+< industryPack.cmd
+
+##########################################################
+# Configure asyn device
+#
+pmacAsynIPConfigure("PMAC_IP","10.135.28.41:1025",0,0,0)
+#asynSetTraceMask("PMAC_IP",-1,0xFF)
+asynSetTraceIOMask("PMAC_IP",-1,0x1)
+#asynSetTraceMask("PMAC_IP",-1,0x1)
+#asynSetTraceIOMask("PMAC_IP",-1,0x0)
+
+pmacAsynMotorCreate("PMAC_IP", 0, 0, 9);
+
+# Setup the motor Asyn layer (portname, low-level driver drvet name, card, number of axes on card)
+drvAsynMotorConfigure("PMAC1", "pmacAsynMotor", 0, 9)
+
+### Motors
+dbLoadTemplate  "motors.template"
+
+# A set of scan parameters for each positioner.  This is a convenience
+# for the user.  It can contain an entry for each scannable thing in the
+# crate.
+dbLoadTemplate "scanParms.template"
+
+### Allstop, alldone
+dbLoadRecords("$(MOTOR)/motorApp/Db/motorUtil.db","P=13PMAC1:")
+
+dbLoadRecords("$(ASYN)/db/asynRecord.db","P=HindsXray:,R=serial1,PORT=PMAC_IP,ADDR=0,OMAX=80,IMAX=80")
+
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=HindsXray:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+
+# Free-standing user string/number calculations (sCalcout records)
+dbLoadRecords("$(CALC)/calcApp/Db/userStringCalcs10.db", "P=HindsXray:")
+
+# Free-standing user transforms (transform records)
+dbLoadRecords("$(CALC)/calcApp/Db/userTransforms10.db", "P=HindsXray:")
+
+# Miscellaneous PV's, such as burtResult
+dbLoadRecords("$(STD)/stdApp/Db/misc.db", "P=HindsXray:")
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("HindsXray:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=HindsXray:")
+
+iocInit
+
+# save other things every thirty seconds
+create_monitor_set("auto_positions.req", 5, "P=HindsXray:")
+create_monitor_set("auto_settings.req", 30, "P=HindsXray:")
+
+### Start the saveData task.
+# saveData_MessagePolicy
+# 0: wait forever for space in message queue, then send message
+# 1: send message only if queue is not full
+# 2: send message only if queue is not full and specified time has passed (SetCptWait()
+#    sets this time.)
+# 3: if specified time has passed, wait for space in queue, then send message
+# else: don't send message
+#debug_saveData = 2
+saveData_MessagePolicy = 2
+saveData_SetCptWait_ms(100)
+saveData_Init("saveDataExtraPVs.req", "P=HindsXray:")
+#saveData_PrintScanInfo("HindsXray:scan1")
+
