84805e03702e06d4188f598ab6dbf7cde7167a42
Mark Rivers <rivers@cars.uchicago.edu>
2019-12-23 13:28:07 -0600

    New file

---
+++ iocBoot/ioc13MeasCompE1608_2/st.cmd
@@ -0,0 +1,40 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/CARSLinux.dbd"
+CARSLinux_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet(INPUT_POINTS, "4096")
+epicsEnvSet(OUTPUT_POINTS, "4096")
+
+epicsEnvSet("PREFIX", "13E1608_2:")
+
+cbAddBoard("E-1608", "164.54.160.96")
+
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      boardNum,        # The number of this board assigned by the Measurement Computing Instacal program 
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+MultiFunctionConfig("E1608_1", 0, $(INPUT_POINTS), $(OUTPUT_POINTS))
+
+#asynSetTraceMask E1608_1 -1 255
+
+dbLoadTemplate("E1608.substitutions")
+dbLoadTemplate("pid_slow.substitutions")
+
+< ../calc_GSECARS.iocsh
+
+### Scan-support software
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=1$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+
+< ../save_restore_IOCSH.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
+
+# Need to force the time arrays to process because the records are scan=I/O Intr
+# but asynPortDriver does not do array callbacks before iocInit.
+
+dbpf $(PREFIX)WaveDigDwell.PROC 1
---
+++ iocBoot/ioc13MeasCompE1608_2/start_epics.bat
@@ -0,0 +1,6 @@
+rem start medm -x -macro "P=13BMCPS1:,R=cam1:" prosilica.adl
+
+set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
+CARSApp st.cmd.windows
+pause
+
