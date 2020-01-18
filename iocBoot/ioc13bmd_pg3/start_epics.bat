cded8d9e12684e175f08cd77f1f79b94e483b8f8
Mark Rivers <rivers@cars.uchicago.edu>
2020-01-18 08:39:54 -0600

    Changed startup script

--- iocBoot/ioc13bmd_pg3/start_epics.bat
+++ iocBoot/ioc13bmd_pg3/start_epics.bat
@@ -1,6 +1,6 @@
 rem start medm -x -macro "P=13BMDPG3:, R=cam1:" ADSpinnaker.adl
 set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
-spinnakerApp st.cmd.BlackflyS_50S5C
+spinnakerApp st.cmd
 pause
 
 
