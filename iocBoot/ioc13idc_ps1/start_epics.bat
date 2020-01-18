47da02ad4de072c204d4b9a93851726d97ec50d1
Mark Rivers <rivers@cars.uchicago.edu>
2020-01-18 08:46:28 -0600

    Converted from ADProsilica to ADVimba

--- iocBoot/ioc13idc_ps1/start_epics.bat
+++ iocBoot/ioc13idc_ps1/start_epics.bat
@@ -1,4 +1,6 @@
-rem start medm -x -macro "P=13IDC_PS1:, R=cam1:" prosilica.adl
+rem start medm -x -macro "P=13IDC_PS1:, R=cam1:" ADVimba.adl
 set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
-prosilicaApp st.cmd
+vimbaApp st.cmd
+pause
+
 
