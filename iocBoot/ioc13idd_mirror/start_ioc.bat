0861909043c7b27c8fe3cec490eeda411e1ee6af
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-22 08:31:52 -0600

    Change from win32-x86-static to windows-x64-static

--- iocBoot/ioc13idd_mirror/start_ioc.bat
+++ iocBoot/ioc13idd_mirror/start_ioc.bat
@@ -1,5 +1,5 @@
-#start medm -x 13IDD_Mirror.adl
-set PATH=C:\epics_windows_binaries\win32-x86-static;%PATH%
+rem start medm -x 13IDD_Mirror.adl
+set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
 CARSApp.exe st.cmd
 pause
 
