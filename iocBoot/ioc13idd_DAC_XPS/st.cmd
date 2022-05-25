6b150e626e165577139c67a3804dc2a140f2339e
Mark Rivers <rivers@cars.uchicago.edu>
2022-05-25 16:41:25 -0500

    Changes for XPS-D

--- iocBoot/ioc13idd_DAC_XPS/st.cmd
+++ iocBoot/ioc13idd_DAC_XPS/st.cmd
@@ -3,7 +3,7 @@
 # erroneous "Interrupted system call" message on Linux OS.
 errlogInit(0)
 #
-dbLoadDatabase("../../dbd/CARSLinux.dbd")
+dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
 
@@ -13,52 +13,26 @@ CARSLinux_registerRecordDeviceDriver(pdbbase)
 # asyn port, IP address, IP port, number of axes, 
 # active poll period (ms), idle poll period (ms), 
 # enable set position, set position settling time (ms)
-XPSCreateController("XPS1", "newport-xps3", 5001, 8, 10, 500, 1, 500)
-asynSetTraceIOMask("XPS1", 0, 2)
+XPSCreateController("XPS1", "newport-xps16", 5001, 6, 10, 500, 1, 500)
+asynSetTraceIOMask("XPS1", 0, 6)
 #asynSetTraceMask("XPS1", 0, 255)
 
 # asynPort, IP address, IP port, poll period (ms)
-XPSAuxConfig("XPS_AUX1", "newport-xps3", 5001, 50)
+#XPSAuxConfig("XPS_AUX1", "newport-xps16", 5001, 50)
 #asynSetTraceIOMask("XPS_AUX1", 0, 2)
 #asynSetTraceMask("XPS_AUX1", 0, 255)
 
 # XPS asyn port,  axis, groupName.positionerName, stepSize
 XPSCreateAxis("XPS1",0,"G1.STX",  "10000")  
-XPSCreateAxis("XPS1",1,"G1.STZ",  "10000")  
-XPSCreateAxis("XPS1",2,"G1.STY",  "50000")  
-XPSCreateAxis("XPS1",3,"G1.OM",    "2000")  
-XPSCreateAxis("XPS1",4,"G2.SLX",  "10000")  
-# Share XPS axis 5 with "GPD X" PE Cell "Soller Z"
-# (In 13IDC use DB25 connector labled "m94 - Soller Z")
-# To change:
-# 1) Edit motor.template swap setup for m94
-# 2) Edit lines below, swap setup for axis 5
-# 3) FTP to the XPS the corresponding system.ini file
-#    a. copy to system.ini either:
-#        "system - Standard DAC and LVP Setup.ini" or
-#        "system Plug5 used for GPD X Stage.ini"
-#    b. from a corvette command prompt in the ioc13idd_DAC_XPS directory
-#         type: ftp_put_xps        
-# 4) Connect via web browser to "newport-xps3.cars.aps.anl.gov" and
-#      reboot the xps
-# 5) Under the DAC table switch the XPS connector A/B box with the
-#     yellow tape to output B
-#     
-# Used for normal DAC and LVP Setup
-XPSCreateAxis("XPS1",5,"G2.SLZ",  "10000")
-  
-# used when xps plug 5 is setup for gpd x stage
-#XPSCreateAxis("XPS1",5,"G4.GPD_X",  "10000")
- 
-# used when xps plug 5 is setup for gpd z stage
-#XPSCreateAxis("XPS1",5,"G4.GPD_Z",  "10000") 
-
-XPSCreateAxis("XPS1",6,"G2.SLT",  "10000")  
-XPSCreateAxis("XPS1",7,"G3.CZ",    "1000")  
+XPSCreateAxis("XPS1",1,"G2.STY",  "50000")  
+XPSCreateAxis("XPS1",2,"G3.STZ",  "10000")  
+XPSCreateAxis("XPS1",3,"G4.OM",  "10000")  
+XPSCreateAxis("XPS1",4,"G5.CLY",  "10000")  
+XPSCreateAxis("XPS1",5,"G6.CLX",  "10000")  
 
 # XPS asyn port,  max points, FTP username, FTP password
 # Note: this must be done after configuring axes
-XPSCreateProfile("XPS1", 2048, "Administrator", "Administrator")
+XPSCreateProfile("XPS1", 10010, "Administrator", "Administrator")
 
 # Disable setting position from motor record
 XPSEnableSetPosition(0) 
@@ -68,10 +42,10 @@ XPSEnableSetPosition(0)
 dbLoadTemplate("motors.template")
 
 # Auxillary I/O records
-dbLoadTemplate("XPSAux.substitutions")
+#dbLoadTemplate("XPSAux.substitutions")
 
 # asyn record for debugging
-drvAsynIPPortConfigure("xps", "newport-xps3:5001", 0, 0, 0)
+drvAsynIPPortConfigure("xps", "newport-xps16:5001", 0, 0, 0)
 asynSetTraceIOMask("xps",0,2)
 asynSetTraceMask("xps",0,9)
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=13IDD:, R=trajAsyn1, PORT=xps, ADDR=0, OMAX=300, IMAX=32000")
@@ -119,8 +93,8 @@ set_requestfile_path("$(SSCAN)",    "sscanApp/Db")
 set_requestfile_path("$(STD)",      "stdApp/Db")
 set_requestfile_path("$(VME)",      "vmeApp/Db")
 
-save_restoreSet_status_prefix("13IDD_XPS:")
-dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=13IDD_XPS:")
+save_restoreSet_status_prefix("13IDD_DAC_XPS16:")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=13IDD_DAC_XPS16:")
 
 # A set of scan parameters for each positioner.  This is a convenience
 # for the user.  It can contain an entry for each scannable thing in the
@@ -128,7 +102,7 @@ dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=13IDD_XPS:")
 dbLoadTemplate "scanParms.template"
 
 ### motorUtil - for allstop, moving, etc.
-dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=13IDD_XPS:")
+dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=13IDD_DAC_XPS16:")
 
 # User calc stuff
 epicsEnvSet("PREFIX", "13IDD:")
@@ -138,7 +112,7 @@ epicsEnvSet("PREFIX", "13IDD:")
 epicsEnvSet("ENGINEER", "Mark Rivers")
 epicsEnvSet("LOCATION","corvette")
 epicsEnvSet("GROUP","GSECARS")
-dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=13IDD_XPS:")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=13IDD_DAC_XPS16:")
 
 iocInit
 
@@ -161,16 +135,10 @@ dbpf "13IDD:userStringSeqEnable","1"
 
 # Set the NTM fields of the XPS motors to 0 (NO) so they don't get stopped when the motor changes direction due to PID
 dbpf("13IDD:m81.NTM","0")
-dbpf("13IDD:m82.NTM","0")
 dbpf("13IDD:m83.NTM","0")
+dbpf("13IDD:m82.NTM","0")
 dbpf("13IDD:m84.NTM","0")
-dbpf("13IDD:m93.NTM","0")
 dbpf("13IDD:m94.NTM","0")
-dbpf("13IDD:m95.NTM","0")
-dbpf("13IDD:m96.NTM","0")
+dbpf("13IDD:m93.NTM","0")
 
-motorUtilInit("13IDD_XPS:")
-
-# Enable the mode where the XPS determines axis move complete by socket response, 
-# not GroupStatusGet()
-XPSEnableMovingMode XPS1
+motorUtilInit("13IDD_DAC_XPS16:")
