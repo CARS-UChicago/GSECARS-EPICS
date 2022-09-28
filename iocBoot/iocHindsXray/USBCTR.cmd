ca94052b523ab43fa5344e37b1c9dbad3bff7b93
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-28 18:10:09 -0500

    Changes for new version of measComp

--- iocBoot/iocHindsXray/USBCTR.cmd
+++ iocBoot/iocHindsXray/USBCTR.cmd
@@ -1,25 +1,24 @@
 epicsEnvSet("USBCTR_PREFIX"             "$(PREFIX)USBCTR:")
 epicsEnvSet("MCS_PREFIX"                "$(USBCTR_PREFIX)MCS:")
+epicsEnvSet("UNIQUE_ID",                "01954963")
 epicsEnvSet("RNAME",                    "mca")
 epicsEnvSet("MAX_COUNTERS",             "9")
 epicsEnvSet("MAX_POINTS",               "2048")
 epicsEnvSet("PORT",                     "USBCTR")
+epicsEnvSet("POLL_TIME",                "0.01")
 # For MCA records FIELD=READ, for waveform records FIELD=PROC
 epicsEnvSet("FIELD",                    "PROC")
-epicsEnvSet("MODEL",                    "USBCTR-08")
-
-# This line is for Linux only
-cbAddBoard("USB-CTR", "")
 
 ## Configure port driver
 # USBCTRConfig(portName,       # The name to give to this asyn port driver
-#              boardNum,       # The number of this board assigned by the Measurement Computing Instacal program 
-#              maxTimePoints)  # Maximum number of time points for MCS
-USBCTRConfig("$(PORT)", 0, 2048, .01)
+#              uniqueID,       # Device serial number.
+#              maxTimePoints,  # Maximum number of time points for MCS
+#              pollTime,       # Time to sleep between polls
+USBCTRConfig("$(PORT)", "$(UNIQUE_ID)", $(MAX_POINTS), $(POLL_TIME))
 
 #asynSetTraceMask($(PORT), 0, TRACE_ERROR|TRACE_FLOW|TRACEIO_DRIVER)
 
-dbLoadTemplate("USBCTR.substitutions")
+dbLoadTemplate("$(MEASCOMP)/db/USBCTR.substitutions", "P=$(USBCTR_PREFIX), PORT=$(PORT)")
 
 # This loads the scaler record and supporting records
 dbLoadRecords("$(SCALER)/db/scaler.db", "P=$(USBCTR_PREFIX), S=scaler1, DTYP=Asyn Scaler, OUT=@asyn(USBCTR), FREQ=10000000")
@@ -28,7 +27,6 @@ dbLoadRecords("$(SCALER)/db/scaler.db", "P=$(USBCTR_PREFIX), S=scaler1, DTYP=Asy
 dbLoadRecords("$(MEASCOMP)/db/measCompMCS.template", "P=$(MCS_PREFIX), PORT=$(PORT), MAX_POINTS=$(MAX_POINTS)")
 
 # The number of records loaded must be the same as MAX_COUNTERS defined above
-
 dbLoadRecords("$(MCA)/db/SIS38XX_waveform.template", "P=$(MCS_PREFIX), R=$(RNAME)1,  INP=@asyn($(PORT) 0),  CHANS=$(MAX_POINTS)")
 dbLoadRecords("$(MCA)/db/SIS38XX_waveform.template", "P=$(MCS_PREFIX), R=$(RNAME)2,  INP=@asyn($(PORT) 1),  CHANS=$(MAX_POINTS)")
 dbLoadRecords("$(MCA)/db/SIS38XX_waveform.template", "P=$(MCS_PREFIX), R=$(RNAME)3,  INP=@asyn($(PORT) 2),  CHANS=$(MAX_POINTS)")
@@ -40,5 +38,4 @@ dbLoadRecords("$(MCA)/db/SIS38XX_waveform.template", "P=$(MCS_PREFIX), R=$(RNAME
 dbLoadRecords("$(MCA)/db/SIS38XX_waveform.template", "P=$(MCS_PREFIX), R=$(RNAME)9,  INP=@asyn($(PORT) 8),  CHANS=$(MAX_POINTS)")
 
 asynSetTraceIOMask($(PORT),0,2)
-#asynSetTraceFile("$(PORT)",0,"$(MODEL).out")
 
