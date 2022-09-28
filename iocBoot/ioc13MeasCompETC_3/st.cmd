cfa74539a43e7e79961f5ec0bcc027e34484d062
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-28 17:41:20 -0500

    New file

---
+++ iocBoot/ioc13MeasCompETC_3/st.cmd
@@ -0,0 +1,27 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/CARSLinux.dbd"
+CARSLinux_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet(PREFIX, "13ETC_3:")
+epicsEnvSet(PORT, "ETC_1")
+epicsEnvSet(UNIQUE_ID, "10.54.160.49")
+
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+MultiFunctionConfig("$(PORT)", "$(UNIQUE_ID)", 1, 1)
+
+#asynSetTraceMask $(PORT) -1 255
+
+dbLoadTemplate("$(MEASCOMP)/db/ETC.substitutions", "P=$(PREFIX), PORT=$(PORT)")
+
+< ../save_restore_IOCSH.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX))
+
