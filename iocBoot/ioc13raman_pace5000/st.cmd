ef292bc69c32f53a79c04d155256d54187aa0ccb
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 12:54:59 -0500

    Revert to using Ethernet rather than serial

--- iocBoot/ioc13raman_pace5000/st.cmd
+++ iocBoot/ioc13raman_pace5000/st.cmd
@@ -10,9 +10,13 @@ CARSLinux_registerRecordDeviceDriver(pdbbase)
 drvAsynIPPortConfigure("PACE5000", "gse-pace5000-4:5025", 0, 0, 0)
 asynOctetSetInputEos ("PACE5000",0,"\r\n")
 asynOctetSetOutputEos("PACE5000",0,"\r\n")
+#drvAsynIPPortConfigure("PACE5000", "gsets8:2001", 0, 0, 0)
+#asynOctetSetInputEos ("PACE5000",0,"\r")
+#asynOctetSetOutputEos("PACE5000",0,"\r")
 
 epicsEnvSet STREAM_PROTOCOL_PATH $(IP)/db
 dbLoadRecords("$(IP)/db/PACE5000.db", "P=$(PREFIX),R=PC1:,PORT=PACE5000")
+#dbLoadRecords("$(IP)/db/PACE5000_serial.db", "P=$(PREFIX),R=PC1:,PORT=PACE5000")
 
 < ../save_restore_IOCSH.cmd
 save_restoreSet_status_prefix("$(PREFIX)")
@@ -28,3 +32,8 @@ iocInit
 
 # save other things every thirty seconds
 create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+
+asynSetTraceIOMask(PACE5000 0, ESCAPE)
+asynSetTraceMask(PACE5000 0, ERROR)
+#asynSetTraceMask(PACE5000 0, ERROR|DRIVER)
+#asynSetTraceFile(PACE5000 0, 'idd_pace_debug.txt')
