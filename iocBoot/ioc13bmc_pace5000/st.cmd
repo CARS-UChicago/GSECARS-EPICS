eb5cde189372bbf191e16ad4ce5978449b711020
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 10:48:53 -0500

    Change to IP name, not address; set Trace masks

--- iocBoot/ioc13bmc_pace5000/st.cmd
+++ iocBoot/ioc13bmc_pace5000/st.cmd
@@ -7,7 +7,7 @@ epicsEnvSet("PREFIX", "13BMC_PACE5000:")
 dbLoadDatabase("../../dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
-drvAsynIPPortConfigure("PACE5000", "164.54.160.146:5025", 0, 0, 0)
+drvAsynIPPortConfigure("PACE5000", "gse-pace5000-3:5025", 0, 0, 0)
 asynOctetSetInputEos ("PACE5000",0,"\r\n")
 asynOctetSetOutputEos("PACE5000",0,"\r\n")
 
@@ -28,3 +28,8 @@ iocInit
 
 # save other things every thirty seconds
 create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+
+asynSetTraceIOMask(PACE5000 0, ESCAPE)
+asynSetTraceMask(PACE5000 0, ERROR)
+#asynSetTraceMask(PACE5000 0, ERROR|DRIVER)
+#asynSetTraceFile(PACE5000 0, 'idd_pace_debug.txt')
