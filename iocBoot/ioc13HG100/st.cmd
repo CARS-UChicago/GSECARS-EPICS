3eddb26d1c9dd3b55ad5c7f35297012610e096eb
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 12:34:09 -0500

    Change terminal server, use COM to configure; remove PID

--- iocBoot/ioc13HG100/st.cmd
+++ iocBoot/ioc13HG100/st.cmd
@@ -10,20 +10,26 @@ CARSLinux_registerRecordDeviceDriver(pdbbase)
 
 # Set up serial port on Moxa terminal server
 # Must be set for 9600, 8, 1, None.
-drvAsynIPPortConfigure("serial1", "GSETS8:2101", 0, 0, 1)
+#drvAsynIPPortConfigure("serial1", "GSETS8:2101", 0, 0, 1)
+drvAsynIPPortConfigure("serial1", "GSETS11:4001 COM", 0, 0, 1)
+asynSetOption(serial1, 0, baud, 9600)
+asynSetOption(serial1, 0, bits, 8)
+asynSetOption(serial1, 0, stop, 1)
+asynSetOption(serial1, 0, parity, none)
 
-asynSetTraceIOMask(serial1, 0, 4)
-#asynSetTraceMask(serial1, 0, 9)
 
 # Load asyn record
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX), R=HG100:Asyn, PORT=serial1, ADDR=0, OMAX=80, IMAX=80")
 
 dbLoadRecords("$(IP)/db/HG-100.db", "P=$(PREFIX), R=HG100:, PORT=serial1")
 
+asynSetTraceIOMask(serial1, 0, 4)
+asynSetTraceMask(serial1, 0, 9)
+
 <../calc_GSECARS.iocsh
 
 #PID slow
-dbLoadTemplate "pid_slow.substitutions"
+#dbLoadTemplate "pid_slow.substitutions"
 
 < ../save_restore_IOCSH.cmd
 save_restoreSet_status_prefix("$(PREFIX)")
