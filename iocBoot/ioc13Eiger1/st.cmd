f7a62276852fef3b40db489531035a22122eb200
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 12:29:10 -0500

    Change to eiger1.template; add ADEIGER to DB_INCLUDE_PATH

--- iocBoot/ioc13Eiger1/st.cmd
+++ iocBoot/ioc13Eiger1/st.cmd
@@ -13,12 +13,12 @@ epicsEnvSet("XSIZE",  "1030")
 epicsEnvSet("YSIZE",  "1065")
 epicsEnvSet("NCHANS", "8192")
 epicsEnvSet("CBUFFS", "500")
-epicsEnvSet("EIGERIP", "164.54.160.234")
-epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+epicsEnvSet("EIGERIP", "10.54.160.234")
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(ADEIGER)/db")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "5000000")
 
 eigerDetectorConfig("$(PORT)", "$(EIGERIP)", 0, 0)
-dbLoadRecords("$(ADEIGER)/db/eiger.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADEIGER)/db/eiger1.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Debug
 #asynSetTraceMask("$(PORT)", 0, 0x11)
