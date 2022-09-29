3cb64c0c3de77a755d1a0a7a9ba9f184527f70e1
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 13:05:03 -0500

    Change from 32 to 64 bit

--- iocBoot/ioc13Saturn1/START_IOC_WIN32.bat
+++ iocBoot/ioc13Saturn1/START_IOC_WIN32.bat
@@ -1,5 +1,5 @@
 rem start medm -x -macro "P=13Saturn1:, D=dxp1:, M=mca1"  dxpSaturn.adl
-set PATH=C:\epics_windows_binaries\win32-x86-static;%PATH%
+set PATH=C:\epics_windows_binaries\windows-x64-static;%PATH%
 dxpApp.exe st.cmd
 pause
 
