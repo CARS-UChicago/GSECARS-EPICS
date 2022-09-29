4d671c6e0c3a25ed85af24035168f606ba9a1c74
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 11:11:38 -0500

    Changed from terminal server with GPIB to Prologix and finally to VXI device

--- iocBoot/ioc13PECell/serial.cmd
+++ iocBoot/ioc13PECell/serial.cmd
@@ -1,44 +1,29 @@
 
 # BEGIN serial.cmd ------------------------------------------------------------
 
-# Set up 2 serial ports on terminal server
+# Set up 2 ports
 
-# serial 1 connected to Keithley2K DMM at 19200 baud
-drvAsynIPPortConfigure("serial1", "gsets11:4001 COM", 0, 0, 0)
-asynSetOption("serial1", 0, "baud", "19200")
-asynSetOption("serial1", 0, "bits", "8")
-asynSetOption("serial1", 0, "stop", "1")
-asynSetOption("serial1", 0, "parity", "none")
+# serial 1 connected to Keithley2K DMM over Ethernet
+drvAsynIPPortConfigure("serial1", "gsekeithley3:1394", 0, 0, 0)
 asynOctetSetOutputEos("serial1",0,"\r\n")
 asynOctetSetInputEos("serial1",0,"\r")
 asynSetTraceIOMask("serial1",0,2)
 #asynSetTraceMask("serial1",0,9)
 
 
-# serial 2 connected to Agilent at 19200 baud
-drvAsynIPPortConfigure("serial2", "gsets11:4002 COM", 0, 0, 0)
-asynSetOption(serial2,0,baud,19200)
-asynSetOption(serial2,0,bits,8)
-asynSetOption(serial2,0,stop,1)
-asynSetOption(serial2,0,parity,none)
-asynSetOption(serial2,0,crtscts,Y)
-#asynSetOption(serial2,0,clocal,N)
-
-#asynOctetSetInputEos(const char *portName, int addr,
-#                     const char *eosin,const char *drvInfo)
-asynOctetSetInputEos("serial2",0,"\n")
-# asynOctetSetOutputEos(const char *portName, int addr,
-#                       const char *eosin,const char *drvInfo)
-asynOctetSetOutputEos("serial2",0,"\n")
-asynSetTraceIOMask("serial2",0,2)
-#asynSetTraceMask("serial2",0,9)
+# serial 2 connected to Prologix Ethernet to GPIB conveter and an HP/Agilent power supply
+#prologixGPIBConfigure("serial2", "gse-prologix:1234", 0, 0)
+vxi11Configure("serial2", "164.54.160.16", 0, 0, "gpib0", 0, 0)
+# The power supply is at GPIB address 15, used in these asynTrace commands
+asynSetTraceIOMask("serial2",15,2)
+#asynSetTraceMask("serial2",15,9)
 
 # Keithley
 dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db", "P=13PECELL:,Dmm=DMM1:,PORT=serial1")
 
 # Agilent 66xxA GPIB (Power Supply)
-epicsEnvSet ("STREAM_PROTOCOL_PATH","$(CARS)/db")
-dbLoadRecords("$(CARS)/db/HP_AgilentPS66xxA_test.db","P=13PECELL:,S=PS1,PORT=serial2,ADDR=15")
+epicsEnvSet ("STREAM_PROTOCOL_PATH","$(IP)/db")
+dbLoadRecords("$(IP)/db/HP_Agilent_PS66xxA.db","P=13PECELL:,R=HP_PS1:,PORT=serial2,ADDR=15")
 
 # asyn record on each serial port
 dbLoadTemplate("asynRecord.substitutions")
