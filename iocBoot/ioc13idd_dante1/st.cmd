8593cf2b33daad915ab71ad0b7bf551b1dda78c4
Mark Rivers <rivers@cars.uchicago.edu>
2022-09-29 11:01:08 -0500

    Changes for new version of dante using its substitutions file

--- iocBoot/ioc13idd_dante1/st.cmd
+++ iocBoot/ioc13idd_dante1/st.cmd
@@ -24,13 +24,17 @@ epicsEnvSet("NCHANS", "2048")
 epicsEnvSet("CBUFFS", "500")
 # The maximum number of threads for plugins which can run in multiple threads
 epicsEnvSet("MAX_THREADS", "8")
+# The maximum number of channels in the MCA records
+epicsEnvSet("MCA_CHANS", 4096)
+# The maximum number of points in the ADC trace waveform records
+epicsEnvSet("TRACE_LEN", 16384)
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # DanteConfig(portName, ipAddress, numDetectors, maxBuffers, maxMemory)
 DanteConfig("$(PORT)",  164.54.160.182, 1, 0, 0)
 
-dbLoadTemplate("dante.substitutions")
+dbLoadTemplate($(DANTE)/db/"dante1.substitutions", "P=$(PREFIX), NCHAN=$(MCA_CHANS), TRACE_LEN=$(TRACE_LEN)")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
@@ -43,3 +47,15 @@ iocInit
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
 create_monitor_set("auto_settings.req", 30, "P=$(PREFIX), R=dante1:, M=mca1")
+
+# Wait 1 seconds for all records with PINI=YES to process
+epicsThreadSleep(1)
+
+# Enable downloading configurations
+dbpf("$(PREFIX)dante1:EnableConfigure", "Enable")
+
+# There is a problem with the GatingMode record.
+# Even though it has PINI=YES and has already processed and written the value to the Dante,
+# the value appears to have been changed, and it need to be processed again.  
+# This needs to be tracked down.
+dbpf("$(PREFIX)dante1:GatingMode.PROC", "1")
