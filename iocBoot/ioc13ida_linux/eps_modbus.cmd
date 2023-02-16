4d676cc820d5e62c83c0bf6e762af0f5fc372e31
Mark Rivers <rivers@cars.uchicago.edu>
2023-02-16 11:17:25 -0600

    Increase to 300 input bits; change eps_valid.db to eps_valid.template

--- iocBoot/ioc13ida_linux/eps_modbus.cmd
+++ iocBoot/ioc13ida_linux/eps_modbus.cmd
@@ -16,8 +16,8 @@ modbusInterposeConfig("$(PORT)",0,5000,0)
 # Read 10 words at address 100.  Function code=3.
 drvModbusAsynConfigure("MVI46_In_Word", "$(PORT)", 0, 3, 100, 10, 0,  100, "MVI46")
 
-# Read 200 bits starting at address 100.  Function code=1. MVI46 seems to interpret start address in bits, not works.
-drvModbusAsynConfigure("MVI46_In_Bit", "$(PORT)", 0, 1, 1600, 200, 0,  100, "MVI46")
+# Read 300 bits starting at address 100.  Function code=1. MVI46 seems to interpret start address in bits, not works.
+drvModbusAsynConfigure("MVI46_In_Bit", "$(PORT)", 0, 1, 1600, 300, 0,  100, "MVI46")
 
 # Write 10 words at address 0.  Function code=6.
 drvModbusAsynConfigure("MVI46_Out_Word", "$(PORT)", 0, 6, 0, 10, 0, 100, "MVI46")
@@ -25,7 +25,7 @@ drvModbusAsynConfigure("MVI46_Out_Word", "$(PORT)", 0, 6, 0, 10, 0, 100, "MVI46"
 # Write 200 bits starting at address 0.  Function code=5.
 drvModbusAsynConfigure("MVI46_Out_Bit", "$(PORT)", 0, 5, 0, 200, 0, 100, "MVI46")
 
-dbLoadRecords("$(CARS)/db/eps_valid.db", "P=$(PREFIX)")
+dbLoadRecords("$(CARS)/db/eps_valid.template", "P=$(PREFIX)")
 dbLoadTemplate("eps_inputs.substitutions", "P=$(PREFIX), PORT=MVI46_In_Bit")
 dbLoadTemplate("eps_outputs.substitutions", "P=$(PREFIX), PORT=MVI46_Out_Bit")
 dbLoadTemplate("eps_valves.substitutions", "P=$(PREFIX), IN_PORT=MVI46_In_Bit, OUT_PORT=MVI46_Out_Bit")
