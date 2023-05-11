043dd2ff0b0220cb59561ae23750e0d58c142a54
Mark Rivers <rivers@cars.uchicago.edu>
2023-05-11 09:59:33 -0500

    Add second unit

--- iocBoot/ioc13ida_linux/MeasComp.cmd
+++ iocBoot/ioc13ida_linux/MeasComp.cmd
@@ -1,10 +1,27 @@
+# E1608 #1
+epicsEnvSet("PORT",          "E1608_1")
+epicsEnvSet("UNIQUE_ID",     "gse-e1608-2.cars.aps.anl.gov")
+epicsEnvSet("MAX_POINTS",    "4096")
+epicsEnvSet("E1608_PREFIX_1",  "$(P)E1608_1:")
 ## Configure port driver
 # MultiFunctionConfig((portName,        # The name to give to this asyn port driver
 #                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
 #                      maxInputPoints,  # Maximum number of input points for waveform digitizer
 #                      maxOutputPoints) # Maximum number of output points for waveform generator
-MultiFunctionConfig(E1608_1, gse-e1608-3, 4096, 1)
-MultiFunctionConfig(E1608_2, gse-e1608-1, 4096, 1)
 
-dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(P)E1608_1:, PORT=E1608_1, WDIG_POINTS=4096")
-dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(P)E1608_2:, PORT=E1608_2, WDIG_POINTS=4096")
+MultiFunctionConfig($(PORT), $(UNIQUE_ID), $(MAX_POINTS), 1)
+dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(E1608_PREFIX_1), PORT=$(PORT), WDIG_POINTS=$(MAX_POINTS)")
+
+# E1608 #2
+epicsEnvSet("PORT",          "E1608_2")
+epicsEnvSet("UNIQUE_ID",     "gse-e1608-3.cars.aps.anl.gov")
+epicsEnvSet("MAX_POINTS",    "4096")
+epicsEnvSet("E1608_PREFIX_2",  "$(P)E1608_2:")
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+
+MultiFunctionConfig($(PORT), $(UNIQUE_ID), $(MAX_POINTS), 1)
+dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(E1608_PREFIX_2), PORT=$(PORT), WDIG_POINTS=$(MAX_POINTS)")
