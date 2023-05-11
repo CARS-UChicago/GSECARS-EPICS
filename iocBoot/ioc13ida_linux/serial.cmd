92fea587dd9e476978e62dceb22dadf473971369
Mark Rivers <rivers@cars.uchicago.edu>
2023-05-11 10:17:08 -0500

    Disable ILM, add MKS

--- iocBoot/ioc13ida_linux/serial.cmd
+++ iocBoot/ioc13ida_linux/serial.cmd
@@ -8,13 +8,15 @@ iocshLoad ../asynIPPortConfig.cmd "PORT=serial6,  IPADDR=$(TS):4006, IEOS=,     
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial7,  IPADDR=$(TS):4007, IEOS=\\r,     OEOS=\\r"   # MPC         9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial8,  IPADDR=$(TS):4008, IEOS=\\r,     OEOS=\\r"   # MPC         9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial9,  IPADDR=$(TS):4009, IEOS=\\n,     OEOS=\\r"   # Keithley   19200,'N',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial10, IPADDR=$(TS):4010, IEOS=\\r,     OEOS=\\r"   # Oxford ILM cryometer; C/D mono; 9600,'N',1,8,'N'
+#iocshLoad ../asynIPPortConfig.cmd "PORT=serial10, IPADDR=$(TS):4010, IEOS=\\r,     OEOS=\\r"   # Oxford ILM cryometer; C/D mono; 9600,'N',1,8,'N'
+iocshLoad ../asynIPPortConfig.cmd "PORT=serial10, IPADDR=$(TS):4010, IEOS=\\r,     OEOS=\\r"   # MKS        19200,'E',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial11, IPADDR=$(TS):4011, IEOS=\\r,     OEOS=\\r"   # MKS        19200,'E',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial12, IPADDR=$(TS):4012, IEOS=\\r,     OEOS=\\r"   # MPC         9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial13, IPADDR=$(TS):4013, IEOS=\\n,     OEOS=\\r"   # Keithley 2000; C/D vertical mirror temps; 19200,'N',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"   # Oxford ILM cryometer; E mono;             9600,'N',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial15, IPADDR=$(TS):4015, IEOS=\\r,     OEOS=\\r"   # MKS        19200,'E',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial16, IPADDR=$(TS):4016, IEOS=\\r,     OEOS=\\r"   # SR-570; E WBS/BPM blade current; 9600,'N',1,8,'N'
+#iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"   # Oxford ILM cryometer; E mono;             9600,'N',1,8,'N'
+iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"   # SR-570; E WBS/BPM blade current; 9600,'N',1,8,'N'
+# Serial 15 is connection to VME crate
+# Serial 16 is COM port to Allen-Bradley PLC
 #iocshLoad ../asynIPPortConfig.cmd "PORT=serial17, IPADDR=$(TS):4017, IEOS=\\r,     OEOS=\\r"   # SR-570; C/D pinhole BPM blade current;  9600,'N',1,8,'N'
 
 
@@ -31,15 +33,15 @@ dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial7,  PUMP=ip4, 
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial8,  PUMP=ip9,   PA=0 ,PN=1")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial8,  PUMP=ip10, PORT=serial8, PA=0, PN=2")
 dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db", "P=$(P), PORT=serial9,  Dmm=DMM1")
-dbLoadRecords("$(CARS)/db/ILM200.db",         "P=$(P), PORT=serial10, R=ILM200_1,")
+#dbLoadRecords("$(CARS)/db/ILM200.db",         "P=$(P), PORT=serial10, R=ILM200_1,")
+dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial10, CC1=cc9, CC2=cc10, PR1=pr9, PR2=pr10")
 dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial11, CC1=cc7, CC2=cc8, PR1=pr7, PR2=pr8")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial12, PUMP=ip8,  PA=0, PN=1")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial12, PUMP=ip11, PA=0, PN=2")
 dbLoadRecords("$(IP)/db/TSP.db",              "P=$(P), PORT=serial12, TSP=tsp1,  PA=0")
 dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db", "P=$(P), PORT=serial13, Dmm=DMM2")
-dbLoadRecords("$(CARS)/db/ILM200.db",         "P=$(P), PORT=serial14, R=ILM200_2")
-dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial15, CC1=cc9, CC2=cc10, PR1=pr9, PR2=pr10")
-dbLoadRecords("$(IP)/db/SR570.db",            "P=$(P), PORT=serial16, A=A1,")
+#dbLoadRecords("$(CARS)/db/ILM200.db",         "P=$(P), PORT=serial14, R=ILM200_2")
+dbLoadRecords("$(IP)/db/SR570.db",            "P=$(P), PORT=serial14, A=A1,")
 #dbLoadRecords("$(IP)/db/SR570.db",            "P=$(P), PORT=serial17, A=A2,")
 
 # Load asyn records on all serial ports
@@ -50,6 +52,6 @@ asynSetTraceMask("serial7",0,0)
 asynSetTraceMask("serial8",0,0)
 asynSetTraceMask("serial12",0,0)
 
-asynSetTraceMask("serial11",0,ERROR|DRIVER)
 asynSetTraceIOMask("serial11",0,ESCAPE)
+#asynSetTraceMask("serial11",0,ERROR|DRIVER)
 
