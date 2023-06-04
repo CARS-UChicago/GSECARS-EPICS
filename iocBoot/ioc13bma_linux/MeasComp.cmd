5eff7ae66afe047838919c73e0ea9f36053f5482
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-04 11:36:18 -0500

    Use environment variables

--- iocBoot/ioc13bma_linux/MeasComp.cmd
+++ iocBoot/ioc13bma_linux/MeasComp.cmd
@@ -1,8 +1,13 @@
+# E1608
+epicsEnvSet("PORT",          "E1608")
+epicsEnvSet("UNIQUE_ID",     "gse-e1608-1.cars.aps.anl.gov")
+epicsEnvSet("MAX_POINTS",    "4096")
+epicsEnvSet("E1608_PREFIX",  "$(P)E1608:")
 ## Configure port driver
 # MultiFunctionConfig((portName,        # The name to give to this asyn port driver
 #                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
 #                      maxInputPoints,  # Maximum number of input points for waveform digitizer
 #                      maxOutputPoints) # Maximum number of output points for waveform generator
-MultiFunctionConfig(E1608_1, 10.54.160.96, 4096, 1)
 
-dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(P)E1608_1:, PORT=E1608_1, WDIG_POINTS=4096")
+MultiFunctionConfig($(PORT), $(UNIQUE_ID), $(MAX_POINTS), 1)
+dbLoadTemplate("$(MEASCOMP)/db/E1608.substitutions", "P=$(E1608_PREFIX), PORT=$(PORT), WDIG_POINTS=$(MAX_POINTS)")
