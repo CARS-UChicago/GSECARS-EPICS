f25092ad7d86be9cb9d5566d7da3164285673775
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-04 08:46:58 -0500

    Add motors, EDIO24, devIocStats; fix PREFIX

--- iocBoot/ioc13idd_linux/st.cmd
+++ iocBoot/ioc13idd_linux/st.cmd
@@ -4,16 +4,30 @@
 dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
-epicsEnvSet("PREFIX", "13IDD_TEST:")
+epicsEnvSet("PREFIX", "13IDD:")
+epicsEnvSet("P", "13IDD:")
+epicsEnvSet("LINUX_PREFIX", "13IDD_Linux:")
 
 # Tell StreamDevice where to find protocol files
 iocshCmd("epicsEnvSet(STREAM_PROTOCOL_PATH, $(IP)/db:$(DELAYGEN)/db:$(CARS)/db)")
 
-iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets19")
-iocshLoad("MeasComp.cmd",   "P=$(PREFIX)")
-iocshLoad("Koyo.cmd",       "P=$(PREFIX)LaserPLC:")
+iocshLoad("serial.cmd",          "P=$(PREFIX), TS=gsets19")
+iocshLoad("MeasComp_EDIO24.cmd", "P=$(PREFIX)")
+iocshLoad("Koyo.cmd",            "P=$(PREFIX)LaserPLC:")
+iocshLoad("motors.cmd",          "P=$(PREFIX)")
 
-#dbLoadTemplate("motors.template")
+# User calc stuff
+iocshLoad("../calc_GSECARS.iocsh", "P=$(PREFIX))
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(LINUX_PREFIX)")
+
+# devIocStats
+epicsEnvSet("ENGINEER", "Mark Rivers")
+epicsEnvSet("LOCATION","13IDD roof")
+epicsEnvSet("GROUP","GSECARS")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(LINUX_PREFIX)")
 
 iocInit
 
@@ -25,5 +39,5 @@ iocInit
 # save positions every five seconds
 create_monitor_set("auto_positions.req",5,"P=$(PREFIX)")
 # save other things every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), EDIO24_PREFIX=$(EDIO24_PREFIX)")
 
