26bf6a4a3a904583be007f6ce401757afde6d231
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-04 11:30:35 -0500

    Remove everything except motors

--- iocBoot/ioc13bma/st.cmd
+++ iocBoot/ioc13bma/st.cmd
@@ -14,59 +14,19 @@ errlogInit(20000)
 dbLoadDatabase("$(CARS)/dbd/CARSVX.dbd")
 CARSVX_registerRecordDeviceDriver(pdbbase)
 
-# This IOC loads the MPF server code locally
 cd startup
-#< industryPack.cmd
-#< serial.cmd
 
 devPM304Debug = 0
 drvPM304Debug = 0
 
-# Set up the Allen-Bradley 6008 scanner
-#abConfigNlinks 1
-#abConfigVme 0,0xc00000,0x60,5
-#abConfigAuto
-
-# Load database
-#dbLoadRecords("$(CARS)/db/eps_valid.db", "P=13BMA:")
-#dbLoadTemplate("eps_inputs.template")
-#dbLoadTemplate("eps_outputs.template")
-#dbLoadTemplate("eps_valves.template")
-
 dbLoadTemplate("motors.template")
-
-# BMD and BMC filter racks
-dbLoadRecords("$(CARS)/db/13BMC_Filters.db","P=13BMA:,R=BMC_Filters,MOTOR=m6")
-dbLoadRecords("$(CARS)/db/13BMD_Filters.db","P=13BMA:,R=BMD_Filters,MOTOR=m5")
-
-# Monochromator PID
-dbLoadTemplate "mono_pid.template"
-
-# Auto-shutters
-#dbLoadTemplate("auto_shutter.substitutions")
 
 ### Allstop, alldone
 dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=13BMA:")
 
-### Scan-support software
-# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
-# 1D data, but it doesn't store anything to disk.  (You need the data catcher
-# or the equivalent for that.)  This database is configured to use the
-# "alldone" database (above) to figure out when motors have stopped moving
-# and it's time to trigger detectors.
-#dbLoadRecords("$(SSCAN)/db/scan.db","P=13BMA:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
-
 # A set of scan parameters for each positioner.  This is a convenience
-# for the user.  It can contain an entry for each scannable thing in the
-# crate.
-#dbLoadTemplate("scanParms.template")
-
-# User calc stuff
-epicsEnvSet("PREFIX", "13BMA:")
-iocsh("../calc_GSECARS.iocsh")
-
-# Miscellaneous PV's
-#dbLoadRecords("$(STD)/db/misc.db","P=13BMA:")
+# for the user.  It can contain an entry for each scannable thing in the crate.
+dbLoadTemplate("scanParms.template")
 
 # devIocStats
 putenv("ENGINEER=Mark Rivers")
@@ -103,21 +63,9 @@ create_monitor_set("auto_positions.req",5,"P=13BMA:")
 # save other things every thirty seconds
 create_monitor_set("auto_settings.req",30,"P=13BMA:")
 
-#seq &Keithley2kDMM, "P=13BMA:, Dmm=DMM1, stack=10000"
-#seq &Keithley2kDMM, "P=13BMA:, Dmm=DMM2, stack=10000"
-
-# Enable user string calcs and user transforms
-#dbpf "13BMA:EnableUserTrans.PROC","1"
-#dbpf "13BMA:EnableUserSCalcs.PROC","1"
-#dbpf "13BMA:EnableuserACalcs.PROC","1"
-
-
-seq &BM13_Energy, "E=13BMA:E, MONO=13BMA:m17, EXPTAB_Z=13BMD:m22, YXTAL=13BMA:MON:, ZXTAL=13BMA:m14" 
-
 # Mn 20/Mar/02  see note in ioc13ida st.cmd
 #  this reduces the readback following error for the McLennan mono controller.
 (double) drvPM304ReadbackDelay = 0.25
-
 
 motorUtilInit("13BMA:")
 
