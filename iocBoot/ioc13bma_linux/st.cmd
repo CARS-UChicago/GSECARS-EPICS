13a69b855e099cd819e485349358d047a4138a2e
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-04 11:37:50 -0500

    Enable Keithley, BM_13 SNL program, sscan, devIocStats, etc.

--- iocBoot/ioc13bma_linux/st.cmd
+++ iocBoot/ioc13bma_linux/st.cmd
@@ -5,22 +5,23 @@ dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
 epicsEnvSet("PREFIX", "13BMA:")
+epicsEnvSet("LINUX_PREFIX", "13BMA_Linux:")
 
 iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets16")
-#iocshLoad("eps_modbus.cmd", "P=$(PREFIX), PORT=MVI146_1, IPADDR=gse-mvi46-mnet-1")
-#iocshLoad("MeasComp.cmd",   "P=$(PREFIX)")
+iocshLoad("eps_modbus.cmd", "P=$(PREFIX), PORT=MVI146_1, IPADDR=gse-mvi46-mnet-1")
+iocshLoad("MeasComp.cmd",   "P=$(PREFIX)")
 
 #dbLoadTemplate("motors.template")
 
 # BMD and BMC filter racks
-#dbLoadRecords("$(CARS)/db/13BMC_Filters.db","P=$(PREFIX),R=BMC_Filters,MOTOR=m6")
-#dbLoadRecords("$(CARS)/db/13BMD_Filters.db","P=$(PREFIX),R=BMD_Filters,MOTOR=m5")
+dbLoadRecords("$(CARS)/db/13BMC_Filters.db","P=$(PREFIX),R=BMC_Filters,MOTOR=m6")
+dbLoadRecords("$(CARS)/db/13BMD_Filters.db","P=$(PREFIX),R=BMD_Filters,MOTOR=m5")
 
 # Monochromator PID
-#dbLoadTemplate "mono_pid.template"
+dbLoadTemplate "mono_pid.substitutions"
 
 # Auto-shutters
-#dbLoadTemplate("auto_shutter.substitutions")
+dbLoadTemplate("auto_shutter.substitutions")
 
 ### Allstop, alldone
 #dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(PREFIX)")
@@ -34,25 +35,24 @@ iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets16")
 dbLoadRecords("$(SSCAN)/db/scan.db","P=$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
 
 # A set of scan parameters for each positioner.  This is a convenience
-# for the user.  It can contain an entry for each scannable thing in the
-# crate.
-#dbLoadTemplate("scanParms.template")
+# for the user.  It can contain an entry for each scannable thing in the crate.
+dbLoadTemplate("scanParms.substitutions")
 
 # User calc stuff
-#<../calc_GSECARS.iocsh
+iocshLoad("../calc_GSECARS.iocsh", "P=($PREFIX)")
 
 # Miscellaneous PV's
-#dbLoadRecords("$(STD)/db/misc.db","P=$(PREFIX)")
+dbLoadRecords("$(STD)/db/misc.db","P=$(PREFIX)")
 
 # devIocStats
-#epicsEnvSet("ENGINEER", "Mark Rivers")
-#epicsEnvSet("LOCATION", "13-BM-A roof")
-#epicsEnvSet("GROUP", "GSECARS")
-#dbLoadRecords("$(DEVIOCSTATS)/db/iocAdmin.db","IOC=$(PREFIX)")
+epicsEnvSet("ENGINEER", "Mark Rivers")
+epicsEnvSet("LOCATION", "13-BM-A roof")
+epicsEnvSet("GROUP", "GSECARS")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(LINUX_PREFIX)")
 
 < ../save_restore_IOCSH.cmd
-#save_restoreSet_status_prefix("$(PREFIX)")
-#dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(PREFIX)")
+save_restoreSet_status_prefix("$(LINUX_PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(LINUX_PREFIX)")
 
 
 iocInit
@@ -65,10 +65,10 @@ iocInit
 # save positions every five seconds
 create_monitor_set("auto_positions.req",5,"P=$(PREFIX)")
 # save other things every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), E1608_PREFIX=$(E1608_PREFIX)")
 
-#seq &Keithley2kDMM, "P=$(PREFIX), Dmm=DMM1, stack=10000"
-#seq &Keithley2kDMM, "P=$(PREFIX), Dmm=DMM2, stack=10000"
+seq &Keithley2kDMM, "P=$(PREFIX), Dmm=DMM1, stack=10000"
+seq &Keithley2kDMM, "P=$(PREFIX), Dmm=DMM2, stack=10000"
 
 # Enable user string calcs and user transforms
 #dbpf "$(PREFIX)EnableUserTrans.PROC","1"
@@ -76,11 +76,12 @@ create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
 #dbpf "$(PREFIX)EnableuserACalcs.PROC","1"
 
 
-#seq &BM13_Energy, "E=$(PREFIX)E, MONO=$(PREFIX)m17, EXPTAB_Z=13BMD:m22, YXTAL=$(PREFIX)MON:, ZXTAL=$(PREFIX)m14" 
+# MONO should be m9, but that is causing problems when the controller is off. Use unused m9 for testing.
+seq BM13_Energy, "E=$(PREFIX)E, MONO=$(PREFIX)m9, EXPTAB_Z=13BMD:m22, YXTAL=$(PREFIX)MON:, ZXTAL=$(PREFIX)m14" 
 
 # Mn 20/Mar/02  see note in ioc13ida st.cmd
 #  this reduces the readback following error for the McLennan mono controller.
-var drvPM304ReadbackDelay 0.25
+#var drvPM304ReadbackDelay 0.25
 
 #motorUtilInit("$(PREFIX)")
 
