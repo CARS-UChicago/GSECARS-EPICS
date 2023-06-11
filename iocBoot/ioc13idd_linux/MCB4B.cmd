c288d0684860511ad940492968121eff0b07b9bf
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-11 15:45:38 -0500

    New file

---
+++ iocBoot/ioc13idd_linux/MCB4B.cmd
@@ -0,0 +1,13 @@
+# MCB-4B driver setup parameters:
+#     (1) maximum # of controllers,
+#     (2) maximum # axis per controller
+#     (3) motor task polling rate (min=1Hz, max=60Hz)
+MCB4BSetup(2, 4, 10)
+
+# MCB-4B driver configuration parameters:
+#     (1) controller
+#     (2) asyn port name (e.g. serial1)
+MCB4BConfig(0, "serial17")
+MCB4BConfig(1, "serial18")
+
+dbLoadTemplate("MCB4B_motors.template")
