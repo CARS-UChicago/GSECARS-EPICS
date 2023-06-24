13d93b3cd289050810cb28c5460e90bf05f5e15c
Mark Rivers <rivers@cars.uchicago.edu>
2023-06-24 15:38:00 -0500

    Change terminal server; add calc_GSECARS, sscan, devIocStats, fix autosave

--- iocBoot/ioc13ide_linux/st.cmd
+++ iocBoot/ioc13ide_linux/st.cmd
@@ -4,13 +4,33 @@
 dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
-epicsEnvSet("PREFIX", "13IDE_TEST:")
+epicsEnvSet("PREFIX", "13IDE:")
+epicsEnvSet("LINUX_PREFIX", "13IDE_Linux:")
+#epicsEnvSet("P", "13IDE:")
 
-iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets20")
+iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets2")
 iocshLoad("MeasComp.cmd",   "P=$(PREFIX)")
 
+# User calc stuff
+iocshLoad("../calc_GSECARS.iocsh", "P=$(PREFIX)")
 
-#dbLoadTemplate("motors.template")
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/db/scan.db","P=$(PREFIX),MAXPTS1=2000,MAXPTS2=2000,MAXPTS3=100,MAXPTS4=100,MAXPTSH=2048")
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(LINUX_PREFIX)")
+
+# devIocStats
+epicsEnvSet("ENGINEER", "Mark Rivers")
+epicsEnvSet("LOCATION","Gas loading system")
+epicsEnvSet("GROUP","GSECARS")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(LINUX_PREFIX)")
 
 iocInit
 
@@ -22,5 +42,5 @@ iocInit
 # save positions every five seconds
 create_monitor_set("auto_positions.req", 5, "P=$(PREFIX)")
 # save other things every thirty seconds
-create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+create_monitor_set("auto_settings.req", 30, "P=$(PREFIX), P3104=$(USB3104_PREFIX), P1808=$(USB1808_PREFIX), PCTR=$(USBCTR_PREFIX), MP=$(MCS_PREFIX), SP=$(SCALER_PREFIX)"
 
