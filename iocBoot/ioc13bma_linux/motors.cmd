5dc97a1b0399b0ab540e0515575ad4e3a9443db4
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-20 09:46:31 -0600

    New file

---
+++ iocBoot/ioc13bma_linux/motors.cmd
@@ -0,0 +1,9 @@
+iocshLoad("Galil.cmd", "P=$(P)")
+
+### Allstop, alldone
+#dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(P)")
+
+# A set of scan parameters for each positioner.  This is a convenience
+# for the user.  It can contain an entry for each scannable thing in the
+# crate.
+#dbLoadTemplate("scanParms.template")
