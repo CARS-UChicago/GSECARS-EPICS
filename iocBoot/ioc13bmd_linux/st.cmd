975da56a3630bbf8b7425d1bd3364a244a0706b4
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-20 11:28:47 -0600

    Many changes

--- iocBoot/ioc13bmd_linux/st.cmd
+++ iocBoot/ioc13bmd_linux/st.cmd
@@ -1,25 +1,43 @@
-# Linux startup script for 13-BM-D
+# Linux startup script for 13-ID-D
 < envPaths
 
 dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
 CARSLinux_registerRecordDeviceDriver(pdbbase)
 
 epicsEnvSet("PREFIX", "13BMD:")
+epicsEnvSet("P", "13BMD:")
 epicsEnvSet("LINUX_PREFIX", "13BMD_Linux:")
 
-#iocshLoad("serial.cmd", "P=$(PREFIX), TS=gsets16")
-iocshLoad("MeasComp_EDIO24.cmd", "P=$(PREFIX)")
-#iocshLoad("Koyo.cmd", "P=$(PREFIX)LaserPLC:")
+# Tell StreamDevice where to find protocol files
+iocshCmd("epicsEnvSet(STREAM_PROTOCOL_PATH, $(IP)/db:$(DELAYGEN)/db:$(CARS)/db)")
 
-#dbLoadTemplate("motors.template")
+iocshLoad("serial.cmd",          "P=$(PREFIX), TS=gsets18")
+iocshLoad("MeasComp.cmd",        "P=$(PREFIX)")
+iocshLoad("Koyo.cmd",            "P=$(PREFIX)")
+#iocshLoad("motors.cmd",          "P=$(PREFIX)")
+
+dbLoadTemplate "DAC_ExternalHeating.template"
+dbLoadTemplate "DAC_SparePID.template"
+dbLoadTemplate "LVP_furnace_control.template"
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/db/scan.db","P=$(PREFIX),MAXPTS1=2000,MAXPTS2=2000,MAXPTS3=100,MAXPTS4=100,MAXPTSH=2048")
+
+# User calc stuff
+iocshLoad("../calc_GSECARS.iocsh", "P=$(PREFIX)")
 
 < ../save_restore_IOCSH.cmd
-save_restoreSet_status_prefix("$(LINUX_PREFIX)")
+save_restoreSet_status_prefix("$(PREFIX)")
 dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(LINUX_PREFIX)")
 
 # devIocStats
 epicsEnvSet("ENGINEER", "Mark Rivers")
-epicsEnvSet("LOCATION","13-BM-D")
+epicsEnvSet("LOCATION","13BMD roof")
 epicsEnvSet("GROUP","GSECARS")
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(LINUX_PREFIX)")
 
@@ -33,5 +51,4 @@ iocInit
 # save positions every five seconds
 create_monitor_set("auto_positions.req",5,"P=$(PREFIX)")
 # save other things every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=$(PREFIX),EDIO24_PREFIX=$(EDIO24_PREFIX)")
-
+create_monitor_set("auto_settings.req", 30, "P=$(PREFIX),P3104=$(USB3104_PREFIX),P1808=$(USB1808_PREFIX),PEDIO24=$(EDIO24_PREFIX),PCTR=$(USBCTR_PREFIX), MP=$(MCS_PREFIX), SP=$(SCALER_PREFIX)"
