56ed9b86833e7488388d5db4de712a0d4f007123
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-20 18:13:56 -0600

    Enable scanParms; use doAfterIocInit

--- iocBoot/ioc13bma_linux/motors.cmd
+++ iocBoot/ioc13bma_linux/motors.cmd
@@ -1,9 +1,10 @@
 iocshLoad("Galil.cmd", "P=$(P)")
 
 ### Allstop, alldone
-#dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(P)")
+dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(P)")
+doAfterIocInit "motorUtilInit($(P))"
 
 # A set of scan parameters for each positioner.  This is a convenience
 # for the user.  It can contain an entry for each scannable thing in the
 # crate.
-#dbLoadTemplate("scanParms.template")
+dbLoadTemplate("scanParms.template")
