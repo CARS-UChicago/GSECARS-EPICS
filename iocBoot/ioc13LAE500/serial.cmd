f3b03b001516e9d540ad64fc6878a42d10a6498f
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-21 11:26:28 -0600

    Change terminal server used

--- iocBoot/ioc13LAE500/serial.cmd
+++ iocBoot/ioc13LAE500/serial.cmd
@@ -4,7 +4,7 @@
 
 #drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
 #                        noProcessEos)
-drvAsynIPPortConfigure("serial1", "gsets7.cars.aps.anl.gov:2101", 0, 0, 0)
+drvAsynIPPortConfigure("serial1", "gsets14.cars.aps.anl.gov:2101", 0, 0, 0)
 
 
 asynOctetConnect("serial1", "serial1")
