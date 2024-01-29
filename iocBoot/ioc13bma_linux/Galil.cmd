156361f1e60f2f11d5f147a3ca1a06bd91983c9e
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-29 13:01:06 -0600

    Convert all axes to Galil

--- iocBoot/ioc13bma_linux/Galil.cmd
+++ iocBoot/ioc13bma_linux/Galil.cmd
@@ -1,5 +1,9 @@
 # gse-galil11  In BM-A rack, Step-Pak; Monochromator
-iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil1:, PORT=GALIL1, IPADDR=10.54.160.119, M1=m9,  M2=m10,  M3=m11,  M4=m12,  M5=m13,  M6=m14,  M7=m15,  M8=m16")  
+iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil1:, PORT=GALIL1, IPADDR=10.54.160.119, M1=m9,  M2=m10, M3=m11, M4=m12, M5=m13, M6=m14, M7=m15, M8=m16")
+# gse-galil15  In BM-A rack, Step-Pak; Slits, filters, diagnostic flag
+iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil2:, PORT=GALIL2, IPADDR=10.54.160.123, M1=m1,  M2=m2,  M3=m3,  M4=m4,  M5=m5,  M6=m6,  M7=m7,  M8=m8")  
+# gse-galil16  In BM-B rack, Step-Pak; BM-D mirror, BM-B vertical slits
+iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil3:, PORT=GALIL3, IPADDR=10.54.160.125, M1=m17, M2=m18, M3=m19, M4=m20, M5=m21, M6=m22, M7=m23, M8=m24")  
 
 #Load motor records for real motors
 dbLoadTemplate("Galil_motors.template")
--- iocBoot/ioc13bma_linux/motors.cmd
+++ iocBoot/ioc13bma_linux/motors.cmd
@@ -2,9 +2,11 @@ iocshLoad("Galil.cmd", "P=$(P)")
 
 ### Allstop, alldone
 dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(P)")
-doAfterIocInit "motorUtilInit($(P))"
 
 # A set of scan parameters for each positioner.  This is a convenience
 # for the user.  It can contain an entry for each scannable thing in the
 # crate.
-dbLoadTemplate("scanParms.template")
+dbLoadTemplate("scanParms.substitutions")
+
+doAfterIocInit "motorUtilInit($(P))"
+doAfterIocInit "seq BM13_Energy, 'E=$(P)E,MONO=$(P)m9,EXPTAB_Z=13BMD:m22,YXTAL=$(P)MON:,ZXTAL=$(P)m14'"
