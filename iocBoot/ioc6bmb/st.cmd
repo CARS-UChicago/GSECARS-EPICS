7c699024be64a242e56edc43f12bc4d98931a6c0
Mark Rivers <rivers@cars.uchicago.edu>
2024-07-07 18:23:20 -0500

    New file for APS 6-BM-B

---
+++ iocBoot/ioc6bmb/st.cmd
@@ -0,0 +1,49 @@
+# Linux startup script for 13-BM-A
+< envPaths
+
+dbLoadDatabase("$(CARS)/dbd/CARSLinux.dbd")
+CARSLinux_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("PREFIX", "6BMB_LVP:")
+epicsEnvSet("E1608_PREFIX", "6BMB_LVP:E1608:")
+
+#iocshLoad("serial.cmd",     "P=$(PREFIX), TS=gsets16")
+#iocshLoad("MeasComp.cmd",   "P=$(PREFIX)")
+iocshLoad("motors.cmd",     "P=$(PREFIX)")
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (You need the data catcher
+# or the equivalent for that.)  This database is configured to use the
+# "alldone" database (above) to figure out when motors have stopped moving
+# and it's time to trigger detectors.
+dbLoadRecords("$(SSCAN)/db/scan.db","P=$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+
+# User calc stuff
+iocshLoad("../calc_GSECARS.iocsh", "P=($PREFIX)")
+
+# Miscellaneous PV's
+dbLoadRecords("$(STD)/db/misc.db","P=$(PREFIX)")
+
+# devIocStats
+epicsEnvSet("ENGINEER", "Mark Rivers")
+epicsEnvSet("LOCATION", "6-BM-B roof")
+epicsEnvSet("GROUP", "SEES")
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(PREFIX)")
+
+< ../save_restore_IOCSH.cmd
+save_restoreSet_status_prefix("$(LINUX_PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(PREFIX)")
+
+
+iocInit
+
+### Start up the autosave task and tell it what to do.
+# The task is actually named "save_restore".
+# (See also, 'initHooks' above, which is the means by which the values that
+# will be saved by the task we're starting here are going to be restored.
+#
+# save positions every five seconds
+create_monitor_set("auto_positions.req",5,"P=$(PREFIX)")
+# save other things every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), E1608_PREFIX=$(E1608_PREFIX)")
