560c690e61e44e810da6823a0d7dc708dfe6abe6
Mark Rivers <rivers@cars.uchicago.edu>
2024-10-26 12:06:58 -0500

    Enable Keithley DMM2 for LVP

--- iocBoot/ioc13bmd_linux/serial.cmd
+++ iocBoot/ioc13bmd_linux/serial.cmd
@@ -11,13 +11,15 @@ iocshLoad ../asynIPPortConfig.cmd "PORT=serial10, IPADDR=$(TS):4010, IEOS=\\n,  
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial11, IPADDR=$(TS):4011, IEOS=\\r\\n,  OEOS=\\r\\n" # Mitutoyo     9600,'N',1,8,'N'  PP B05
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial12, IPADDR=$(TS):4012, IEOS=\\r\\n,  OEOS=\\r\\n" # Verdi laser 19200,'N',1,8,'N'  PP B06
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial13, IPADDR=$(TS):4013, IEOS=\\r,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
+# This port is used by ioc13bma_linux for Ion Pump 10
+#iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial15, IPADDR=$(TS):4015, IEOS=\\r,     OEOS=\\r"    # Omega meter; 9600,'N',2,7,'N'  LVP serial
 #iocshLoad ../asynIPPortConfig.cmd "PORT=serial16, IPADDR=$(TS):4016, IEOS=\\r,     OEOS=\\r"    # VME console; 9600,'N',1,8,'N'  VME console
 
 # Set up 2 serial ports on Moxa terminal server which is inside the MCB-4B slit controller box
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial17, IPADDR=10.54.160.57:4001,   IEOS=\\r,     OEOS=\\r"    # MCB-4B slit ; 19200,'N',1,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial18, IPADDR=10.54.160.57:4002,   IEOS=\\r,     OEOS=\\r"    # MCB-4B slit ; 19200,'N',1,8,'N'
+# These are currently run by ioc13bmd_LVP_XPS
+#iocshLoad ../asynIPPortConfig.cmd "PORT=serial17, IPADDR=10.54.160.57:4001,   IEOS=\\r,     OEOS=\\r"    # MCB-4B slit ; 19200,'N',1,8,'N'
+#iocshLoad ../asynIPPortConfig.cmd "PORT=serial18, IPADDR=10.54.160.57:4002,   IEOS=\\r,     OEOS=\\r"    # MCB-4B slit ; 19200,'N',1,8,'N'
 
 # Load asyn records on all ports
 dbLoadTemplate("asynRecord.template", "P=$(P)")
@@ -26,11 +28,12 @@ dbLoadTemplate("$(CARS)/db/Pelco_CM6700.substitutions", "P=$(P), PORT=serial1,  
 dbLoadRecords("$(IP)/db/SR570.db",                      "P=$(P), PORT=serial2,  A=A1")
 dbLoadRecords("$(IP)/db/SR570.db",                      "P=$(P), PORT=serial3,  A=A2")
 dbLoadRecords("$(IP)/db/SR570.db",                      "P=$(P), PORT=serial4,  A=A3")
-dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db",           "P=$(P), PORT=serial9,  Dmm=DMM1")
-#dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db",          "P=$(P), PORT=serial10, Dmm=DMM2")
-#dbLoadRecords("$(CARS)/db/lvp_dmm.db",                 "P=$(P), Dmm=DMM2,      DLY=0.1")
+#dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db",           "P=$(P), PORT=serial10,  Dmm=DMM1")
+dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db",          "P=$(P), PORT=serial9, Dmm=DMM2")
+dbLoadRecords("$(CARS)/db/lvp_dmm.db",                 "P=$(P), Dmm=DMM2,      DLY=0.1")
 dbLoadRecords("$(CARS)/db/Mitutoyo_Digimatic.db",       "P=$(P), PORT=serial11, R=Mitu")
 dbLoadRecords("$(CARS)/db/VerdiLaser.db",               "P=$(P), PORT=serial12, R=Verdi1:")
 dbLoadTemplate "LVP_pressure_control.template"          "P=$(P), PORT=serial15, R=Press_")
 
-doAfterIocInit 'seq &Keithley2kDMM, "P=13BMD:, Dmm=DMM1, channels=20, model=2700, stack=10000"'
+#doAfterIocInit 'seq &Keithley2kDMM, "P=13BMD:, Dmm=DMM1, channels=20, model=2700, stack=10000"'
+doAfterIocInit 'seq &Keithley2kDMM, "P=13BMD:, Dmm=DMM2, channels=10, model=2000, stack=10000"'
