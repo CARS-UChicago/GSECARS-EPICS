966c9c96ff91d092969596a6bc59f1011cf0a1a6
Mark Rivers <rivers@cars.uchicago.edu>
2024-10-26 12:25:34 -0500

    Changes for ion pump controller swaps; add debugging for Keithley

--- iocBoot/ioc13ida_linux/serial.cmd
+++ iocBoot/ioc13ida_linux/serial.cmd
@@ -16,20 +16,20 @@ iocshLoad ../asynIPPortConfig.cmd "PORT=serial12, IPADDR=GSETS2:4008, IEOS=\\n, 
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial13, IPADDR=$(TS):4013, IEOS=\\n,     OEOS=\\r"   # Keithley 2000; C/D vertical mirror temps; 19200,'N',1,8,'N'
 #iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"   # Oxford ILM cryometer; E mono;             9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial14, IPADDR=$(TS):4014, IEOS=\\r,     OEOS=\\r"   # SR-570; E WBS/BPM blade current; 9600,'N',1,8,'N'
-# Serial 15 is connection to VME crate
+iocshLoad ../asynIPPortConfig.cmd "PORT=serial15, IPADDR=$(TS):4015, IEOS=\\r,     OEOS=\\r"   # MPC         9600,'N',1,8,'N'
 # Serial 16 is COM port to Allen-Bradley PLC
 #iocshLoad ../asynIPPortConfig.cmd "PORT=serial17, IPADDR=$(TS):4017, IEOS=\\r,     OEOS=\\r"   # SR-570; C/D pinhole BPM blade current;  9600,'N',1,8,'N'
 
 
 dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial1,  CC1=cc1, CC2=cc2, PR1=pr1, PR2=pr2")
 dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial2,  CC1=cc3, CC2=cc4, PR1=pr3, PR2=pr4")
-dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial3,  PUMP=ip1")
+#dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial3,  PUMP=ip1")
 dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial3,  PUMP=ip5")
 dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial4,  PUMP=ip3")
 dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial5,  CC1=cc5, CC2=cc6, PR1=pr5, PR2=pr6")
 dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial6,  PUMP=ip6")
 dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial6,  PUMP=ip7")
-dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial7,  PUMP=ip2, PA=0, PN=2")
+#dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial7,  PUMP=ip2, PA=0, PN=2")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial7,  PUMP=ip4, PA=0, PN=1")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial8,  PUMP=ip9,   PA=0 ,PN=1")
 dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial8,  PUMP=ip10, PORT=serial8, PA=0, PN=2")
@@ -40,12 +40,15 @@ dbLoadRecords("$(IP)/db/MKS.db",              "P=$(P), PORT=serial11, CC1=cc7, C
 #dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial12, PUMP=ip8,  PA=0, PN=1")
 #dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial12, PUMP=ip11, PA=0, PN=2")
 #dbLoadRecords("$(IP)/db/TSP.db",              "P=$(P), PORT=serial12, TSP=tsp1,  PA=0")
-dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial12, PUMP=ip8")
-dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial12, PUMP=ip11")
+#dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial12, PUMP=ip8")
+#dbLoadRecords("$(IP)/db/Digitel_stream.db",   "P=$(P), PORT=serial12, PUMP=ip11")
 dbLoadRecords("$(IP)/db/Keithley2kDMM_mf.db", "P=$(P), PORT=serial13, Dmm=DMM2")
 #dbLoadRecords("$(CARS)/db/ILM200.db",         "P=$(P), PORT=serial14, R=ILM200_2")
 dbLoadRecords("$(IP)/db/SR570.db",            "P=$(P), PORT=serial14, A=A1,")
 #dbLoadRecords("$(IP)/db/SR570.db",            "P=$(P), PORT=serial17, A=A2,")
+dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial15,  PUMP=ip1, PA=0, PN=2")
+dbLoadRecords("$(IP)/db/MPC.db",              "P=$(P), PORT=serial15,  PUMP=ip2, PA=0, PN=1")
+
 
 # Load asyn records on all serial ports
 dbLoadTemplate("asynRecord.template", P=$(P))
@@ -57,4 +60,10 @@ asynSetTraceMask("serial12",0,0)
 
 asynSetTraceIOMask("serial11",0,ESCAPE)
 #asynSetTraceMask("serial11",0,ERROR|DRIVER)
+
+asynSetTraceIOMask("serial9",0,ESCAPE)
+asynSetTraceMask("serial9",0,ERROR|DRIVER)
+asynSetTraceFile("serial9",0,"keithly9.txt")
+
+
 
