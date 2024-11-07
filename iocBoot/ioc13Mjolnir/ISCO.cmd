7ee20f60e4eeddcbbc1770342591b10ae8a5c9f3
Mark Rivers <rivers@cars.uchicago.edu>
2024-11-07 12:58:27 -0600

    Add support for both Modbus RTU and TCP

--- iocBoot/ioc13Mjolnir/ISCO.cmd
+++ iocBoot/ioc13Mjolnir/ISCO.cmd
@@ -11,11 +11,11 @@ epicsEnvSet("TIMEOUT_MS", "1000")
 
 # Change IP address for your device
 # This is for Modbus/TCP on public network
-#drvAsynIPPortConfigure("$(PORT)", "gse-isco1:502", 0, 0, 0)
+drvAsynIPPortConfigure("$(PORT)", "gse-isco1:502", 0, 0, 0)
 # This is for Modbus/TCP on private network
 #drvAsynIPPortConfigure("$(PORT)", "192.168.0.2:502", 0, 0, 0)
 # This is for Modbus/RTU with Moxa terminal server
-drvAsynIPPortConfigure("$(PORT)", "gsets22:4001", 0, 0, 0)
+#drvAsynIPPortConfigure("$(PORT)", "gsets22:4001", 0, 0, 0)
 
 # Enable ASYN_TRACEIO_HEX on octet server
 asynSetTraceIOMask("$(PORT)", 0, HEX)
@@ -36,7 +36,10 @@ asynSetTraceIOTruncateSize("$(PORT)", 0, 256)
 #                      int timeoutMsec,
 #                      int writeDelayMsec)
 
-modbusInterposeConfig("$(PORT)", 1, $(TIMEOUT_MS), 80)
+# This is for Modbus TCP
+modbusInterposeConfig("$(PORT)", 0, $(TIMEOUT_MS), 0)
+# This is for Modbus RTU
+#modbusInterposeConfig("$(PORT)", 1, $(TIMEOUT_MS), 80)
 
 ### The syringe pump supports the following modbus function codes:
 #    01 - read discrete output coils
