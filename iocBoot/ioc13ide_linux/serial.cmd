c65b84165da098104cb4f1b995451e44d782d6b0
Mark Rivers <rivers@cars.uchicago.edu>
2025-02-14 11:26:45 -0600

    add cp5 cryopulse serial reading

--- iocBoot/ioc13ide_linux/serial.cmd
+++ iocBoot/ioc13ide_linux/serial.cmd
@@ -2,7 +2,7 @@ iocshLoad ../asynIPPortConfig.cmd "PORT=serial1,  IPADDR=$(TS):4001, IEOS=\\r,  
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial2,  IPADDR=$(TS):4002, IEOS=\\r,     OEOS=\\r"    # SRS570;      9600,'N',2,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial3,  IPADDR=$(TS):4003, IEOS=\\r,     OEOS=\\r"    # SRS570;      9600,'N',2,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial4,  IPADDR=$(TS):4004, IEOS=\\r,     OEOS=\\r"    # SRS570;      9600,'N',2,8,'N'
-iocshLoad ../asynIPPortConfig.cmd "PORT=serial5,  IPADDR=$(TS):4005, IEOS=\\r,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
+iocshLoad ../asynIPPortConfig.cmd "PORT=serial5,  IPADDR=$(TS):4005, IEOS=\\r\\n,   OEOS=\\r\\n"    # CryoPulse;   9600,'N',2,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial6,  IPADDR=$(TS):4006, IEOS=\\n,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
 iocshLoad ../asynIPPortConfig.cmd "PORT=serial7,  IPADDR=$(TS):4007, IEOS=\\n,     OEOS=\\r"    # Unused;      9600,'N',1,8,'N'
 #iocshLoad ../asynIPPortConfig.cmd "PORT=serial8,  IPADDR=$(TS):4008, IEOS=\\r,     OEOS=\\r"    # VME console  9600,'N',1,8,'N'
@@ -14,3 +14,6 @@ dbLoadRecords("$(IP)/db/SR570.db",                       "P=$(P), PORT=serial1, 
 dbLoadRecords("$(IP)/db/SR570.db",                       "P=$(P), PORT=serial2,  A=A2")
 dbLoadRecords("$(IP)/db/SR570.db",                       "P=$(P), PORT=serial3,  A=A3")
 dbLoadRecords("$(IP)/db/SR570.db",                       "P=$(P), PORT=serial4,  A=A4")
+# for back-compat, the cryopulse 5 PV names use '13XRM'
+dbLoadRecords("cp_stream.db",                            "P=13XRM:, PORT=serial5,  R=CP5")
+
