f7c1b4638ff089ab60a31b871354c0d48c86a9db
Mark Rivers <rivers@cars.uchicago.edu>
2025-02-23 12:00:03 -0600

    Change port name from L0 to serial1

--- iocBoot/ioc13Syringe2/st.cmd
+++ iocBoot/ioc13Syringe2/st.cmd
@@ -7,14 +7,17 @@ CARSLinux_registerRecordDeviceDriver(pdbbase)
 
 # Serial port settings for pump
 # Baud Rate: 9600
-drvAsynIPPortConfigure("L0","gsets4:2001",0,0,0)
+#drvAsynIPPortConfigure("L0","gsets4:2001",0,0,0)
+drvAsynIPPortConfigure("serial1", "GSETS11:4001 COM", 0, 0, 1)
 # We don't set terminators here, we let StreamDevice take care of it
-asynSetTraceIOMask("L0",0,2)
-#asynSetTraceMask("L0",0,0x9)
+#asynSetTraceIOMask("L0",0,2)
+asynSetTraceIOMask("serial1", 0, 4)
+##asynSetTraceMask("L0",0,0x9)
 
-dbLoadRecords("$(IP)/db/CPSyringe.db", "P=13Syringe2:, R=S1:, PORT=L0")
-dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=13Syringe2:, R=asyn1, PORT=L0, ADDR=0, IMAX=80, OMAX=80")
-
+#dbLoadRecords("$(IP)/db/CPSyringe.db", "P=13Syringe2:, R=S1:, PORT=L0")
+dbLoadRecords("$(IP)/db/CPSyringe.db", "P=13Syringe2:, R=S1:, PORT=serial1")
+#dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=13Syringe2:, R=asyn1, PORT=L0, ADDR=0, IMAX=80, OMAX=80")
+dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=13Syringe2:, R=asyn1, PORT=serial1, ADDR=0, IMAX=80, OMAX=80")
 #var streamDebug 1
 
 < ../save_restore_IOCSH.cmd
