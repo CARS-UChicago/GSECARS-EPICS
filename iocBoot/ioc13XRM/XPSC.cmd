5a5cd835dda06ceee35c3059f15d18c4642d370e
Mark Rivers <rivers@cars.uchicago.edu>
2025-06-11 09:09:58 -0500

    update XPS C/D definitions for 13IDE

--- iocBoot/ioc13XRM/XPSC.cmd
+++ iocBoot/ioc13XRM/XPSC.cmd
@@ -1,17 +1,18 @@
 #MN newport-xsp4 = 10.54.160.180, XPS-C
-# XPSCreateController("XPSC", "newport-xps4", 5001, 3, 10, 500, 0, 500)
-XPSCreateController("XPSC", "10.54.160.180", 5001, 7, 10, 500, 0, 500)
+XPSCreateController("XPSC", "10.54.160.180", 5001, 6, 10, 500, 0, 500)
 
 #--  
 #-- # XPS asyn port,  axis, groupName.positionerName, stepSize
 #-- # card,  axis, groupName.positionerName, stepsPerUnit
-XPSCreateAxis("XPSC", 0, "CFine.X",      "100000") # VP-25XL
-XPSCreateAxis("XPSC", 1, "CFine.Theta",    "2000") # URS75CC
-XPSCreateAxis("XPSC", 2, "CFocus.Pos",   "100000") # VP-25XA
-XPSCreateAxis("XPSC", 3, "XRDX.Pos",        "400") # UTS150CC  xrddet_x
-XPSCreateAxis("XPSC", 4, "XRDZ.Pos",        "400") # UTS150CC  xrddet_z
-XPSCreateAxis("XPSC", 5, "XRDY.Pos",       "2000") # ILS200CC  xrddet_y
-XPSCreateAxis("XPSC", 6, "CoarseY.Pos",    "5000") # IMS300V   coarse_y
+#XPSCreateAxis("XPSC", 0, "CFine.X",      "100000") # VP-25XL
+#XPSCreateAxis("XPSC", 1, "CFine.Theta",    "2000") # URS75CC
+#XPSCreateAxis("XPSC", 2, "CFocus.Pos",   "100000") # VP-25XA
+XPSCreateAxis("XPSC", 0, "XRDX.Pos",        "400") # UTS150CC  xrddet_x
+XPSCreateAxis("XPSC", 1, "XRDZ.Pos",        "400") # UTS150CC  xrddet_z
+XPSCreateAxis("XPSC", 2, "XRDY.Pos",       "2000") # ILS150CC  xrddet_y
+XPSCreateAxis("XPSC", 3, "AnaX.Pos",       "2000") # ILS150CC  herfd_x
+XPSCreateAxis("XPSC", 4, "AnaY.Pos",       "2000") # ILS150CC  herfd_y
+XPSCreateAxis("XPSC", 5, "CoarseY.Pos",    "5000") # IMS300V   coarse_y
 
 XPSCreateProfile("XPSC", 8192, "Administrator", "Administrator")
 #-- 
--- iocBoot/ioc13XRM/XPSD.cmd
+++ iocBoot/ioc13XRM/XPSD.cmd
@@ -1,25 +1,25 @@
 # asyn port, IP address, IP port, number of axes, 
 # active poll period (ms), idle poll period (ms), 
 # enable set position, set position settling time (ms)
-#MN newport-xsp14 = 10.54.160.210, newport-xps14, XPS-D
-XPSCreateController("XPSD", "newport-xps14", 5001, 8, 10, 500, 0, 500)
+#MN newport-xsp14 = 10.54.160.223, newport-xps19, XPS-D
+XPSCreateController("XPSD", "newport-xps19", 5001, 8, 10, 500, 0, 500)
 
 # XPS asyn port,  axis, groupName.positionerName, stepSize
 # card,  axis, groupName.positionerName, stepsPerUnit
 XPSCreateAxis("XPSD", 0, "Fine.X",     "100000") # VP25-XL  fine_x
 XPSCreateAxis("XPSD", 1, "Fine.Y",      "50000") # VP5Z-A   fine_y
-XPSCreateAxis("XPSD", 2, "Focus.Pos"   "100000") # VP25-XA  focus_z
-XPSCreateAxis("XPSD", 3, "CoarseX.Pos",  "2000") # ILS150CC coarse_x
-XPSCreateAxis("XPSD", 4, "AnaX.Pos",     "2000") # ILS150CC herfd_x (det)
-XPSCreateAxis("XPSD", 5, "AnaY.Pos",     "2000") # ILS150CC herfd_y (det)
-XPSCreateAxis("XPSD", 6, "AnaZ.Pos",     "2000") # ILS100PP  herfd_z (analyzer)
+XPSCreateAxis("XPSD", 2, "Fine.Theta",  "10000") # RGV100BL (fine_theta)
+XPSCreateAxis("XPSD", 3, "Focus.Pos"   "100000") # VP25-XA  focus_z
+XPSCreateAxis("XPSD", 4, "Focus2.Pos"  "100000") # VP25-XA  focus_z
+XPSCreateAxis("XPSD", 5, "CoarseX.Pos",  "2000") # ILS150CC coarse_x
+XPSCreateAxis("XPSD", 6, "AnaZ.Pos",     "2000") # ILS200CC  herfd_z (analyzer)
 XPSCreateAxis("XPSD", 7, "AnaTH.Pos",    "1000") # RV160CC  herfd_th (analyzer)
 
 # asynSetTraceIOMask("XPSD", 0, 2)
 # asynSetTraceMask("XPSD", 0, 255)
 
 # asynPort, IP address, IP port, poll period (ms)
-# XPSAuxConfig("XPSD_AUX", "newport-xps14", 5001, 50)
+# XPSAuxConfig("XPSD_AUX", "newport-xps19", 5001, 50)
 
 asynSetTraceIOMask("XPSD",0,2)
 #asynSetTraceMask("XPSD",0,0x3)
