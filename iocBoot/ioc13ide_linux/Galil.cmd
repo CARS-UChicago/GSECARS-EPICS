320186b6eb616c614f2777cf07c7a432583ffa14
Mark Rivers <rivers@cars.uchicago.edu>
2026-01-29 12:59:52 -0600

    update for ID-E Galils

--- iocBoot/ioc13ide_linux/Galil.cmd
+++ iocBoot/ioc13ide_linux/Galil.cmd
@@ -1,8 +1,13 @@
 # gse-galil24 In ID-E rack, 4140 drivers, Patch Panel A
 iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil1:, PORT=GALIL1, IPADDR=10.54.160.134, M1=m1,  M2=m2,  M3=m3,  M4=m4,  M5=m5,  M6=m6,  M7=m7,  M8=m8")  
 
-# gse-galil27 In ID-E rack, 4140 drivers, Patch Panel B
-iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil2:, PORT=GALIL2, IPADDR=10.54.160.137, M1=m9,  M2=m10, M3=m11, M4=m12, M5=m13, M6=m14, M7=m15, M8=m16")  
+# 2026-Jan-27: try 4040 for KB Mirror motors instead of 4140
+# before 2026-Jan-27
+### gse-galil27 In ID-E rack, 4140 drivers, Patch Panel B
+### iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil2:, PORT=GALIL2, IPADDR=10.54.160.137, M1=m9,  M2=m10, M3=m11, M4=m12, M5=m13, M6=m14, M7=m15, M8=m16")  
+
+# gse-galil14 In ID-E rack, 4140 drivers, Patch Panel B
+iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil2:, PORT=GALIL2, IPADDR=10.54.160.122, M1=m9,  M2=m10, M3=m11, M4=m12, M5=m13, M6=m14, M7=m15, M8=m16")  
 
 # gse-galil28 In ID-E rack, 4140 drivers, Patch Panel C
 iocshLoad("../Galil_stepper_controller.cmd", "P=$(P)Galil3:, PORT=GALIL3, IPADDR=10.54.160.138, M1=m17, M2=m18, M3=m19, M4=m20, M5=m21, M6=m22, M7=m23, M8=m24")  
